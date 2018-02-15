37a70cd9f6492cf12bf52049b8cb6e9dc2c15420
John Coxhead <jcoxhead@hotmail.com>
2018-02-15 20:37:29 +0000

    fix to tests

--- src/SFA.DAS.AssessorService.Application.Api.UnitTests/WebAPI/OrganisationContoller/WhenGetAssessmentProvidersValidationFails.cs
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/WebAPI/OrganisationContoller/WhenGetAssessmentProvidersValidationFails.cs
@@ -25,7 +25,7 @@
 
         private Machine.Specifications.It verify_succesfully = () =>
         {
-            Result.Should().BeOfType<BadRequestResult>();
+            Result.Should().BeOfType<BadRequestObjectResult>();
         };
     }
 }
