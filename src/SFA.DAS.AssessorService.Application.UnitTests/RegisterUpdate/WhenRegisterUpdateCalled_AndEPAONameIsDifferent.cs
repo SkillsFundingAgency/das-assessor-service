7cc7fe7474d7bfc726b2048b0b4e6581b01b7d59
David Gouge <davegouge@gmail.com>
2018-02-21 15:29:28 +0000

    Test and functionality to update Org name if changed on Register

---
+++ src/SFA.DAS.AssessorService.Application.UnitTests/RegisterUpdate/WhenRegisterUpdateCalled_AndEPAONameIsDifferent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.AssessmentOrgs.Api.Client.Core.Types;
+using SFA.DAS.AssessorService.Application.RegisterUpdate;
+using SFA.DAS.AssessorService.ViewModel.Models;
+
+namespace SFA.DAS.AssessorService.Application.UnitTests.RegisterUpdate
+{
+    public class WhenRegisterUpdateCalled_AndEPAONameIsDifferent : RegisterUpdateTestsBase
+    {
+        private Guid _organisationId;
+
+        [SetUp]
+        public void Arrange()
+        {
+            Setup();
+
+            _organisationId = Guid.NewGuid();
+
+            ApiClient.Setup(c => c.FindAllAsync())
+                .Returns(Task.FromResult(new List<OrganisationSummary>()
+                {
+                    new OrganisationSummary {Id = "EPA0001", Name = "The New EPAO Name"},
+                    new OrganisationSummary {Id = "EPA0002", Name = "Another EPAO"}
+
+                }.AsEnumerable()));
+
+            
+            OrganisationRepository.Setup(r => r.GetAllOrganisations())
+                .Returns(Task.FromResult(new List<OrganisationQueryViewModel>
+                {
+                    new OrganisationQueryViewModel() {EndPointAssessorOrganisationId = "EPA0001", EndPointAssessorName = "OLD NAME", Id = _organisationId},
+                    new OrganisationQueryViewModel() {EndPointAssessorOrganisationId = "EPA0002", EndPointAssessorName = "Another EPAO"}
+                }.AsEnumerable()));
+        }
+
+        [Test]
+        public void ThenOrganisationIsUpdatedWithNewName()
+        {
+            RegisterUpdateHandler.Handle(new RegisterUpdateRequest(), new CancellationToken()).Wait();
+            Mediator.Verify(m =>
+                m.Send(
+                    It.Is<OrganisationUpdateViewModel>(vm =>
+                        vm.Id == _organisationId && vm.EndPointAssessorName == "The New EPAO Name"),
+                    new CancellationToken()));
+        }
+    }
+}
