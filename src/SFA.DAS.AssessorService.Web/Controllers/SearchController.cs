f980323cef072c60e2ea39a079479774ff3a0f8c
David Gouge <davegouge@gmail.com>
2018-02-22 16:02:29 +0000

    Start of Search Controller

---
+++ src/SFA.DAS.AssessorService.Web/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SFA.DAS.AssessorService.Web.Controllers
+{
+    [Authorize]
+    public class SearchController : Controller
+    {
+        private readonly ILogger<SearchController> _logger;
+
+        public SearchController(ILogger<SearchController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
