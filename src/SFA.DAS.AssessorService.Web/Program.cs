22b0fb740409d87f88eb25bc354b80b80da46124
David Gouge <davegouge@gmail.com>
2018-02-23 08:02:26 +0000

    Dev Changes for startup
    Change to startup to only use Kestrels ssl settings if in a Dev environment

--- src/SFA.DAS.AssessorService.Web/Program.cs
+++ src/SFA.DAS.AssessorService.Web/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -24,18 +25,33 @@ namespace SFA.DAS.AssessorService.Web
             }
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            IHostingEnvironment hostingEnvironment = null;
+
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(
+                    services =>
+                    {
+                        hostingEnvironment = services
+                            .Where(x => x.ServiceType == typeof(IHostingEnvironment))
+                            .Select(x => (IHostingEnvironment) x.ImplementationInstance)
+                            .First();
+                    })
                 .UseKestrel(options =>
                 {
                     options.AddServerHeader = false;
-                    options.Listen(IPAddress.Loopback, 5015, listenOptions =>
+                    if (hostingEnvironment.IsDevelopment())
                     {
-                        listenOptions.UseHttps("sfa.das.assessorservice.pfx", "C0ventry18");
-                    });
+                        options.Listen(IPAddress.Loopback, 5015, listenOptions =>
+                        {
+                            listenOptions.UseHttps("sfa.das.assessorservice.pfx", "C0ventry18");
+                        });
+                    }
                 })
                 .UseStartup<Startup>()
                 .UseNLog()
                 .Build();
+        }
     }
 }
