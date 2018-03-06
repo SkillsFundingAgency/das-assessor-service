cbf6998aa007197f23543281f3598f2190c06a45
David Gouge <davegouge@gmail.com>
2018-03-06 07:54:48 +0000

    Moved Config init and added a log to see what's what

--- src/SFA.DAS.AssessorService.Web/Startup.cs
+++ src/SFA.DAS.AssessorService.Web/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.AssessorService.Application.Api.Client;
 using SFA.DAS.AssessorService.Application.Api.Client.Clients;
 using SFA.DAS.AssessorService.Settings;
@@ -15,18 +16,23 @@ namespace SFA.DAS.AssessorService.Web
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+        private readonly ILogger<Startup> _logger;
         private const string ServiceName = "SFA.DAS.AssessorService";
         private const string Version = "1.0";
 
-        public Startup(IConfiguration config)
+        public Startup(IConfiguration config, ILogger<Startup> logger)
         {
-            Configuration = ConfigurationService.GetConfig(config["EnvironmentName"], config["ConnectionStrings:Storage"], Version, ServiceName).Result;
+            _config = config;
+            _logger = logger;
         }
 
-        public IWebConfiguration Configuration { get; }
+        public IWebConfiguration Configuration { get; set; }
 
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            _logger.LogInformation($"dasass_config: {_config["EnvironmentName"]}, {_config["ConnectionStrings:Storage"]}");
+            Configuration = ConfigurationService.GetConfig(_config["EnvironmentName"], _config["ConnectionStrings:Storage"], Version, ServiceName).Result;
             services.AddAndConfigureAuthentication(Configuration);
             services.AddMvc().AddControllersAsServices().AddSessionStateTempDataProvider();
             services.AddSession();
