6be020ab5ad1e1c58cc89b792bb4341aef1f8809
David Gouge <davegouge@gmail.com>
2018-03-08 14:33:11 +0000

    Test Distributed Cache

--- src/SFA.DAS.AssessorService.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.AssessorService.Web/Controllers/HomeController.cs
@@ -1,13 +1,21 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using SFA.DAS.AssessorService.Web.Models;
 
 namespace SFA.DAS.AssessorService.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IDistributedCache _cache;
+
+        public HomeController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
         public IActionResult Index()
         {
+            _cache.SetString("Hello", "Dave");
             return View();
         }
 
