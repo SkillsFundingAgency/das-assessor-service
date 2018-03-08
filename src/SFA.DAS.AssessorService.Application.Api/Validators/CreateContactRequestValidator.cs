8ede12727d1b72fe61be2efcadfd7173a21fc977
John Coxhead <jcoxhead@hotmail.com>
2018-03-08 15:15:57 +0000

    fix to 212 bug

--- src/SFA.DAS.AssessorService.Application.Api/Validators/CreateContactRequestValidator.cs
+++ src/SFA.DAS.AssessorService.Application.Api/Validators/CreateContactRequestValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.Extensions.Localization;
 using SFA.DAS.AssessorService.Api.Types.Models;
 using SFA.DAS.AssessorService.Application.Api.Consts;
@@ -10,7 +11,7 @@ namespace SFA.DAS.AssessorService.Application.Api.Validators
     {
         private readonly IOrganisationQueryRepository _organisationQueryRepository;
         private readonly IContactQueryRepository _contactQueryRepository;
- 
+
         public CreateContactRequestValidator(IStringLocalizer<CreateContactRequestValidator> localiser,
             IOrganisationQueryRepository organisationQueryRepository,
             IContactQueryRepository contactQueryRepository
@@ -22,7 +23,7 @@ namespace SFA.DAS.AssessorService.Application.Api.Validators
             // ReSharper disable once LocalNameCapturedOnly
             CreateContactRequest createContactRequest;
             RuleFor(contact => contact.Email).NotEmpty().WithMessage(
-                   localiser[ResourceMessageName.DisplayNameMustBeDefined].Value)
+                    localiser[ResourceMessageName.DisplayNameMustBeDefined].Value)
                 .MaximumLength(120)
                 // Please note we have to string.Format this due to limitation in Moq not handling Optional
                 // Params
@@ -30,7 +31,7 @@ namespace SFA.DAS.AssessorService.Application.Api.Validators
                     nameof(createContactRequest.Email), 120));
 
             RuleFor(contact => contact.DisplayName).NotEmpty().WithMessage(
-                localiser[ResourceMessageName.EMailMustBeDefined].Value)
+                    localiser[ResourceMessageName.EMailMustBeDefined].Value)
                 .MaximumLength(120)
                 // Please note we have to string.Format this due to limitation in Moq not handling Optional
                 // Params
@@ -40,26 +41,28 @@ namespace SFA.DAS.AssessorService.Application.Api.Validators
             RuleFor(contact => contact.Username)
                 .NotEmpty()
                 .WithMessage(
-                       localiser[ResourceMessageName.UserNameMustBeDefined].Value)
-               .MaximumLength(12)
-               // Please note we have to string.Format this due to limitation in Moq not handling Optional
-               // Params
-               .WithMessage(string.Format(localiser[ResourceMessageName.MaxLengthError].Value,
+                    localiser[ResourceMessageName.UserNameMustBeDefined].Value)
+                .MaximumLength(12)
+                // Please note we have to string.Format this due to limitation in Moq not handling Optional
+                // Params
+                .WithMessage(string.Format(localiser[ResourceMessageName.MaxLengthError].Value,
                     nameof(createContactRequest.Username), 30));
 
             RuleFor(contact => contact.EndPointAssessorOrganisationId).NotEmpty().WithMessage(
                 localiser[ResourceMessageName.EndPointAssessorOrganisationIdMustBeDefined].Value);
 
-            RuleFor(contact => contact).Must(NotAlreadyExist).WithMessage(localiser[ResourceMessageName.AlreadyExists].Value);
+            RuleFor(contact => contact).Custom((contact, context) =>
+                {
+                    var result = _contactQueryRepository.CheckContactExists(contact.Username).Result;
+                    if (result)
+                    {
+                        context.AddFailure(new ValidationFailure("Contact",
+                            localiser[ResourceMessageName.AlreadyExists].Value));
+                    }
+                });
 
             RuleFor(contact => contact.EndPointAssessorOrganisationId).Must(HaveExistingOrganisation).WithMessage(
                 localiser[ResourceMessageName.HaveExistingOrganisation].Value);
-        }
-
-        private bool NotAlreadyExist(CreateContactRequest contact)
-        {
-            var result = _contactQueryRepository.CheckContactExists(contact.Username).Result;
-            return !result;
         }
 
         private bool HaveExistingOrganisation(string endPointAssessorOrganisationId)
