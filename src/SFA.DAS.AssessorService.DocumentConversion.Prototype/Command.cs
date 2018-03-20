e25797231d4874b429b7f0dd1d5cf676b1f605b4
John Coxhead <jcoxhead@hotmail.com>
2018-03-20 10:47:53 +0000

    Skeleton  for Prototype

---
+++ src/SFA.DAS.AssessorService.DocumentConversion.Prototype/Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.AssessorService.DocumentConversion.Prototype.Data;
+
+namespace SFA.DAS.AssessorService.DocumentConversion.Prototype
+{
+    public class Command
+    {
+        public Task Execute()
+        {
+            foreach (var certificate in CertificateData.GetData())
+            {
+                Console.WriteLine($"Processig Certificate - {certificate.Id}");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.DocumentConversion.Prototype/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SFA.DAS.AssessorService.DocumentConversion.Prototype
+{
+    public class AppMain
+    {
+        static void Main(string[] args)
+        {
+            Bootstrapper.Initialise();
+
+            var command = Bootstrapper.Container.GetInstance<Command>();
+            command.Execute().GetAwaiter().GetResult();
+        }
+    }
+}
