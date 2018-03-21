b2b02fa06d61b6df3830a09974ebd21fcefb84ee
John Coxhead <jcoxhead@hotmail.com>
2018-03-21 10:59:41 +0000

    Changes for merge

--- src/SFA.DAS.AssessorService.DocumentConversion.Prototype/Data/CertificatesRepository.cs
+++ src/SFA.DAS.AssessorService.DocumentConversion.Prototype/Data/CertificatesRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using FizzWare.NBuilder;
 using Newtonsoft.Json;
 using SFA.DAS.AssessorService.Domain.Entities;
@@ -7,25 +8,33 @@ namespace SFA.DAS.AssessorService.DocumentConversion.Prototype.Data
 {
     public class CertificatesRepository
     {
-        public static IEnumerable<Certificate> GetData()
+        public IEnumerable<Certificate> GetData()
         {
             //var mergedList = list1.Union(list2).ToList();
             // Merge in data from example ....
+            var certificates = new List<Certificate>();
+            for (var sequenceNumber = 1; sequenceNumber <= 8; sequenceNumber++)
+            {
+                var number = sequenceNumber;
+                certificates.Add(
+                    Builder<Certificate>.CreateNew().With(q => q.CertificateReference = number.ToString().PadLeft(8, '0')).Build()
+                );
+            }
 
-            var certificates = Builder<Certificate>.CreateListOfSize(2)
+            var dummyCertificates = Builder<Certificate>.CreateListOfSize(2)
                 .All()
                 .With(
                     q => q.CertificateData =
                         JsonConvert.SerializeObject(
                             Builder<SFA.DAS.AssessorService.Domain.JsonData.CertificateData>
-                                .CreateNew()                                
+                                .CreateNew()
                                 .With(x => x.ContactName = "Kim Fucious")
                                 .With(x => x.ContactPostCode = "B61 4FE")
                                 .Build()
                         ))
                 .Build();
 
-            return certificates;
+            return certificates.Union(dummyCertificates);
         }
     }
 }
