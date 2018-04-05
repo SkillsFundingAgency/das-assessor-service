3fbbc7da9ebe4e3ce91b54dc57b696d0f0eb1103
John Coxhead <jcoxhead@hotmail.com>
2018-04-05 09:55:10 +0100

    fix to broken build

--- src/SFA.DAS.AssessorService.PrintFunctionProcessFlow.UnitTests/Class1.cs
+++ src/SFA.DAS.AssessorService.PrintFunctionProcessFlow.UnitTests/Class1.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Threading;
+using FizzWare.NBuilder;
+using Moq;
+using NUnit.Framework;
 
 namespace SFA.DAS.AssessorService.PrintFunctionProcessFlow.UnitTests
 {
-    public class Class1
+    public class DummyTest
     {
+        [SetUp]
+        public void Arrange()
+        {
+
+        }
+
+        [Test]
+        public void ItShouldReturnResult()
+        {
+
+        }
     }
 }
