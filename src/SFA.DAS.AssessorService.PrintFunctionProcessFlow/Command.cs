4c9212ad18f5bea4f3de8cca048afc47b3bf0e16
John Coxhead <jcoxhead@hotmail.com>
2018-04-08 20:02:56 +0100

    Added email sender

--- src/SFA.DAS.AssessorService.PrintFunctionProcessFlow/Command.cs
+++ src/SFA.DAS.AssessorService.PrintFunctionProcessFlow/Command.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using SFA.DAS.AssessorService.PrintFunctionProcessFlow.DomainServices;
+using SFA.DAS.AssessorService.PrintFunctionProcessFlow.EMail;
 using SFA.DAS.AssessorService.PrintFunctionProcessFlow.Logger;
 
 namespace SFA.DAS.AssessorService.PrintFunctionProcessFlow
@@ -10,26 +11,31 @@ namespace SFA.DAS.AssessorService.PrintFunctionProcessFlow
         private readonly IAggregateLogger _aggregateLogger;
         private readonly CoverLetterService _coverLetterService;
         private readonly IFACertificateService _ifaCertificateService;
+        private readonly EMailSender _emailSender;
 
         public Command(IAggregateLogger aggregateLogger,
             CoverLetterService coverLetterService,
-            IFACertificateService ifaCertificateService)
+            IFACertificateService ifaCertificateService,
+            EMailSender emailSender)
         {
             _aggregateLogger = aggregateLogger;
             _coverLetterService = coverLetterService;
             _ifaCertificateService = ifaCertificateService;
+            _emailSender = emailSender;
         }
 
         public async Task Execute()
         {
             _aggregateLogger.LogInfo("Print Function Flow Started");
 
-            _aggregateLogger.LogInfo("101 Azure Function Demo - Accessing Environment variables");
-            var customSetting = Environment.GetEnvironmentVariable("CustomSetting", EnvironmentVariableTarget.Process);
-            _aggregateLogger.LogInfo($"Process Environment = {EnvironmentVariableTarget.Process}");
+            await _emailSender.SendEMail();
 
-            await _coverLetterService.Create();
-            await _ifaCertificateService.Create();
+            //_aggregateLogger.LogInfo("101 Azure Function Demo - Accessing Environment variables");
+            //var customSetting = Environment.GetEnvironmentVariable("CustomSetting", EnvironmentVariableTarget.Process);
+            //_aggregateLogger.LogInfo($"Process Environment = {EnvironmentVariableTarget.Process}");
+
+            //await _coverLetterService.Create();
+            //await _ifaCertificateService.Create();
         }
     }
 }
