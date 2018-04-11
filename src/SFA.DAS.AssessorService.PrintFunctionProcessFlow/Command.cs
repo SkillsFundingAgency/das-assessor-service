140b9abd009b4e0103bfc6698908b543bd2c230b
John Coxhead <jcoxhead@hotmail.com>
2018-04-11 23:08:51 +0100

    Test Commit

--- src/SFA.DAS.AssessorService.PrintFunctionProcessFlow/Command.cs
+++ src/SFA.DAS.AssessorService.PrintFunctionProcessFlow/Command.cs
@@ -44,16 +44,16 @@ namespace SFA.DAS.AssessorService.PrintFunctionProcessFlow
 
             _notificationService.Send();
 
-            //if (AnythingToProcess())
-            //{
-            //    //await _emailSender.SendEMail();
-            //    await _coverLetterService.Create();
-            //    await _ifaCertificateService.Create();
-            //}
-            //else
-            //{
-            //    _aggregateLogger.LogInfo("Nothing to Process");
-            //}
+            if (AnythingToProcess())
+            {
+                //await _emailSender.SendEMail();
+                await _coverLetterService.Create();
+                await _ifaCertificateService.Create();
+            }
+            else
+            {
+                _aggregateLogger.LogInfo("Nothing to Process");
+            }
         }
 
         private bool AnythingToProcess()
