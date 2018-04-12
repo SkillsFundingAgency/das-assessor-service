4637cba5481300f9e37b54fa7557affc8e2dcadf
John Coxhead <jcoxhead@hotmail.com>
2018-04-12 08:33:08 +0100

    Adding back commented out code

--- src/SFA.DAS.AssessorService.PrintFunctionProcessFlow/Command.cs
+++ src/SFA.DAS.AssessorService.PrintFunctionProcessFlow/Command.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using SFA.DAS.AssessorService.PrintFunctionProcessFlow.Data;
 using SFA.DAS.AssessorService.PrintFunctionProcessFlow.DomainServices;
-using SFA.DAS.AssessorService.PrintFunctionProcessFlow.EMail;
 using SFA.DAS.AssessorService.PrintFunctionProcessFlow.Logger;
 using SFA.DAS.AssessorService.PrintFunctionProcessFlow.Notification;
 
@@ -16,21 +15,18 @@ namespace SFA.DAS.AssessorService.PrintFunctionProcessFlow
         private readonly IFACertificateService _ifaCertificateService;
         private readonly CertificatesRepository _certificatesRepository;
         private readonly NotificationService _notificationService;
-        private readonly EMailSender _emailSender;
 
         public Command(IAggregateLogger aggregateLogger,
             CoverLetterService coverLetterService,
             IFACertificateService ifaCertificateService,
             CertificatesRepository certificatesRepository,
-            NotificationService notificationService,
-            EMailSender emailSender)
+            NotificationService notificationService)
         {
             _aggregateLogger = aggregateLogger;
             _coverLetterService = coverLetterService;
             _ifaCertificateService = ifaCertificateService;
             _certificatesRepository = certificatesRepository;
             _notificationService = notificationService;
-            _emailSender = emailSender;
         }
 
         public async Task Execute()
@@ -42,13 +38,12 @@ namespace SFA.DAS.AssessorService.PrintFunctionProcessFlow
                 Environment.GetEnvironmentVariable("CustomSetting", EnvironmentVariableTarget.Process);
             _aggregateLogger.LogInfo($"Process Environment = {EnvironmentVariableTarget.Process}");
 
-            _notificationService.Send();
-
             if (AnythingToProcess())
             {
-                //await _emailSender.SendEMail();
                 await _coverLetterService.Create();
                 await _ifaCertificateService.Create();
+
+                await _notificationService.Send();
             }
             else
             {
