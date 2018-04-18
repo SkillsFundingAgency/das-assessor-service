74ff56d3aae28e9258da5b95cfbbcbcef69bb5c0
John Coxhead <jcoxhead@hotmail.com>
2018-04-18 14:27:37 +0100

    Added new table EMailTemplates

---
+++ src/SFA.DAS.AssessorService.Domain/Entities/EMailTemplate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SFA.DAS.AssessorService.Domain.Entities
+{
+    public class EMailTemplate : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public string TemplateName { get; set; }
+        public string TemplateId { get; set; }
+        public string Recipients { get; set; }
+    }
+}
