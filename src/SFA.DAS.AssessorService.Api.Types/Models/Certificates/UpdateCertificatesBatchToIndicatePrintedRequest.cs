bf6eb446dec935fa390e34cd29395a9693b20ede
John Coxhead <jcoxhead@hotmail.com>
2018-04-20 08:24:44 +0100

    Changes for Batch Number

---
+++ src/SFA.DAS.AssessorService.Api.Types/Models/Certificates/UpdateCertificatesBatchToIndicatePrintedRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace SFA.DAS.AssessorService.Api.Types.Models.Certificates
+{
+    public class UpdateCertificatesBatchToIndicatePrintedRequest : IRequest
+    {
+        public int BatchNumber { get; set; }
+        public List<CertificateStatus> CertificateStatuses { get; set; }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Application/Handlers/Certificates/UpdateCertificatesBatchToIndicatePrintedHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.AssessorService.Api.Types.Models.Certificates;
+using SFA.DAS.AssessorService.Application.Interfaces;
+
+namespace SFA.DAS.AssessorService.Application.Handlers.Certificates
+{
+    public class UpdateCertificatesBatchToIndicatePrintedHandler : IRequestHandler<UpdateCertificatesBatchToIndicatePrintedRequest>
+    {
+        private readonly ICertificateRepository _certificateRepository;
+
+        public UpdateCertificatesBatchToIndicatePrintedHandler(ICertificateRepository certificateRepository)
+        {
+            _certificateRepository = certificateRepository;
+        }
+
+        public async Task Handle(UpdateCertificatesBatchToIndicatePrintedRequest updateCertificatesBatchToIndicatePrintedRequest, CancellationToken cancellationToken)
+        {
+            await _certificateRepository.UpdateStatuses(updateCertificatesBatchToIndicatePrintedRequest);
+        }
+    } 
+}
