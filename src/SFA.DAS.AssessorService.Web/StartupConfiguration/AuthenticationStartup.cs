e86d13bcf6b5a05d25427a87f3db6b609d256bfe
David Gouge <davegouge@gmail.com>
2018-04-24 10:43:27 +0100

    Tidied up Auth Startup code

--- src/SFA.DAS.AssessorService.Web/StartupConfiguration/AuthenticationStartup.cs
+++ src/SFA.DAS.AssessorService.Web/StartupConfiguration/AuthenticationStartup.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using JWT.Algorithms;
-using JWT.Builder;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.WsFederation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,7 +25,6 @@ namespace SFA.DAS.AssessorService.Web.StartupConfiguration
                 {
                     options.Wtrealm = configuration.Authentication.WtRealm;
                     options.MetadataAddress = configuration.Authentication.MetadataAddress;
-                    options.Events.OnSecurityTokenValidated = OnTokenValidated;
                     options.CallbackPath = "/Account/SignedIn";
                 })
                 .AddCookie(options =>
@@ -38,24 +32,6 @@ namespace SFA.DAS.AssessorService.Web.StartupConfiguration
                     options.ReturnUrlParameter = "/Account/SignedIn";
                     options.Cookie = new CookieBuilder() {Name = ".Assessors.Cookies"};
                 });
-        }
-
-
-        private static Task OnTokenValidated(SecurityTokenValidatedContext context)
-        {
-            //var ukprn = (context.Principal.FindFirst("http://schemas.portal.com/ukprn"))?.Value;
-
-            //var jwt = new JwtBuilder().WithAlgorithm(new HMACSHA256Algorithm())
-            //    .WithSecret(_configuration.Api.TokenEncodingKey)
-            //    .Issuer("sfa.das.assessorservice")
-            //    .Audience("sfa.das.assessorservice.api")
-            //    .ExpirationTime(DateTime.Now.AddMinutes(5))
-            //    .AddClaim("ukprn", ukprn)
-            //    .Build();
-
-            //context.HttpContext.Session.SetString(ukprn, jwt);
-
-            return Task.FromResult(0);
         }
     }
 }
