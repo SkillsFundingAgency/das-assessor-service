f8273e83cb12f9d7508524ec0e45fdff54798ce6
David Gouge <davegouge@gmail.com>
2018-04-25 12:51:48 +0100

    Added logging

--- src/SFA.DAS.AssessorService.Application/Handlers/Search/SearchHandler.cs
+++ src/SFA.DAS.AssessorService.Application/Handlers/Search/SearchHandler.cs
@@ -4,8 +4,10 @@ using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.AssessorService.Api.Types.Models;
 using SFA.DAS.AssessorService.Application.Interfaces;
+using SFA.DAS.AssessorService.Application.Logging;
 using SFA.DAS.AssessorService.ExternalApis.AssessmentOrgs;
 
 namespace SFA.DAS.AssessorService.Application.Handlers.Search
@@ -16,13 +18,15 @@ namespace SFA.DAS.AssessorService.Application.Handlers.Search
         private readonly IOrganisationQueryRepository _organisationRepository;
         private readonly IIlrRepository _ilrRepository;
         private readonly ICertificateRepository _certificateRepository;
+        private readonly ILogger<SearchHandler> _logger;
 
-        public SearchHandler(IAssessmentOrgsApiClient assessmentOrgsApiClient, IOrganisationQueryRepository organisationRepository, IIlrRepository ilrRepository, ICertificateRepository certificateRepository)
+        public SearchHandler(IAssessmentOrgsApiClient assessmentOrgsApiClient, IOrganisationQueryRepository organisationRepository, IIlrRepository ilrRepository, ICertificateRepository certificateRepository, ILogger<SearchHandler> logger)
         {
             _assessmentOrgsApiClient = assessmentOrgsApiClient;
             _organisationRepository = organisationRepository;
             _ilrRepository = ilrRepository;
             _certificateRepository = certificateRepository;
+            _logger = logger;
         }
         public async Task<List<SearchResult>> Handle(SearchQuery request, CancellationToken cancellationToken)
         {
@@ -37,6 +41,8 @@ namespace SFA.DAS.AssessorService.Application.Handlers.Search
                 Uln = request.Uln,
                 StandardIds = theStandardsThisEpaoProvides.Select(s => s.StandardCode).ToList()
             });
+
+            _logger.LogInformation(ilrResults.Any() ? LoggingConstants.SearchSuccess : LoggingConstants.SearchFailure);
 
             var searchResults = Mapper.Map<List<SearchResult>>(ilrResults)
                 .MatchUpExistingCompletedStandards(request, _certificateRepository)
--- src/SFA.DAS.AssessorService.Application/Logging/LoggingConstants.cs
+++ src/SFA.DAS.AssessorService.Application/Logging/LoggingConstants.cs
@@ -5,5 +5,13 @@
         public const string SignInSuccessful = "SignIn_Successful";
         public const string SignInIncorrectRole = "SignIn_IncorrectRole";
         public const string SignInNotAnEpao = "SignIn_NotAnEpao";
+        public const string SignInEpaoDeleted = "SignIn_EpaoDeleted";
+        
+        public const string SearchSuccess = "Search_Success";
+        public const string SearchFailure = "Search_Failure";
+        
+        public const string CertificateStarted = "Certificate_Started";
+        public const string CertificateSubmitted = "Certificate_Submitted";
+        public const string CertificatePrinted = "Certificate_Printed";
     }
 }
