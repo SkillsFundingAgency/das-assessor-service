2abddeccca3ee61122ba0d77747f2b156b7ff682
David Gouge <davegouge@gmail.com>
2018-04-25 09:44:52 +0100

    Start of logging constants

---
+++ src/SFA.DAS.AssessorService.Application/Logging/LoggingConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.AssessorService.Application.Logging
+{
+    public class LoggingConstants
+    {
+        public const string SignInSuccessful = "SignIn_Successful";
+        public const string SignInIncorrectRole = "SignIn_IncorrectRole";
+        public const string SignInNotAnEpao = "SignIn_NotAnEpao";
+    }
+}
