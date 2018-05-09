470bb6db7eb926a7eed534bbe0d6578713b16fd6
John Coxhead <jcoxhead@hotmail.com>
2018-05-09 13:15:24 +0100

    adding unit tests

--- src/SFA.DAS.AssessorService.PrintFunctionProcessFlow.UnitTests/WhenSystemSendsNotification.cs
+++ src/SFA.DAS.AssessorService.PrintFunctionProcessFlow.UnitTests/WhenSystemSendsNotification.cs
@@ -1,29 +1,35 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using FizzWare.NBuilder;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.AssessorService.Api.Types.Models.Certificates;
-using SFA.DAS.AssessorService.Domain.JsonData;
 using SFA.DAS.AssessorService.EpaoImporter.Data;
 using SFA.DAS.AssessorService.EpaoImporter.Logger;
 using SFA.DAS.AssessorService.EpaoImporter.Notification;
 using SFA.DAS.AssessorService.Settings;
 using SFA.DAS.Notifications.Api.Client;
+using SFA.DAS.Notifications.Api.Types;
 
 namespace SFA.DAS.AssessorService.PrintFunctionProcessFlow.UnitTests
 {
     public class WhenSystemSendsNotification
     {
+        private Mock<INotificationsApi> _notificationsApiMock;
+
         [SetUp]
         public void Arrange()
         {
             var aggregateLoggerMock = new Mock<IAggregateLogger>();
-            var webConfigurationMock = new Mock<IWebConfiguration>();
+
             var assessorServiceApiMock = new Mock<IAssessorServiceApi>();
-            var notificationsApi = new Mock<INotificationsApi>();
+            _notificationsApiMock = new Mock<INotificationsApi>();
+
+            var webConfigurationMock = new Mock<IWebConfiguration>();
 
             var notificationService = new NotificationService(
-                notificationsApi.Object,
+                _notificationsApiMock.Object,
                 aggregateLoggerMock.Object,
                 webConfigurationMock.Object,
                 assessorServiceApiMock.Object
@@ -37,12 +43,36 @@ namespace SFA.DAS.AssessorService.PrintFunctionProcessFlow.UnitTests
 
             var coverLettersProduced = Builder<CoverLettersProduced>.CreateNew().Build();
 
+            webConfigurationMock.SetupGet(q => q.Sftp)
+                .Returns(new SftpSettings
+                {
+                    UploadDirectory = "C:\\testupoad",
+                    ProofDirectory = "C:\\proofdirectory"
+                });
+
+            webConfigurationMock.SetupGet(q => q.NotificationsApiClientConfiguration)
+                .Returns(new NotificationsApiClientConfiguration
+                {
+                    ApiBaseUrl = "http://localhost",
+                    ClientToken = "12323333333333333"
+                });
+
+            assessorServiceApiMock.Setup(q => q.GetEmailTemplate())
+                .Returns(Task.FromResult(new Domain.Entities.EMailTemplate
+                {
+                    Recipients = "jcoxhead@hotmil.com",
+                    TemplateId = "123",
+                    TemplateName = "ProcessMyItems",
+                    Id = Guid.NewGuid()
+                }));
+
             notificationService.Send(1, certificateResponses, coverLettersProduced).GetAwaiter().GetResult();
         }
 
         [Test]
         public void ThenItShouldSendNotification()
         {
+            _notificationsApiMock.Verify(q => q.SendEmail(It.IsAny<Email>()), Times.Once);
         }
     }
 }
