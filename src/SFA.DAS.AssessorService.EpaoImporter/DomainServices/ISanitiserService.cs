991551aa0514724abed8da4830acb2cdb043db1f
John Coxhead <jcoxhead@hotmail.com>
2018-05-09 11:43:52 +0100

    Adding sanitizing code

---
+++ src/SFA.DAS.AssessorService.EpaoImporter/DomainServices/ISanitiserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.AssessorService.Api.Types.Models.Certificates;
+
+namespace SFA.DAS.AssessorService.EpaoImporter.DomainServices
+{
+    public interface ISanitiserService
+    {
+        List<CertificateResponse> Sanitize(List<CertificateResponse> certificateResponses);
+    }
+}
---
+++ src/SFA.DAS.AssessorService.EpaoImporter/DomainServices/SanitiserService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using SFA.DAS.AssessorService.Api.Types.Models.Certificates;
+using SFA.DAS.AssessorService.EpaoImporter.Logger;
+
+namespace SFA.DAS.AssessorService.EpaoImporter.DomainServices
+{
+    public class SanitiserService : ISanitiserService
+    {
+        private readonly IAggregateLogger _aggregateLogger;
+
+        public SanitiserService(IAggregateLogger aggregateLogger)
+        {
+            _aggregateLogger = aggregateLogger;
+        }
+
+        public List<CertificateResponse> Sanitize(List<CertificateResponse> certificateResponses)
+        {
+            var sanitizedCertificateResponses = new List<CertificateResponse>();
+
+            foreach (var certificateResponse in certificateResponses)
+            {
+                _aggregateLogger.LogInfo("Sanitizing Certificates ...");
+
+                var certificateData = certificateResponse.CertificateData;
+
+                var certificateContactDetails = "";
+
+                if (!string.IsNullOrEmpty(certificateData.ContactAddLine1))
+                {
+                    certificateContactDetails += certificateData.ContactAddLine1 + System.Environment.NewLine;
+                }
+
+                if (!string.IsNullOrEmpty(certificateData.ContactPostCode))
+                {
+                    certificateContactDetails += certificateData.ContactPostCode + System.Environment.NewLine;
+                }
+
+                if (string.IsNullOrEmpty(certificateContactDetails))
+                {
+                    if (!string.IsNullOrEmpty(certificateData.LearnerGivenNames)
+                        && !string.IsNullOrEmpty(certificateData.LearnerFamilyName))
+                    {
+                        _aggregateLogger.LogInfo(
+                            $"Unprintable Certificate -> Given Names = {certificateData.LearnerGivenNames} Familly Name = {certificateData.LearnerFamilyName} - Cannot be processed");
+                    }
+                    else
+                    {
+                        _aggregateLogger.LogInfo($"Unprintable Certificate - Cannot be processed");
+                    }
+                }
+                else
+                {
+                    sanitizedCertificateResponses.Add(certificateResponse);
+                }
+            }
+            return sanitizedCertificateResponses;
+        }
+    }
+}
