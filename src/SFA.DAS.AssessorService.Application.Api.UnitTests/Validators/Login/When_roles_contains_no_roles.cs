2e3306a89fe5e47109aa0830ab6b36905bf2d015
David Gouge <davegouge@gmail.com>
2018-05-14 08:30:48 +0100

    Removed validator requirement for roles to have >= 1

---
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/Validators/Login/When_roles_contains_no_roles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.Extensions.Localization;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.AssessorService.Api.Types.Models;
+using SFA.DAS.AssessorService.Application.Api.Validators;
+
+namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Validators.Login
+{
+    [TestFixture]
+    public class When_roles_contains_no_roles
+    {
+        [Test]
+        public void Then_validation_passes()
+        {
+            var validator = new LoginRequestValidator(new Mock<IStringLocalizer<LoginRequestValidator>>().Object);
+
+            var result = validator.Validate(new LoginRequest()
+            {
+                DisplayName = "Dave",
+                Email = "E@mail.com",
+                UkPrn = 1234,
+                Username = "username",
+                Roles = new List<string>()
+            });
+
+            result.IsValid.Should().BeTrue();
+        }
+    }
+}
