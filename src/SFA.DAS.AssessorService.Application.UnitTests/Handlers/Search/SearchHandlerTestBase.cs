902f5425574c286a1081d8e9b2dff153496eab39
David Gouge <davegouge@gmail.com>
2018-05-15 12:10:34 +0100

    Improved defensiveness of standardcode parsin

--- src/SFA.DAS.AssessorService.Application.UnitTests/Handlers/Search/SearchHandlerTestBase.cs
+++ src/SFA.DAS.AssessorService.Application.UnitTests/Handlers/Search/SearchHandlerTestBase.cs
@@ -14,33 +14,37 @@ namespace SFA.DAS.AssessorService.Application.UnitTests.Handlers.Search
         protected SearchHandler SearchHandler;
         protected Mock<ICertificateRepository> CertificateRepository;
         protected Mock<IIlrRepository> IlrRepository;
+        protected Mock<IAssessmentOrgsApiClient> AssessmentOrgsApiClient;
 
         public void Setup()
         {
             MappingBootstrapper.Initialize();
 
-            var assessmentOrgsApiClient = new Mock<IAssessmentOrgsApiClient>();
-            assessmentOrgsApiClient.Setup(c => c.FindAllStandardsByOrganisationIdAsync("EPA001"))
+            AssessmentOrgsApiClient = new Mock<IAssessmentOrgsApiClient>();
+            AssessmentOrgsApiClient.Setup(c => c.FindAllStandardsByOrganisationIdAsync("EPA001"))
                 .ReturnsAsync(new List<StandardOrganisationSummary>
                 {
                     new StandardOrganisationSummary {StandardCode = "12"},
                     new StandardOrganisationSummary {StandardCode = "13"}
                 });
-            assessmentOrgsApiClient.Setup(c => c.GetStandard(12))
+            AssessmentOrgsApiClient.Setup(c => c.GetStandard(12))
                 .ReturnsAsync(new Standard {Title = "Standard Name 12", Level = 2});
-            assessmentOrgsApiClient.Setup(c => c.GetStandard(13))
+            AssessmentOrgsApiClient.Setup(c => c.GetStandard(13))
                 .ReturnsAsync(new Standard {Title = "Standard Name 13", Level = 3});
 
             var orgQueryRepo = new Mock<IOrganisationQueryRepository>();
             orgQueryRepo.Setup(r => r.GetByUkPrn(12345))
                 .ReturnsAsync(new Organisation() {EndPointAssessorOrganisationId = "EPA001"});
+            
+            orgQueryRepo.Setup(r => r.GetByUkPrn(99999))
+                .ReturnsAsync(new Organisation() {EndPointAssessorOrganisationId = "EPA0050"});
 
             IlrRepository = new Mock<IIlrRepository>();
             
 
             CertificateRepository = new Mock<ICertificateRepository>();
 
-            SearchHandler = new SearchHandler(assessmentOrgsApiClient.Object, orgQueryRepo.Object, IlrRepository.Object,
+            SearchHandler = new SearchHandler(AssessmentOrgsApiClient.Object, orgQueryRepo.Object, IlrRepository.Object,
                 CertificateRepository.Object, new Mock<ILogger<SearchHandler>>().Object);
         }
     }
