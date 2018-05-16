f71a542950671340202bdf0cbd64c7dda343e61d
davidgouge <davegouge@gmail.com>
2018-05-16 13:19:28 +0100

    Change to logic of learner search

--- src/SFA.DAS.AssessorService.Application/Interfaces/SearchRequest.cs
+++ src/SFA.DAS.AssessorService.Application/Interfaces/SearchRequest.cs
@@ -6,6 +6,5 @@ namespace SFA.DAS.AssessorService.Application.Interfaces
     {
         public long Uln { get; set; }
         public string FamilyName { get; set; }
-        public List<int> StandardIds { get; set; }
     }
 }
--- src/SFA.DAS.AssessorService.Data/IlrRepository.cs
+++ src/SFA.DAS.AssessorService.Data/IlrRepository.cs
@@ -17,27 +17,23 @@ namespace SFA.DAS.AssessorService.Data
             _context = context;
         }
 
-        public async Task<IEnumerable<Ilr>> Search(SearchRequest searchRequest)
+        public async Task<IEnumerable<Ilr>> SearchForLearner(SearchRequest searchRequest)
         {
             var learnerRecords = await _context.Ilrs.Where(r =>
                 string.Equals(r.FamilyName.Trim(), searchRequest.FamilyName.Trim(), StringComparison.CurrentCultureIgnoreCase)
                 && r.Uln == searchRequest.Uln).ToListAsync();
-                
-            learnerRecords = learnerRecords.Where(r => searchRequest.StandardIds.Contains(r.StdCode)).ToList();
-
+               
             var response = learnerRecords;
 
             return response;
         }
 
-        public async Task<IEnumerable<Ilr>> SearchLike(SearchRequest searchRequest)
+        public async Task<IEnumerable<Ilr>> SearchForLearnerLike(SearchRequest searchRequest)
         {
             var learnerRecords = await _context.Ilrs.Where(r => 
                 EF.Functions.Like(r.FamilyName, searchRequest.FamilyName)
                 && r.Uln == searchRequest.Uln).ToListAsync();
 
-            learnerRecords = learnerRecords.Where(r => searchRequest.StandardIds.Contains(r.StdCode)).ToList();
-            
             var response = learnerRecords;
 
             return response;
