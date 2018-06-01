427ea3703e62f3f53932ce229f57c8a5c6bf7d0a
John Coxhead <jcoxhead@hotmail.com>
2018-06-01 09:00:40 +0100

    unit tests

--- src/SFA.DAS.AssessorService.EpaoImporter/DomainServices/IfaCertificateService.cs
+++ src/SFA.DAS.AssessorService.EpaoImporter/DomainServices/IfaCertificateService.cs
@@ -178,8 +178,18 @@ namespace SFA.DAS.AssessorService.EpaoImporter.DomainServices
                 if (certificateData.AchievementDate.HasValue)
                     worksheet.Cells[row, 1].Value = certificateData.AchievementDate.Value.ToString("dd MMMM, yyyy");
 
+                var learnerName = $"{certificateData.LearnerGivenNames} {certificateData.LearnerFamilyName}";
                 if (certificateData.FullName != null)
+                {
                     worksheet.Cells[row, 2].Value = certificateData.FullName.ToUpper();
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(learnerName))
+                    {
+                        worksheet.Cells[row, 2].Value = learnerName.ToUpper();
+                    }
+                }
 
                 if (certificateData.StandardName != null)
                     worksheet.Cells[row, 3].Value = certificateData.StandardName.ToUpper();
