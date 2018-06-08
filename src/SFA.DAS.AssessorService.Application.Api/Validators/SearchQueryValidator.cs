085a33270c943e49dc600fbb13b6dfbdbb2f1868
David Gouge <davegouge@gmail.com>
2018-06-08 09:35:16 +0100

    The one where 9999999999 as a uln search is fixed

--- src/SFA.DAS.AssessorService.Application.Api/Validators/SearchQueryValidator.cs
+++ src/SFA.DAS.AssessorService.Application.Api/Validators/SearchQueryValidator.cs
@@ -12,7 +12,7 @@ namespace SFA.DAS.AssessorService.Application.Api.Validators
             RuleFor(query => query.Surname).NotEmpty().WithMessage(localizer[ResourceMessageName.MustHaveSurname]);
             RuleFor(query => query.UkPrn).InclusiveBetween(10000000, 99999999)
                 .WithMessage(localizer[ResourceMessageName.InvalidUkprn].Value);
-            RuleFor(query => query.Uln).LessThan(9999999999)
+            RuleFor(query => query.Uln).LessThanOrEqualTo(9999999999)
                 .WithMessage(localizer[ResourceMessageName.InvalidUln].Value);
             RuleFor(query => query.Username).NotEmpty()
                 .WithMessage(localizer[ResourceMessageName.MustHaveUsername].Value);
