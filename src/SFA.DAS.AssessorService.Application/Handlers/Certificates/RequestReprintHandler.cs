29b5333ff8ae110c1a8de8da239ba78b7b269aad
John Coxhead <jcoxhead@hotmail.com>
2018-06-11 10:00:26 +0100

    adding cuntionalitry for reprint

---
+++ src/SFA.DAS.AssessorService.Application/Handlers/Certificates/RequestReprintHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.AssessorService.Api.Types.Models.Certificates;
+using SFA.DAS.AssessorService.Application.Exceptions;
+using SFA.DAS.AssessorService.Application.Interfaces;
+using NotFound = SFA.DAS.AssessorService.Domain.Exceptions.NotFound;
+
+namespace SFA.DAS.AssessorService.Application.Handlers.Certificates
+{
+    public class RequestReprintHandler : IRequestHandler<CertificateReprintRequest>
+    {
+        private readonly ICertificateRepository _certificateRepository;
+
+        public RequestReprintHandler(ICertificateRepository certificateRepository)
+        {
+            _certificateRepository = certificateRepository;
+        }
+
+        public async Task Handle(CertificateReprintRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _certificateRepository.RequestReprint(request.Username, request.CertificateReference, request.LastName, request.AchievementDate);
+            if (result == false)
+                throw new NotFound();
+        }
+    }
+}
