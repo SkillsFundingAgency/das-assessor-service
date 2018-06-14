012c58234f24e3bc0ea913adf702a5a106104af2
David Gouge <davegouge@gmail.com>
2018-06-14 10:04:24 +0100

    Back button now smart enough to go back to check page if that was where it came from

--- src/SFA.DAS.AssessorService.Web/ViewModels/Certificate/ICertificateViewModel.cs
+++ src/SFA.DAS.AssessorService.Web/ViewModels/Certificate/ICertificateViewModel.cs
@@ -9,6 +9,7 @@ namespace SFA.DAS.AssessorService.Web.ViewModels.Certificate
         string FamilyName { get; set; }
         string GivenNames { get; set; }
         void FromCertificate(Domain.Entities.Certificate cert);
+        bool BackToCheckPage { get; set; }
         Domain.Entities.Certificate GetCertificateFromViewModel(Domain.Entities.Certificate certificate, CertificateData certData);
     }
 }
