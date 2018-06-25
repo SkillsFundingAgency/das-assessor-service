7225461c39265b898d324661865d986a5510a6e1
David Gouge <davegouge@gmail.com>
2018-06-25 08:13:03 +0100

    Search now filters out spaces in Surnames (both in search and data)

--- src/SFA.DAS.AssessorService.Data/IlrRepository.cs
+++ src/SFA.DAS.AssessorService.Data/IlrRepository.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using SFA.DAS.AssessorService.Application.Interfaces;
 using SFA.DAS.AssessorService.Domain.Entities;
@@ -19,24 +18,7 @@ namespace SFA.DAS.AssessorService.Data
 
         public async Task<IEnumerable<Ilr>> SearchForLearner(SearchRequest searchRequest)
         {
-            var learnerRecords = await _context.Ilrs.Where(r =>
-                string.Equals(r.FamilyName.Trim(), searchRequest.FamilyName.Trim(), StringComparison.CurrentCultureIgnoreCase)
-                && r.Uln == searchRequest.Uln).ToListAsync();
-               
-            var response = learnerRecords;
-
-            return response;
-        }
-
-        public async Task<IEnumerable<Ilr>> SearchForLearnerLike(SearchRequest searchRequest)
-        {
-            var learnerRecords = await _context.Ilrs.Where(r => 
-                EF.Functions.Like(r.FamilyName, searchRequest.FamilyName)
-                && r.Uln == searchRequest.Uln).ToListAsync();
-
-            var response = learnerRecords;
-
-            return response;
+            return await _context.Ilrs.Where(r => r.Uln == searchRequest.Uln).ToListAsync();
         }
 
         public async Task<Ilr> Get(long uln, int standardCode)
