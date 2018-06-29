9d56c3efdd6fbc81bc4c23294d02524ec762539e
David Gouge <davegouge@gmail.com>
2018-06-29 09:07:29 +0100

    Added CompletionStatus to LearnerImport. Table is now deleted at start instead of end

--- LoadlrData.sql
+++ LoadlrData.sql
@@ -20,7 +20,7 @@ CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Baldy N3rd Face';
 ---- Create a database scoped credential with Azure storage account key as the secret.
 CREATE DATABASE SCOPED CREDENTIAL BlobCredential 
 WITH IDENTITY = 'SHARED ACCESS SIGNATURE', 
-SECRET = '';
+SECRET = 'st=2018-06-29T07%3A46%3A11Z&se=2018-06-30T07%3A46%3A11Z&sp=rwdl&sv=2017-04-17&sr=c&sig=5GtueHBsul4XXDb2EsBWnjaSgWO%2BwsEjzxqMNBhbrRI%3D';
 
 ---- Create an external data source with CREDENTIAL option.
 CREATE EXTERNAL DATA SOURCE BlobStorage WITH (
@@ -28,6 +28,14 @@ CREATE EXTERNAL DATA SOURCE BlobStorage WITH (
     LOCATION = 'https://esfatempstorage.blob.core.windows.net/import',
     CREDENTIAL = BlobCredential
 );
+
+IF (EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo' 
+                 AND  TABLE_NAME = 'LearnerImport'))
+BEGIN
+    DROP TABLE [LearnerImport]
+END
 
 CREATE TABLE [dbo].[LearnerImport](
 	[Uln] [bigint] NULL,
@@ -39,7 +47,8 @@ CREATE TABLE [dbo].[LearnerImport](
 	[LearnStartDate] [nvarchar](50) NULL,
 	[EpaOrgId] [nvarchar](50) NULL,
 	[FundingModel] [int] NULL,
-	[ApprenticeshipId] [bigint] NULL
+	[ApprenticeshipId] [bigint] NULL,
+	[CompletionStatus] [int] NULL
 )
 GO
 
@@ -54,8 +63,6 @@ SELECT
 	FROM LearnerImport li
 	LEFT OUTER JOIN Ilrs i ON i.Uln = li.ULN AND i.LearnRefNumber = li.LearnRefNumber AND i.StdCode = li.StdCode AND i.UkPrn = li.UKPRN AND i.LearnStartDate = li.LearnStartDate
 	WHERE i.Uln IS NULL
-
-DROP TABLE LearnerImport
 
 UPDATE Ilrs SET FamilyName = REPLACE(REPLACE(REPLACE(FamilyName, '`',''''),'’',''''),'–','-'), GivenNames = REPLACE(REPLACE(REPLACE(GivenNames, '`',''''),'’',''''),'–','-')
 
