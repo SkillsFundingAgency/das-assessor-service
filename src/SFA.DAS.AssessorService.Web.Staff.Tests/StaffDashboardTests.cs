91cf1d9ffaa89028833c7650da64228e3efea5dd
David Gouge <davegouge@gmail.com>
2018-07-02 14:37:36 +0100

    Removed test testing

--- src/SFA.DAS.AssessorService.Web.Staff.Tests/StaffDashboardTests.cs
+++ src/SFA.DAS.AssessorService.Web.Staff.Tests/StaffDashboardTests.cs
@@ -8,30 +8,30 @@ using NUnit.Framework;
 
 namespace SFA.DAS.AssessorService.Web.Staff.Tests
 {
-    [TestFixture]
-    public class StaffDashboardTests 
-    {
-        private HttpClient _httpClient;
+    //[TestFixture]
+    //public class StaffDashboardTests 
+    //{
+    //    private HttpClient _httpClient;
 
-        [SetUp]
-        public void Setup()
-        {
-            _httpClient = new WebApplicationFactory<Startup>().CreateClient(
-                new WebApplicationFactoryClientOptions() {BaseAddress = new Uri("https://localhost:44347")});
-        }
+    //    [SetUp]
+    //    public void Setup()
+    //    {
+    //        _httpClient = new WebApplicationFactory<Startup>().CreateClient(
+    //            new WebApplicationFactoryClientOptions() {BaseAddress = new Uri("https://localhost:44347")});
+    //    }
 
-        [Test]
-        public async Task HomeReturns200()
-        {
-            var response = await _httpClient.GetAsync("/");
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-        }
+    //    [Test]
+    //    public async Task HomeReturns200()
+    //    {
+    //        var response = await _httpClient.GetAsync("/");
+    //        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    //    }
 
-        [Test]
-        public async Task AccountSignInReturnsRedirect()
-        {
-            var response = await _httpClient.GetAsync("/Dashboard/Index");
-            Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
-        }
-    }
+    //    [Test]
+    //    public async Task AccountSignInReturnsRedirect()
+    //    {
+    //        var response = await _httpClient.GetAsync("/Dashboard/Index");
+    //        Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+    //    }
+    //}
 }
