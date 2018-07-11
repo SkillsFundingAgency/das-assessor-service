dc1518a90ea579afc81b73ec565c0fe4a68a9f25
markcain <mark.cain@digital.education.gov.uk>
2018-07-11 14:56:10 +0100

    ON-682 setting up repository

--- src/SFA.DAS.AssessorService.Data/RegisterRepository.cs
+++ src/SFA.DAS.AssessorService.Data/RegisterRepository.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using SFA.DAS.AssessorService.Application.Interfaces;
+using SFA.DAS.AssessorService.Domain.Entities.ao;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SFA.DAS.AssessorService.Data
 {
-    class RegisterRepository
+    public class RegisterRepository : IRegisterRepository
     {
+        private readonly AssessorDbContext _assessorDbContext;
+
+        public RegisterRepository(AssessorDbContext assessorDbContext)
+        {
+            _assessorDbContext = assessorDbContext;
+        }
+
+        public Task<List<EpaOrganisationType>> GetOrganisationTypes()
+        {
+            var connectionString = _assessorDbContext.Database.GetDbConnection().ConnectionString;
+
+            using (var connection = new SqlConnection(connectionString))
+                {
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+                    //connection.Query()
+                    var orgTypes = connection.Query<EpaOrganisationType>("select * from [ao].[OrganisationType]").ToList();
+
+                    connection.Close();
+
+                    return orgTypes;
+                }
+        }
     }
 }
