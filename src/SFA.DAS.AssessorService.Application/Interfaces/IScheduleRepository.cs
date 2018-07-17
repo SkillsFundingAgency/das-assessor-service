3736c11ff2bff734d629b46f9d301aa554ac28de
David Gouge <davegouge@gmail.com>
2018-07-17 08:49:38 +0100

    Added a new scheduling system

---
+++ src/SFA.DAS.AssessorService.Application/Interfaces/IScheduleRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.AssessorService.Domain.Entities;
+
+namespace SFA.DAS.AssessorService.Application.Interfaces
+{
+    public interface IScheduleRepository
+    {
+        Task<ScheduleRun> GetNextScheduleRun(int scheduleType);
+        Task CompleteScheduleRun(Guid scheduleRunId);
+        Task QueueImmediateRun(int scheduleType);
+        Task SetScheduleRun(ScheduleRun scheduleRun);
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Domain/Entities/ScheduleRun.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SFA.DAS.AssessorService.Domain.Entities
+{
+    public class ScheduleRun
+    {
+        public Guid Id { get; set; }
+        public DateTime RunTime { get; set; }
+        public bool IsComplete { get; set; }
+        public long? Interval { get; set; }
+        public bool IsRecurring { get; set; }
+        public int ScheduleType { get; set; }
+    }
+}
