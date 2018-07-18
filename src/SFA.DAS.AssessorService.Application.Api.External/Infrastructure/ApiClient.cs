c64890241726ddb911d90754b22759c1b39b1712
Martin Hornsby <martin-hornsby@ntlworld.com>
2018-07-18 11:34:47 +0100

    Initial Vesion

---
+++ src/SFA.DAS.AssessorService.Application.Api.External/Infrastructure/ApiClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SFA.DAS.AssessorService.Application.Api.External.Infrastructure
+{
+    public class ApiClient
+    {
+        private readonly HttpClient _client;
+        private readonly ILogger<ApiClient> _logger;
+
+        public ApiClient(HttpClient client, ILogger<ApiClient> logger)
+        {
+            _client = client;
+            _logger = logger;
+        }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Application.Api.External/Startup.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SFA.DAS.AssessorService.Application.Api.External.Infrastructure;
+using SFA.DAS.AssessorService.Settings;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SFA.DAS.AssessorService.Application.Api.External
+{
+    //TODO: This all needs configuring correctly as I don't know what I'm supposed to put in here
+    public class Startup
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly ILogger<Startup> _logger;
+        private const string ServiceName = "SFA.DAS.AssessorService";
+        private const string Version = "1.0";
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env, ILogger<Startup> logger)
+        {
+            _env = env;
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebConfiguration ApplicationConfiguration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ApplicationConfiguration = ConfigurationService.GetConfig(Configuration["EnvironmentName"], Configuration["ConfigurationStorageConnectionString"], Version, ServiceName).Result;
+
+            services.AddHttpClient<ApiClient>("ApiClient", config =>
+            {
+                config.BaseAddress = new Uri(ApplicationConfiguration.ClientApiAuthentication.ApiBaseAddress);
+                config.DefaultRequestHeaders.Add("Accept", "Application/json");
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "SFA.DAS.AssessorService.Application.Api.External", Version = "v1" });
+
+                if (_env.IsDevelopment())
+                {
+                    var basePath = AppContext.BaseDirectory;
+                    var xmlPath = Path.Combine(basePath, "SFA.DAS.AssessorService.Application.Api.External.xml");
+                    c.IncludeXmlComments(xmlPath);
+                }
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            try
+            {
+                //MappingStartup.AddMappings();
+
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+                else
+                {
+                    app.UseHsts();
+                }
+
+                app.UseSwagger()
+                    .UseSwaggerUI(c =>
+                    {
+                        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SFA.DAS.AssessorService.Application.Api.External v1");
+                    })
+                    .UseAuthentication();
+
+                //TODO: put this back, but it's a bugger when coding.
+                //app.UseMiddleware(typeof(ErrorHandlingMiddleware));
+                app.UseHttpsRedirection();
+                app.UseMvc();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error during Startup Configure");
+                throw;
+            }
+        }
+    }
+}
