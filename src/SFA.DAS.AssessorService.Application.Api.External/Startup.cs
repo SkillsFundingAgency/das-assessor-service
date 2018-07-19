cf4db55d565e4a7f7c7a8e59d6a1c8f72d102feb
David Gouge <davegouge@gmail.com>
2018-07-19 08:04:46 +0100

    Small startup changes

--- src/SFA.DAS.AssessorService.Application.Api.External/Startup.cs
+++ src/SFA.DAS.AssessorService.Application.Api.External/Startup.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using SFA.DAS.AssessorService.Application.Api.External.Infrastructure;
 using SFA.DAS.AssessorService.Settings;
 using Swashbuckle.AspNetCore.Swagger;
@@ -84,8 +79,6 @@ namespace SFA.DAS.AssessorService.Application.Api.External
                     })
                     .UseAuthentication();
 
-                //TODO: put this back, but it's a bugger when coding.
-                //app.UseMiddleware(typeof(ErrorHandlingMiddleware));
                 app.UseHttpsRedirection();
                 app.UseMvc();
             }
