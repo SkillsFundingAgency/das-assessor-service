46efb15fb370b10cc26c30dc5e4c80e8b5b10ed6
David Gouge <davegouge@gmail.com>
2018-07-30 13:45:54 +0100

    Delete tmp tables if present

--- src/SFA.DAS.AssessorService.Database/Script.PreDeployment1.sql
+++ src/SFA.DAS.AssessorService.Database/Script.PreDeployment1.sql
@@ -9,6 +9,24 @@
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+
+IF (EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo' 
+                 AND  TABLE_NAME = 'TmpContacts'))
+BEGIN
+    DROP TABLE TmpContacts
+END
+
+IF (EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo' 
+                 AND  TABLE_NAME = 'TmpOrganisations'))
+BEGIN
+    DROP TABLE TmpOrganisations
+END
+
+
 SELECT * INTO TmpContacts FROM Contacts
 SELECT * INTO TmpOrganisations FROM Organisations
 
