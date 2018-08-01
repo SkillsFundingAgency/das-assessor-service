765cc01bd3df4abcc726840f639ca36e7196c0c2
John Coxhead <jcoxhead@hotmail.com>
2018-08-01 13:23:13 +0100

    Added date acheivement for certificate ammend in staff UI

---
+++ src/SFA.DAS.AssessorService.Web.Staff/Controllers/CertificateDateController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AssessorService.Application.Api.Client.Clients;
+using SFA.DAS.AssessorService.Domain.Consts;
+using SFA.DAS.AssessorService.Web.Staff.Infrastructure;
+using SFA.DAS.AssessorService.Web.Staff.Validators;
+using SFA.DAS.AssessorService.Web.Staff.ViewModels;
+
+namespace SFA.DAS.AssessorService.Web.Staff.Controllers
+{
+    [Authorize]
+    [Route("certificate/date")]
+    public class CertificateDateController : CertificateBaseController
+    {
+        private readonly CertificateDateViewModelValidator _validator;
+
+        public CertificateDateController(ILogger<CertificateAmmendController> logger,
+            IHttpContextAccessor contextAccessor,
+            ApiClient apiClient) : base(logger, contextAccessor, apiClient)
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Date(Guid certificateid)
+        {
+            return await LoadViewModel<CertificateDateViewModel>(certificateid, "~/Views/CertificateAmmend/Date.cshtml");
+        }
+        
+        [HttpPost(Name = "Date")]
+        public async Task<IActionResult> Date(CertificateDateViewModel vm)
+        {
+            var result = _validator.Validate(vm);
+
+            if (!result.IsValid && result.Errors.Any(e => e.Severity == Severity.Warning))
+            {
+                vm.WarningShown = "true";
+                return View("~/Views/CertificateAmmend/Date.cshtml", vm);
+            }
+
+
+            var actionResult = await SaveViewModel(vm, 
+                returnToIfModelNotValid: "~/Views/Certificate/Date.cshtml",
+                nextAction: RedirectToAction("Address", "CertificateAddress"), action: CertificateActions.Date);
+
+
+            return actionResult;
+        }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Web.Staff/ViewModels/CertificateDateViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+using SFA.DAS.AssessorService.Domain.JsonData;
+
+namespace SFA.DAS.AssessorService.Web.Staff.ViewModels
+{
+    public class CertificateDateViewModel : CertificateBaseViewModel, ICertificateViewModel
+    {
+        public DateTime Date { get; set; }
+        public string Day { get; set; }
+        public string Month { get; set; }
+        public string Year { get; set; }
+        public DateTime StartDate { get; set; }
+        public string WarningShown { get; set; }
+
+        public void FromCertificate(Domain.Entities.Certificate cert)
+        {
+            BaseFromCertificate(cert);
+            Day = CertificateData.AchievementDate?.Day.ToString();
+            Month = CertificateData.AchievementDate?.Month.ToString();
+            Year = CertificateData.AchievementDate?.Year.ToString();
+            StartDate = CertificateData.LearningStartDate;
+            WarningShown = "false";
+        }
+
+        public Domain.Entities.Certificate GetCertificateFromViewModel(Domain.Entities.Certificate certificate, CertificateData data)
+        {
+            data.AchievementDate = new DateTime(int.Parse(Year), int.Parse(Month), int.Parse(Day));
+
+            certificate.CertificateData = JsonConvert.SerializeObject(data);
+
+            return certificate;
+        }
+    }
+}
