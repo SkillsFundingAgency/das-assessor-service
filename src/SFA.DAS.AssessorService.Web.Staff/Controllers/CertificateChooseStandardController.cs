f2abbd3b2f709fbed7bd1f9933e0ee4585730e0f
John Coxhead <jcoxhead@hotmail.com>
2018-08-01 14:33:53 +0100

    adding start of select standard

---
+++ src/SFA.DAS.AssessorService.Web.Staff/Controllers/CertificateChooseStandardController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AssessorService.Application.Api.Client.Clients;
+using SFA.DAS.AssessorService.Domain.Consts;
+using SFA.DAS.AssessorService.Web.Staff.Infrastructure;
+using SFA.DAS.AssessorService.Web.Staff.ViewModels;
+
+namespace SFA.DAS.AssessorService.Web.Staff.Controllers
+{
+    [Authorize]
+    [Route("certificate/grade")]
+    public class CertificateChooseStandardController : CertificateBaseController
+    {
+        public CertificateChooseStandardController(ILogger<CertificateAmmendController> logger,
+            IHttpContextAccessor contextAccessor,
+            ApiClient apiClient)
+            : base(logger, contextAccessor, apiClient)
+        { }
+
+        [HttpGet]
+        public async Task<IActionResult> ChooseStandard(Guid certificateid)
+        {
+            return await LoadViewModel<CertificateGradeViewModel>(certificateid, "~/Views/CertificateAmmend/Grade.cshtml");
+        }
+
+        [HttpPost(Name = "ChooseStandard")]
+        public async Task<IActionResult> ChooseStandard(CertificateGradeViewModel vm)
+        {
+            return await SaveViewModel(vm,
+                returnToIfModelNotValid: "~/Views/CertificateAmmend/Grade.cshtml",
+                nextAction: RedirectToAction("Check", "CertificateAmmend"), action: CertificateActions.Grade);
+        }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Web.Staff/ViewModels/ChooseStandardViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.AssessorService.Web.Staff.ViewModels
+{   
+    public class ChooseStandardViewModel
+    {
+        public string SelectedStandardCode { get; set; }
+        //public IEnumerable<ResultViewModel> SearchResults { get; set; }
+    }
+
+    public class SelectedStandardViewModel
+    {
+        public string GivenNames { get; set; }
+        public string FamilyName { get; set; }
+        public string Uln { get; set; }
+        public string Standard { get; set; }
+        public string StdCode { get; set; } 
+        public string OverallGrade { get; set; }
+        public string CertificateReference { get; set; }
+        public string Level { get; set; }
+        public string SubmittedAt { get; set; }
+        public string SubmittedBy { get; set; }
+        public string LearnerStartDate { get; set; }
+        public string AchievementDate { get; set; }
+        
+        public bool ShowExtraInfo { get; set; }
+    }
+}
