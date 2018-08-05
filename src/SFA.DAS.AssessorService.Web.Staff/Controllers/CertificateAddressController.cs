03f675311d52361f3e647af9292381bd6508b5da
John Coxhead <jcoxhead@hotmail.com>
2018-08-05 11:15:35 +0100

    Adding address for staff UI

---
+++ src/SFA.DAS.AssessorService.Web.Staff/Controllers/CertificateAddressController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AssessorService.Domain.Consts;
+using SFA.DAS.AssessorService.Web.Staff.Infrastructure;
+using SFA.DAS.AssessorService.Web.Staff.ViewModels;
+
+namespace SFA.DAS.AssessorService.Web.Staff.Controllers
+{   
+    public class CertificateAddressController : CertificateBaseController
+    {
+        public CertificateAddressController(ILogger<CertificateAmmendController> logger,
+            IHttpContextAccessor contextAccessor,
+            ApiClient apiClient)
+            : base(logger, contextAccessor, apiClient)
+        { }
+
+        [HttpGet]
+        public async Task<IActionResult> Address(Guid certificateid)
+        {
+            return await LoadViewModel<CertificateAddressViewModel>(certificateid, "~/Views/CertificateAmmend/Address.cshtml");
+        }
+
+        [HttpPost(Name = "Grade")]
+        public async Task<IActionResult> Address(CertificateAddressViewModel vm)
+        {
+            return await SaveViewModel(vm,
+                returnToIfModelNotValid: "~/Views/CertificateAmmend/Address.cshtml",
+                nextAction: RedirectToAction("Check", "CertificateAmmend"), action: CertificateActions.Grade);
+        }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Web.Staff/ViewModels/CertificateAddressViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Newtonsoft.Json;
+using SFA.DAS.AssessorService.Domain.JsonData;
+
+namespace SFA.DAS.AssessorService.Web.Staff.ViewModels
+{
+    public class CertificateAddressViewModel : CertificateBaseViewModel, ICertificateViewModel
+    {
+        public string Employer { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+
+
+
+        public void FromCertificate(Domain.Entities.Certificate cert)
+        {
+            BaseFromCertificate(cert);
+            Employer = CertificateData.ContactOrganisation;
+            AddressLine1 = CertificateData.ContactAddLine1;
+            AddressLine2 = CertificateData.ContactAddLine2;
+            AddressLine3 = CertificateData.ContactAddLine3;
+            City = CertificateData.ContactAddLine4;
+            Postcode = CertificateData.ContactPostCode;
+        }
+        
+        public Domain.Entities.Certificate GetCertificateFromViewModel(Domain.Entities.Certificate certificate, CertificateData data)
+        {
+            data.ContactOrganisation = Employer;
+            data.ContactAddLine1 = AddressLine1;
+            data.ContactAddLine2 = AddressLine2;
+            data.ContactAddLine3 = AddressLine3;
+            data.ContactAddLine4 = City;
+            data.ContactPostCode = Postcode;
+
+            certificate.CertificateData = JsonConvert.SerializeObject(data);
+
+            return certificate;
+        }
+
+        //public CertificateAddressViewModel CopyFromCertificateAddress(CertificateAddress certificatePreviousAddress)
+        //{
+        //    this.AddressLine1 = certificatePreviousAddress.AddressLine1;
+        //    this.AddressLine2 = certificatePreviousAddress.AddressLine2;
+        //    this.AddressLine3 = certificatePreviousAddress.AddressLine3;
+        //    this.City = certificatePreviousAddress.City;
+        //    this.Postcode = certificatePreviousAddress.PostCode;
+
+        //    return this;
+        //}
+    }
+}
