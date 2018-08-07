b06ea682499a619038331774b55a8e493ec34449
John Coxhead <jcoxhead@hotmail.com>
2018-08-07 08:29:51 +0100

    Adding apprentice details validator

---
+++ src/SFA.DAS.AssessorService.Web.Staff/Validators/CertificateApprenticeDetailsViewModelValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SFA.DAS.AssessorService.Web.Staff.ViewModels;
+
+namespace SFA.DAS.AssessorService.Web.Staff.Validators
+{
+    public class CertificateApprenticeDetailsViewModelValidator : AbstractValidator<CertificateApprenticeDetailsViewModel>
+    {
+        public CertificateApprenticeDetailsViewModelValidator()
+        {
+            RuleFor(vm => vm.FamilyName).NotEmpty().WithMessage("Family Name must be defined");
+        }
+    }
+}
