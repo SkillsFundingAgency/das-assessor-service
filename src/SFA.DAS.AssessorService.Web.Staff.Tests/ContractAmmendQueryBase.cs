0f58aa6295d2892e0918ff402d91a37796315de2
John Coxhead <jcoxhead@hotmail.com>
2018-08-09 10:59:45 +0100

    Refactor WhenUserLoadsAmmendGradePage

---
+++ src/SFA.DAS.AssessorService.Web.Staff.Tests/ContractAmmendQueryBase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Security.Claims;
+using FizzWare.NBuilder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using RichardSzalay.MockHttp;
+using SFA.DAS.AssessorService.Application.Api.Client;
+using SFA.DAS.AssessorService.Domain.Entities;
+using SFA.DAS.AssessorService.Domain.JsonData;
+using SFA.DAS.AssessorService.Web.Staff.Controllers;
+using SFA.DAS.AssessorService.Web.Staff.Infrastructure;
+
+namespace SFA.DAS.AssessorService.Web.Staff.Tests
+{
+    public class ContractAmmendQueryBase
+    {
+        protected CertificateGradeController CertificateGradeController;        
+        protected Certificate Certificate;
+
+        public ContractAmmendQueryBase()
+        {
+           var mockedLogger = new Mock<ILogger<CertificateAmmendController>>();
+            var mockedApiClientLogger = new Mock<ILogger<ApiClient>>();
+
+            var mockHttpContextAccessor = SetupMockedHttpContextAccessor();
+            var apiClient = SetupApiClient(mockedApiClientLogger);
+         
+            CertificateGradeController = new CertificateGradeController(mockedLogger.Object, mockHttpContextAccessor.Object, apiClient);
+        }        
+
+        private static Mock<IHttpContextAccessor> SetupMockedHttpContextAccessor()
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn", "jcoxhead")
+            }));
+
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var context = new DefaultHttpContext {User = user};
+
+            mockHttpContextAccessor.Setup(_ => _.HttpContext).Returns(context);
+            return mockHttpContextAccessor;
+        }
+
+        private ApiClient SetupApiClient(Mock<ILogger<ApiClient>> apiClientLoggerMock)
+        {
+            Certificate = SetupCertificate();
+
+            var tokenServiceMock = new Mock<ITokenService>();
+
+            var mockHttp = new MockHttpMessageHandler();
+
+            var client = mockHttp.ToHttpClient();
+            client.BaseAddress = new Uri("http://localhost:59022/");
+
+            mockHttp.When($"http://localhost:59022/api/v1/certificates/{Certificate.Id}")
+                .Respond("application/json", JsonConvert.SerializeObject(Certificate));
+
+            mockHttp.When($"http://localhost:59022/api/v1/organisations/{Certificate.OrganisationId}")
+                .Respond("application/json", JsonConvert.SerializeObject(Certificate));
+
+            var apiClient = new ApiClient(client, apiClientLoggerMock.Object, tokenServiceMock.Object);
+            return apiClient;
+        }
+
+        private Certificate SetupCertificate()
+        {
+            var certificateId = Guid.NewGuid();
+            var certificate = new Builder().CreateNew<Certificate>()
+                .With(q => q.CertificateData = JsonConvert.SerializeObject(new Builder().CreateNew<CertificateData>().Build()))
+                .Build();
+
+            var organisaionId = Guid.NewGuid();
+            certificate.OrganisationId = organisaionId;
+
+            var organisation = new Builder().CreateNew<Organisation>()
+                .Build();
+
+            certificate.Organisation = organisation;
+
+            return certificate;
+        }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Web.Staff.Tests/WhenUserLoadsAmmendsGradePage.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SFA.DAS.AssessorService.Domain.JsonData;
+using SFA.DAS.AssessorService.Web.Staff.ViewModels;
+
+namespace SFA.DAS.AssessorService.Web.Staff.Tests
+{
+    public class WhenUserLoadsAmmendsGradePage : ContractAmmendQueryBase
+    {     
+        private IActionResult _result;
+
+        [SetUp]
+        public void Arrange()
+        {                              
+            _result = CertificateGradeController.Grade(Certificate.Id).GetAwaiter().GetResult();           
+        }
+
+        [Test]
+        public void ThenShouldreturnValidViewModel()
+        {
+            var result = _result as ViewResult;
+            var model = result.Model as CertificateGradeViewModel;
+
+            var certificateData = JsonConvert.DeserializeObject<CertificateData>(Certificate.CertificateData);
+
+            model.Id.Should().Be(Certificate.Id);
+            model.SelectedGrade.Should().Be(certificateData.OverallGrade);            
+        }
+    }   
+}
