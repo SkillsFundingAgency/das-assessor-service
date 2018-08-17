9a47898c698fc0038cf39af7d27a9d902b059361
markcain <mark.cain@digital.education.gov.uk>
2018-08-17 13:54:12 +0100

    ON-803 final finessing of 803

--- src/SFA.DAS.AssessorService.Data.IntegrationTests/Handlers/OrganisationHandler.cs
+++ src/SFA.DAS.AssessorService.Data.IntegrationTests/Handlers/OrganisationHandler.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Text;
-using System.Threading.Tasks;
 using Dapper;
 using SFA.DAS.AssessorService.Api.Types.Models.AO;
 using SFA.DAS.AssessorService.Data.DapperTypeHandlers;
