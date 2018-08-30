91f565ffadd65daf2ea5af3da6236c8ced190e50
John Coxhead <jcoxhead@hotmail.com>
2018-08-30 16:05:35 +0100

    fix to certificate repo

--- src/SFA.DAS.AssessorService.Data/CertificateRepository.cs
+++ src/SFA.DAS.AssessorService.Data/CertificateRepository.cs
@@ -44,7 +44,10 @@ namespace SFA.DAS.AssessorService.Data
                 _context.SaveChanges();
                 await UpdateCertificateLog(certificate, CertificateActions.Start, certificate.CreatedBy);
                 _context.SaveChanges();
+
+                return certificate;
             }
+
             return existingCert;
         }
 
