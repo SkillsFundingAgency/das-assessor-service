d8ccfb767a0db8f9402db8e257f60735f4c4d8b8
John Coxhead <jcoxhead@hotmail.com>
2018-09-11 14:06:22 +0100

    caching optimiations for ukprn

--- src/SFA.DAS.AssessorService.Web/Controllers/Private/CertificatePrivateProviderUkprnController.cs
+++ src/SFA.DAS.AssessorService.Web/Controllers/Private/CertificatePrivateProviderUkprnController.cs
@@ -1,9 +1,7 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.AssessorService.Application.Api.Client.Clients;
 using SFA.DAS.AssessorService.Domain.Consts;
@@ -19,7 +17,7 @@ namespace SFA.DAS.AssessorService.Web.Controllers.Private
     {
         private readonly IAssessmentOrgsApiClient _assessmentOrgsApiClient;
 
-        public CertificatePrivateProviderUkprnController(ILogger<CertificateController> logger, 
+        public CertificatePrivateProviderUkprnController(ILogger<CertificateController> logger,
             IHttpContextAccessor contextAccessor,
             IAssessmentOrgsApiClient assessmentOrgsApiClient,
             ICertificateApiClient certificateApiClient, ISessionService sessionService)
@@ -30,22 +28,17 @@ namespace SFA.DAS.AssessorService.Web.Controllers.Private
 
         [HttpGet]
         public async Task<IActionResult> Ukprn(bool? redirectToCheck = false)
-        {           
+        {
             var viewResult = await LoadViewModel<CertificateUkprnViewModel>("~/Views/Certificate/Ukprn.cshtml");
-            var vm = ((viewResult as ViewResult).Model) as CertificateUkprnViewModel;
-         
             return viewResult;
         }
 
         [HttpPost(Name = "Ukprn")]
         public async Task<IActionResult> Ukprn(CertificateUkprnViewModel vm)
         {
-            var ukprns = (await _assessmentOrgsApiClient.GetProviders())
-                .Select(q => new SelectListItem { Value = q.Ukprn.ToString(), Text = q.Ukprn.ToString() });          
-
             return await SaveViewModel(vm,
                 returnToIfModelNotValid: "~/Views/Certificate/Ukprn.cshtml",
-                nextAction: RedirectToAction("Grade", "CertificateGrade"), action: CertificateActions.LearningStartDate);
+                nextAction: RedirectToAction("Grade", "CertificateGrade"), action: CertificateActions.Ukprn);
         }
     }
 }
