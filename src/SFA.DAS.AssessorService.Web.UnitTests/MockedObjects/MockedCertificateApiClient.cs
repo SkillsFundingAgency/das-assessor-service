51dc3f394a57c789ef7ed249a87e259bf08fe4bf
John Coxhead <jcoxhead@hotmail.com>
2018-09-12 14:54:49 +0100

    Refactor tidyup

---
+++ src/SFA.DAS.AssessorService.Web.UnitTests/MockedObjects/MockedCertificateApiClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using RichardSzalay.MockHttp;
+using SFA.DAS.AssessorService.Application.Api.Client;
+using SFA.DAS.AssessorService.Application.Api.Client.Clients;
+using SFA.DAS.AssessorService.Domain.Entities;
+
+namespace SFA.DAS.AssessorService.Web.UnitTests.MockedObjects
+{
+    public class MockedCertificateApiClient
+    {
+        public static CertificateApiClient Setup(Certificate certificate, Mock<ILogger<CertificateApiClient>> apiClientLoggerMock)
+        {           
+            var tokenServiceMock = new Mock<ITokenService>();
+
+            var mockHttp = new MockHttpMessageHandler();
+
+            var client = mockHttp.ToHttpClient();
+            client.BaseAddress = new Uri("http://localhost:59022/");
+
+            mockHttp.When($"http://localhost:59022/api/v1/certificates/{certificate.Id}")
+                .Respond("application/json", JsonConvert.SerializeObject(certificate));
+
+            mockHttp.When($"http://localhost:59022/api/v1/organisations/{certificate.OrganisationId}")
+                .Respond("application/json", JsonConvert.SerializeObject(certificate));
+
+            var apiClient = new CertificateApiClient(client, tokenServiceMock.Object, apiClientLoggerMock.Object);
+
+            return apiClient;
+        }
+    }
+}
--- src/SFA.DAS.AssessorService.Web.UnitTests/PrivateCertificateTests/Queries/CertificateQueryBase.cs
+++ src/SFA.DAS.AssessorService.Web.UnitTests/PrivateCertificateTests/Queries/CertificateQueryBase.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Claims;
 using FizzWare.NBuilder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
@@ -8,15 +7,13 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
 using Moq;
 using Newtonsoft.Json;
-using RichardSzalay.MockHttp;
-using SFA.DAS.AssessorService.Application.Api.Client;
 using SFA.DAS.AssessorService.Application.Api.Client.Clients;
 using SFA.DAS.AssessorService.Domain.Entities;
 using SFA.DAS.AssessorService.Domain.JsonData;
 using SFA.DAS.AssessorService.ExternalApis.AssessmentOrgs;
-using SFA.DAS.AssessorService.ExternalApis.AssessmentOrgs.Types;
 using SFA.DAS.AssessorService.Web.Controllers;
 using SFA.DAS.AssessorService.Web.Infrastructure;
+using SFA.DAS.AssessorService.Web.UnitTests.MockedObjects;
 using Organisation = SFA.DAS.AssessorService.Domain.Entities.Organisation;
 
 namespace SFA.DAS.AssessorService.Web.UnitTests.PrivateCertificateTests.Queries
@@ -25,133 +22,29 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.PrivateCertificateTests.Queries
     {
         protected Mock<ILogger<CertificateController>> MockLogger;
         protected Mock<IHttpContextAccessor> MockHttpContextAccessor;
-        protected Mock<ISessionService> MockSession;
-
         protected ICertificateApiClient MockCertificateApiClient;
         protected IAssessmentOrgsApiClient MockAssessmentOrgsApiClient;
 
+        protected Mock<ISessionService> MockSession;
 
         protected Certificate Certificate;
         protected CertificateData CertificateData;
 
         public CertificateQueryBase()
         {
+            Certificate = SetupCertificate();
+
             MockLogger = new Mock<ILogger<CertificateController>>();
             var mockedApiClientLogger = new Mock<ILogger<CertificateApiClient>>();
 
-            MockHttpContextAccessor = SetupMockedHttpContextAccessor();
-            MockCertificateApiClient = SetupCertificateApiClient(mockedApiClientLogger);
-            MockAssessmentOrgsApiClient = SetupAssessmentOrgsApiClient(mockedApiClientLogger);
-
             MockSession = new Mock<ISessionService>();
 
+            MockHttpContextAccessor = MockedHttpContextAccessor.Setup();
+            MockCertificateApiClient = MockedCertificateApiClient.Setup(Certificate, mockedApiClientLogger);
+
+            MockAssessmentOrgsApiClient = MockedAssessmentOrgsApiClient.Setup(mockedApiClientLogger);
+
             CertificateData = JsonConvert.DeserializeObject<CertificateData>(Certificate.CertificateData);
-        }
-
-        private static Mock<IHttpContextAccessor> SetupMockedHttpContextAccessor()
-        {
-            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
-            {
-                new Claim(ClaimTypes.NameIdentifier, "1"),
-                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn", "jcoxhead")
-            }));
-
-            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
-            var context = new DefaultHttpContext { User = user };
-
-            mockHttpContextAccessor.Setup(_ => _.HttpContext).Returns(context);
-            return mockHttpContextAccessor;
-        }
-
-        private CertificateApiClient SetupCertificateApiClient(Mock<ILogger<CertificateApiClient>> apiClientLoggerMock)
-        {
-            Certificate = SetupCertificate();
-
-            var tokenServiceMock = new Mock<ITokenService>();
-
-            var mockHttp = new MockHttpMessageHandler();
-
-            var client = mockHttp.ToHttpClient();
-            client.BaseAddress = new Uri("http://localhost:59022/");
-
-            mockHttp.When($"http://localhost:59022/api/v1/certificates/{Certificate.Id}")
-                .Respond("application/json", JsonConvert.SerializeObject(Certificate));
-
-            mockHttp.When($"http://localhost:59022/api/v1/organisations/{Certificate.OrganisationId}")
-                .Respond("application/json", JsonConvert.SerializeObject(Certificate));
-
-            var apiClient = new CertificateApiClient(client, tokenServiceMock.Object, apiClientLoggerMock.Object);
-
-            return apiClient;
-        }
-
-        private IAssessmentOrgsApiClient SetupAssessmentOrgsApiClient(Mock<ILogger<CertificateApiClient>> mockedApiClientLogger)
-        {
-            var standardOrganisartionSummaries = new List<StandardOrganisationSummary>
-            {
-                new StandardOrganisationSummary
-                {
-                    StandardCode = "93"
-                },
-                new StandardOrganisationSummary
-                {
-                    StandardCode = "92"
-                },
-                new StandardOrganisationSummary
-                {
-                    StandardCode = "91"
-                }
-            };
-
-
-            var standards = new List<Standard>
-            {
-                new Standard
-                {
-                    Id = 91,
-                    Level = 2,
-                    Title = "Test Title 1"
-                },
-                new Standard
-                {
-                    Id = 92,
-                    Level = 3,
-                    Title = "Test Title 2"
-                },
-                new Standard
-                {
-                    Id = 93,
-                    Level = 5,
-                    Title = "Test Title 3"
-                },
-                new Standard
-                {
-                    Id = 94,
-                    Level = 2,
-                    Title = "Test Title 4"
-                },
-                new Standard
-                {
-                    Id = 95,
-                    Level = 2,
-                    Title = "Test Title 5"
-                },
-            };
-
-            var mockHttp = new MockHttpMessageHandler();
-
-            var client = mockHttp.ToHttpClient();
-            client.BaseAddress = new Uri("https://findapprenticeshiptraining-api.sfa.bis.gov.uk/");
-
-            mockHttp.When($"/assessment-organisations/EPA00001/standards")
-                .Respond("application/json", JsonConvert.SerializeObject(standardOrganisartionSummaries));
-
-            mockHttp.When($"https://findapprenticeshiptraining-api.sfa.bis.gov.uk/standards")
-                .Respond("application/json", JsonConvert.SerializeObject(standards));
-
-            var apiClient = new AssessmentOrgsApiClient(client);
-
-            return apiClient;
         }
 
         private Certificate SetupCertificate()
