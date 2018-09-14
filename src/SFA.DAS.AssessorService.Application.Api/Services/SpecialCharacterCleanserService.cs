ec38b7f460e5813bf5c8032b0d00821d8b3c4eae
markcain <mark.cain@digital.education.gov.uk>
2018-09-14 13:35:04 +0100

    ON-867b code and test complete

---
+++ src/SFA.DAS.AssessorService.Application.Api/Services/SpecialCharacterCleanserService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SFA.DAS.AssessorService.Application.Interfaces;
+
+namespace SFA.DAS.AssessorService.Application.Api.Services
+{
+    public class SpecialCharacterCleanserService : ISpecialCharacterCleanserService
+    {
+        private Dictionary<char, char> _alternates;
+
+        public SpecialCharacterCleanserService()
+        {
+            BuildAlternates();
+        }
+
+        private void BuildAlternates()
+        {
+            _alternates = new Dictionary<char, char>
+            {
+                {'’', '\''},
+                {'`', '\''},
+                {'-', '–'}
+            };
+        }
+
+        public string CleanseStringForSpecialCharacters(string inputString)
+        {
+            var processedString = inputString.Trim();
+
+            var specialCharacters = SpecialCharactersInString(processedString);
+
+            if (specialCharacters.Length <= 0) return processedString;
+            foreach (var specialCharacter in specialCharacters)
+            {
+                var matchingEntry = _alternates.First(x => x.Key == specialCharacter);
+                processedString = processedString.Replace(matchingEntry.Key, matchingEntry.Value);
+            }
+
+            return processedString;
+        }
+
+        private char[] SpecialCharactersInString(string inputString)
+        {
+            return _alternates.Where(kvp => inputString.Contains(kvp.Key)).Select(kvp => kvp.Key).ToArray();
+        }
+    }
+}
