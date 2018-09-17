695f2138b214f72e82cfeae14b8fe616255fbe52
David Gouge <davegouge@gmail.com>
2018-09-17 16:20:28 +0100

    Updated the rest of the API controllers with role requirement

--- src/SFA.DAS.AssessorService.Application.Api/Controllers/RegisterController.cs
+++ src/SFA.DAS.AssessorService.Application.Api/Controllers/RegisterController.cs
@@ -15,7 +15,7 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 namespace SFA.DAS.AssessorService.Application.Api.Controllers
 {
 
-    [Authorize]
+    [Authorize(Roles = "AssessorServiceInternalAPI")]
     [Route("api/ao/assessment-organisations")]
     [ValidateBadRequest]
     public class RegisterController : Controller
