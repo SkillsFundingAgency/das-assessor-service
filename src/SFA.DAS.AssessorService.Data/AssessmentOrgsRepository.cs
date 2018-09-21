4d5101e9fc54b426ea88c4b49ff0b28a76a1fa8e
MarkFCain <mark.cain@digital.education.gov.uk>
2018-09-21 08:37:31 +0100

    ON-668 updated connection string changes

--- src/SFA.DAS.AssessorService.Data/AssessmentOrgsRepository.cs
+++ src/SFA.DAS.AssessorService.Data/AssessmentOrgsRepository.cs
@@ -371,7 +371,7 @@ namespace SFA.DAS.AssessorService.Data
         private string LocalConnectionString()
         {
             var connectionString = _configuration.SqlConnectionString;
-            connectionString = connectionString.Replace("MultipleActiveResultSets=True", "MultipleActiveResultSets=False");
+            //connectionString = connectionString.Replace("MultipleActiveResultSets=True", "MultipleActiveResultSets=False");
             connectionString = connectionString.Replace("Pooling=False", "Pooling=True");
             return connectionString;
         }
