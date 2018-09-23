fe233518cf9cc47a2256dbf6baae161658ac08ec
John Coxhead <jcoxhead@hotmail.com>
2018-09-23 18:36:41 +0100

    Added ukprns for privately funded

---
+++ src/SFA.DAS.AssessorService.Web.Staff/Controllers/Private/CertificatePrivateProviderUkprnController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AssessorService.Domain.Consts;
+using SFA.DAS.AssessorService.Web.Staff.Infrastructure;
+using SFA.DAS.AssessorService.Web.Staff.ViewModels.Private;
+
+namespace SFA.DAS.AssessorService.Web.Staff.Controllers.Private
+{
+    [Authorize]
+    [Route("certificate/ukprns")]
+    public class CertificatePrivateProviderUkprnController : CertificateBaseController
+    { 
+        public CertificatePrivateProviderUkprnController(ILogger<CertificateAmmendController> logger,
+            IHttpContextAccessor contextAccessor,
+            ApiClient apiClient) : base(logger, contextAccessor, apiClient)
+        {     
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Ukprn(Guid certificateId)
+        {
+            var viewResult = await LoadViewModel<CertificateUkprnViewModel>(certificateId, "~/Views/CertificateAmmend/Ukprn.cshtml");
+            return viewResult;
+        }
+
+        [HttpPost(Name = "Ukprn")]
+        public async Task<IActionResult> Ukprn(CertificateUkprnViewModel vm)
+        {
+            var actionResult = await SaveViewModel(vm,
+                returnToIfModelNotValid: "~/Views/CertificateAmmend/Ukprn.cshtml",
+                nextAction: RedirectToAction("Check", "CertificateAmmend", new { certificateid = vm.Id }), action: CertificateActions.Ukprn);
+
+            return actionResult;           
+        }
+    }
+}
