5b4342d122fc09e583f46c07c42d89bcf775db71
MarkFCain <mark.cain@digital.education.gov.uk>
2018-09-26 10:57:23 +0100

    OB-891 transition cos need to work on other branch

--- src/SFA.DAS.AssessorService.Web.Staff/Validators/RegisterAddOrganisationViewModelValidator.cs
+++ src/SFA.DAS.AssessorService.Web.Staff/Validators/RegisterAddOrganisationViewModelValidator.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentValidation;
+using Microsoft.Extensions.Localization;
+using SFA.DAS.AssessorService.Application.Api.Consts;
+using SFA.DAS.AssessorService.Application.Api.Validators;
+using SFA.DAS.AssessorService.Application.Interfaces;
+using SFA.DAS.AssessorService.Data;
+using SFA.DAS.AssessorService.Settings;
 using SFA.DAS.AssessorService.Web.Staff.Models;
 
 namespace SFA.DAS.AssessorService.Web.Staff.Validators
 {
     public class RegisterAddOrganisationViewModelValidator : AbstractValidator<RegisterAddOrganisationViewModel>
     {
-        public RegisterAddOrganisationViewModelValidator()
+        //private readonly IWebConfiguration _configuration;
+        private readonly IStringLocalizer<EpaOrganisationValidator> _localizer;
+        private readonly IRegisterQueryRepository _repository;
+
+        public RegisterAddOrganisationViewModelValidator(IStringLocalizer<EpaOrganisationValidator> localizer, IRegisterQueryRepository repository)
         {
-            RuleFor(vm => vm.Name).NotEmpty().WithMessage("Search string required");
+           // _configuration = configuration;
+            _localizer = localizer;
+            _repository = repository;      
+            var validator = new EpaOrganisationValidator(_repository, _localizer);
+           
+           // var res = validator.CheckOrganisationName("");
+          
+            RuleFor(vm => vm.Name).NotEmpty().WithMessage("needs words");
         }
     }
 }
