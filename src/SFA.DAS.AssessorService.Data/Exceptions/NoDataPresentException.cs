ae06548f8fbdab815622a17c4aab2d32dcf904c8
MarkFCain <mark.cain@digital.education.gov.uk>
2018-09-28 09:19:24 +0100

    ON-805 merge with master

---
+++ src/SFA.DAS.AssessorService.Data/Exceptions/NoDataPresentException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SFA.DAS.AssessorService.Data.Exceptions
+{
+    public class NoDataPresentException : Exception
+    {
+        public NoDataPresentException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Domain/Entities/AssessmentOrganisations/EpaOrganisationStandardDeliveryArea.cs
@@ -0,0 +1,13 @@
+﻿namespace SFA.DAS.AssessorService.Domain.Entities.AssessmentOrganisations
+{
+    public class EpaOrganisationStandardDeliveryArea
+    {
+        public int Id { get; set; }
+        public int OrganisationStandardId { get; set; }
+        public string EndPointAssessorOrganisationId { get; set; }
+        public int StandardCode { get; set; }
+        public int DeliveryAreaId { get; set; }
+        public string Comments { get; set; }
+        public string Status { get; set; }
+    }
+}
