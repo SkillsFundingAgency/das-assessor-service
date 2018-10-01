dafbdf2bdece4ff9bd26c29641fe0c49218ce91d
John Coxhead <jcoxhead@hotmail.com>
2018-10-01 10:44:32 +0100

    Changes for private last name

---
+++ src/SFA.DAS.AssessorService.Web.Staff/Controllers/Private/CertificatePrivateLastNameController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AssessorService.Application.Api.Client.Clients;
+using SFA.DAS.AssessorService.Domain.Consts;
+using SFA.DAS.AssessorService.Web.Infrastructure;
+using SFA.DAS.AssessorService.Web.Staff.Controllers;
+using SFA.DAS.AssessorService.Web.Staff.Infrastructure;
+using SFA.DAS.AssessorService.Web.Staff.ViewModels.Private;
+using SFA.DAS.AssessorService.Web.ViewModels.Certificate.Private;
+
+namespace SFA.DAS.AssessorService.Web.Controllers.Private
+{
+    [Authorize]
+    [Route("certificate/lastname")]
+    public class CertificatePrivateLastNameController : CertificateBaseController
+    {
+        public CertificatePrivateLastNameController(ILogger<CertificateAmendController> logger,
+            IHttpContextAccessor contextAccessor,
+            ApiClient apiClient) : base(logger, contextAccessor, apiClient)
+        { }
+
+        [HttpGet]
+        public async Task<IActionResult> LastName(Guid certificateid)
+        {
+            return await LoadViewModel<CertificateLastNameViewModel>(certificateid, "~/Views/CertificateAmend/LastName.cshtml");
+        }
+
+        [HttpPost(Name = "LastName")]
+        public async Task<IActionResult> LastName(CertificateLastNameViewModel vm)
+        {
+            var actionResult = await SaveViewModel(vm,
+                returnToIfModelNotValid: "~/Views/CertificateAmend/LastName.cshtml",
+                nextAction: RedirectToAction("Check", "CertificateAmend", new { certificateid = vm.Id }), action: CertificateActions.LastName);
+
+            return actionResult;
+        }
+    }
+}
