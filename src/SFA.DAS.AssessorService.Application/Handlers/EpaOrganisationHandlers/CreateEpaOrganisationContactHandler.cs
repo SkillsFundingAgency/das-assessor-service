a2d4bcf8bce979aa6cc9777feee9bfb813cadde1
MarkFCain <mark.cain@digital.education.gov.uk>
2018-10-05 09:21:38 +0100

    ON-959 preliminary work

---
+++ src/SFA.DAS.AssessorService.Application/Handlers/EpaOrganisationHandlers/CreateEpaOrganisationContactHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AssessorService.Api.Types.Models;
+using SFA.DAS.AssessorService.Application.Interfaces;
+
+namespace SFA.DAS.AssessorService.Application.Handlers.EpaOrganisationHandlers
+{
+    public class CreateEpaOrganisationContactHandler : IRequestHandler<CreateOrganisationContactRequest, string>
+    {
+        private readonly IRegisterRepository _registerRepository;
+        private readonly ILogger<CreateEpaOrganisationContactHandler> _logger;
+        private readonly IEpaOrganisationValidator _validator;
+        private readonly ISpecialCharacterCleanserService _cleanser;
+
+        public CreateEpaOrganisationContactHandler(IRegisterRepository registerRepository, IEpaOrganisationValidator validator, ISpecialCharacterCleanserService cleanser, ILogger<CreateEpaOrganisationContactHandler> logger)
+        {
+            _registerRepository = registerRepository;
+            _validator = validator;
+            _cleanser = cleanser;
+            _logger = logger;
+        }
+
+        public Task<string> Handle(CreateOrganisationContactRequest request, CancellationToken cancellationToken)
+        {
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
