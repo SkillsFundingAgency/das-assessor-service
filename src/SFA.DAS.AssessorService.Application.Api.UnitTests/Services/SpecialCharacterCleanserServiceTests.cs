ecef2926c025be2283cc976ca74f7d0814d02d01
MarkFCain <mark.cain@digital.education.gov.uk>
2018-10-08 11:12:49 +0100

    ON-893 fixed broken test

--- src/SFA.DAS.AssessorService.Application.Api.UnitTests/Services/SpecialCharacterCleanserServiceTests.cs
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/Services/SpecialCharacterCleanserServiceTests.cs
@@ -16,13 +16,13 @@ namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Services
             _cleanser = new SpecialCharacterCleanserService();   
         }
 
-        [TestCase("Colleges’ ", "Colleges' ")]
-        [TestCase("Colleges ", "Colleges ")]
+        [TestCase("Colleges’ ", "Colleges'")]
+        [TestCase("Colleges ", "Colleges")]
         [TestCase("Raj o`intel", "Raj o\'intel")]
         [TestCase("Intel - Technologies Ltd", "Intel – Technologies Ltd")]
         [TestCase("column 1\u00A0column 2", "column 1 column 2")]
         [TestCase("column 3\tcolumn 4", "column 3 column 4")]
-        [TestCase("wildcard%", "wildcard " )]
+        [TestCase("wildcard%", "wildcard" )]
         public void GetCleansedStringFromOriginalString(string inputString, string outputString)
         {
             var returnedString = _cleanser.CleanseStringForSpecialCharacters(inputString);
