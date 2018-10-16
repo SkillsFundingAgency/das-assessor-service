8ddc3404e54ff4d6eb377c593062387108c1524d
John Coxhead <jcoxhead@hotmail.com>
2018-10-16 11:22:49 +0100

    Simple tests for TitleCase conversion

---
+++ src/SFA.DAS.AssessorService.PrintFunctionProcessFlow.UnitTests/AssessorServiceApi/WhenSystemConvertsFullNameToTitleCase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+
+namespace SFA.DAS.AssessorService.PrintFunctionProcessFlow.UnitTests.AssessorServiceApi
+{
+    public class WhenSystemConvertsFullNameToTitleCase
+    {
+        [TestCase(null)]
+        [TestCase("TEST STRING")]
+        [TestCase("Test sTring")]
+        [TestCase("test string")]
+        [TestCase("Test String")]
+        [TestCase("test string")]
+        [TestCase("tEST sSTRING")]
+        [TestCase("TeST sSTRING")]
+        public void ThenItShouldUpdateCertificates(string firstName)
+        {
+            var learnerName = "test string";
+
+            var result = firstName != null
+                ? CultureInfo.CurrentCulture.TextInfo.ToTitleCase(learnerName)
+                : CultureInfo.CurrentCulture.TextInfo.ToTitleCase(learnerName);
+
+            Assert.AreEqual(result, "Test String");
+            Console.WriteLine(result);
+        }
+    }
+}
