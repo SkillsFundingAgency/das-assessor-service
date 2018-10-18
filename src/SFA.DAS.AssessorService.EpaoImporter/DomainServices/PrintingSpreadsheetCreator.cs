7c564062238e2a23a32d6372313070139d2fe472
John Coxhead <jcoxhead@hotmail.com>
2018-10-18 08:02:22 +0100

    convert to ternary expression

--- src/SFA.DAS.AssessorService.EpaoImporter/DomainServices/PrintingSpreadsheetCreator.cs
+++ src/SFA.DAS.AssessorService.EpaoImporter/DomainServices/PrintingSpreadsheetCreator.cs
@@ -173,16 +173,8 @@ namespace SFA.DAS.AssessorService.EpaoImporter.DomainServices
 
                 var learnerName = $"{certificateData.LearnerGivenNames} {certificateData.LearnerFamilyName}";
                 var resultName = String.Empty;
-                if (!string.IsNullOrEmpty(certificateData.FullName))
-                {
-                    resultName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(certificateData.FullName.ToLower());                 
-                }
-                else
-                {
-                    resultName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(learnerName.ToLower());
-                }
-
-                worksheet.Cells[row, 2].Value = resultName;
+                worksheet.Cells[row, 2].Value = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(!string.IsNullOrEmpty(certificateData.FullName) ? 
+                        certificateData.FullName.ToLower() : learnerName.ToLower());               
 
                 if (certificateData.StandardName != null)
                     worksheet.Cells[row, 3].Value = certificateData.StandardName.ToUpper();
