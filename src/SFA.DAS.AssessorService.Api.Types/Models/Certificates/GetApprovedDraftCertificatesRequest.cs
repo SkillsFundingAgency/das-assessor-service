85d29d02d9dee7abe66953020493be019b01497e
John Coxhead <jcoxhead@hotmail.com>
2018-10-31 10:21:49 +0000

    resolve review comments

--- src/SFA.DAS.AssessorService.Api.Types/Models/Certificates/GetApprovedDraftCertificatesRequest.cs
+++ src/SFA.DAS.AssessorService.Api.Types/Models/Certificates/GetApprovedDraftCertificatesRequest.cs
@@ -3,8 +3,8 @@ using MediatR;
 
 namespace SFA.DAS.AssessorService.Api.Types.Models.Certificates
 {
+    // Please note this is empty by desig as there are currently no input paramters  to allow mediator to work.
     public class GetApprovedDraftCertificatesRequest : IRequest<List<CertificateSummaryResponse>>
-    {     
-        
+    {             
     }
 }
