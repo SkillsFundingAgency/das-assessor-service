f81ba9d4ab11c109a16644537b18f9a4913e2878
MarkFCain <mark.cain@digital.education.gov.uk>
2018-11-05 10:17:38 +0000

    ON-1030 added organisationStatus to viewModel

--- src/SFA.DAS.AssessorService.Web.Staff/Controllers/RegisterController.cs
+++ src/SFA.DAS.AssessorService.Web.Staff/Controllers/RegisterController.cs
@@ -334,6 +334,7 @@ namespace SFA.DAS.AssessorService.Web.Staff.Controllers
             vm.StandardLastDateForNewStarts = standard.LastDateForNewStarts;
             vm.AvailableDeliveryAreas = availableDeliveryAreas;
             vm.DeliveryAreas = vm.DeliveryAreas ?? new List<int>();
+            vm.OrganisationStatus = organisation.Status;
             return vm;
         }
         private void GatherOrganisationStandards(RegisterViewAndEditOrganisationViewModel viewAndEditModel)
