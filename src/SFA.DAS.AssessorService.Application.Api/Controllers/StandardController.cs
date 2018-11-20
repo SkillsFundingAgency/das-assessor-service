cf91ac5424ceb02718ca8afd7a48401cfe0152a1
MarkFCain <mark.cain@digital.education.gov.uk>
2018-11-20 14:29:48 +0000

    ON-792 minor modifications before tech review

--- src/SFA.DAS.AssessorService.Application.Api/Controllers/StandardController.cs
+++ src/SFA.DAS.AssessorService.Application.Api/Controllers/StandardController.cs
@@ -26,7 +26,7 @@ namespace SFA.DAS.AssessorService.Application.Api.Controllers
             _logger = logger;
             _mediator = mediator;
         }
-        [HttpPost("update-standards",Name = "GatherAndStoreStandards")]
+        [HttpPost("update-standards",Name = "update-standards/GatherAndStoreStandards")]
         [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(GatherStandardsResponse))]
         [SwaggerResponse((int)HttpStatusCode.BadRequest, typeof(ApiResponse))]
         [SwaggerResponse((int)HttpStatusCode.Conflict, Type = typeof(ApiResponse))]
@@ -34,9 +34,8 @@ namespace SFA.DAS.AssessorService.Application.Api.Controllers
         public async Task<IActionResult> GatherAndStoreStandards([FromBody] GatherStandardsRequest request)
         {
             var processDetails = await _mediator.Send(request);
-            var compiledResponseText = processDetails;
-            _logger.LogInformation(compiledResponseText);
-            return Ok(new GatherStandardsResponse(compiledResponseText));
+            _logger.LogInformation(processDetails.ToString());
+            return Ok(new GatherStandardsResponse(processDetails));
         }
 
         [HttpGet("assessment-organisations/collated-standards", Name = "GetCollatedStandards")]
