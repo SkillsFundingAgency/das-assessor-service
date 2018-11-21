a57b59128ef7801baf39577559404c7a689c65be
Martin Hornsby <martin-hornsby@ntlworld.com>
2018-11-21 09:58:34 +0000

    ON-1133 -  Mapping current list of organisation types to the new list

--- src/SFA.DAS.AssessorService.Database/Script.PostDeployment1.sql
+++ src/SFA.DAS.AssessorService.Database/Script.PostDeployment1.sql
@@ -31,3 +31,45 @@ END
 UPDATE CERTIFICATES
 set IsPrivatelyFunded = 0
 WHERE IsPrivatelyFunded IS NULL 
+
+
+-- Amend Organisations Type to have description
+
+IF NOT EXISTS 
+(
+    SELECT * 
+    FROM INFORMATION_SCHEMA.COLUMNS 
+    WHERE table_name = 'OrganisationType'
+    AND column_name = 'TypeDescription'
+)
+BEGIN
+    ALTER TABLE [OrganisationType] ADD [TypeDescription] nvarchar(500);
+END
+GO
+
+UPDATE [OrganisationType] SET [Type] =  'AwardingOrganisations', [TypeDescription] = 'Awarding Organisations' WHERE id = 1;
+UPDATE [OrganisationType] SET [Type] =  'AssessmentOrganisations', [TypeDescription] = 'Assessment Organisations' WHERE id = 2;
+UPDATE [OrganisationType] SET [Type] =  'TradeBody', [TypeDescription] = 'Trade Body' WHERE id = 3;
+UPDATE [OrganisationType] SET [Type] =  'ProfessionalBody', [TypeDescription] = 'Professional Body - approved by the relevant council' WHERE id = 4;
+UPDATE [OrganisationType] SET [Type] =  'HEI', [TypeDescription] = 'HEI monitored and supported by OfS' WHERE id = 5;
+UPDATE [OrganisationType] SET [Type] =  'NSA_SSC', [TypeDescription] = 'National Skills Academy / Sector Skills Council' WHERE id = 6;
+UPDATE [OrganisationType] SET [Type] =  'TrainingProvider', [TypeDescription] = 'Training Provider - including HEI not in England' WHERE id = 7;
+UPDATE [OrganisationType] SET [Status] =  'Deleted' WHERE id = 8;
+UPDATE [OrganisationType] SET [Type] =  'PublicSector', [TypeDescription] = 'Incorporated as Public Sector Body, Local authority including LEA schools, Central Government Department / Executive Agency / Non-departmental public body, NHS Trust / Fire Authority, Police Constabulary or Police Crime Commissioner' WHERE id = 9;
+
+-- 'college','Academy_FreeSchool'
+
+SET identity_insert OrganisationType ON
+INSERT INTO [OrganisationType] (ID,[Type],[Status], [TypeDescription]) VALUES (10,'College','Live','GFE College currently receiving funding from the ESFA, 6th form / FE college');
+INSERT INTO [OrganisationType] (ID,[Type],[Status], [TypeDescription]) VALUES (11,'Academy_FreeSchool','Live','Academy or Free school registered with the ESFA');
+SET identity_insert OrganisationType OFF
+
+UPDATE [Organisations] SET [OrganisationTypeId] = 3 WHERE [EndPointAssessorOrganisationId] = 'EPA0027';
+UPDATE [Organisations] SET [OrganisationTypeId] = 9 WHERE [EndPointAssessorOrganisationId] = 'EPA0035';
+UPDATE [Organisations] SET [OrganisationTypeId] = 4 WHERE [EndPointAssessorOrganisationId] = 'EPA0049';
+UPDATE [Organisations] SET [OrganisationTypeId] = 6 WHERE [EndPointAssessorOrganisationId] = 'EPA0051';
+UPDATE [Organisations] SET [OrganisationTypeId] = 9 WHERE [EndPointAssessorOrganisationId] = 'EPA0085';
+UPDATE [Organisations] SET [OrganisationTypeId] = 6 WHERE [EndPointAssessorOrganisationId] = 'EPA0104';
+UPDATE [Organisations] SET [OrganisationTypeId] = 9 WHERE [EndPointAssessorOrganisationId] = 'EPA0141';
+UPDATE [Organisations] SET [OrganisationTypeId] = 10 WHERE [EndPointAssessorOrganisationId] = 'EPA0159';
+UPDATE [Organisations] SET [OrganisationTypeId] = 7 WHERE [EndPointAssessorOrganisationId] = 'EPA0173';
