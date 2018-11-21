1a575ae9340894e8a6d8e3d9ecaa03231a4dca8d
Martin Hornsby <martin-hornsby@ntlworld.com>
2018-11-21 12:14:17 +0000

    Remove old SQL from Post Deployment script

--- src/SFA.DAS.AssessorService.Database/Script.PostDeployment1.sql
+++ src/SFA.DAS.AssessorService.Database/Script.PostDeployment1.sql
@@ -9,29 +9,6 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-IF NOT EXISTS(SELECT * FROM ScheduleRuns)
-BEGIN
-	INSERT INTO ScheduleRuns (RunTime, IsComplete, Interval, IsRecurring, ScheduleType) VALUES ('2018-08-17 08:00:00', 0, 10080, 1, 1)
-END
-
-IF NOT EXISTS(SELECT * FROM StaffReports)
-BEGIN
-	INSERT INTO StaffReports (ReportName, StoredProcedure, DisplayOrder) VALUES ('Monthly detailed extract', 'StaffReports_DetailedExtract', 1)
-	INSERT INTO StaffReports (ReportName, StoredProcedure, DisplayOrder) VALUES ('Monthly summary', 'StaffReports_MonthlySummary', 2)
-	INSERT INTO StaffReports (ReportName, StoredProcedure, DisplayOrder) VALUES ('Weekly summary', 'StaffReports_WeeklySummary', 3)
-	INSERT INTO StaffReports (ReportName, StoredProcedure, DisplayOrder) VALUES ('Batch', 'StaffReports_ByBatch', 4)
-	INSERT INTO StaffReports (ReportName, StoredProcedure, DisplayOrder) VALUES ('EPAO', 'StaffReports_ByEpao', 5)
-	INSERT INTO StaffReports (ReportName, StoredProcedure, DisplayOrder) VALUES ('EPAO, standard and grade', 'StaffReports_ByEpaoAndStandardAndGrade', 6)
-	INSERT INTO StaffReports (ReportName, StoredProcedure, DisplayOrder) VALUES ('Provider', 'StaffReports_ByProvider', 7)
-	INSERT INTO StaffReports (ReportName, StoredProcedure, DisplayOrder) VALUES ('Provider and grade', 'StaffReports_ByProviderAndGrade', 8)
-	INSERT INTO StaffReports (ReportName, StoredProcedure, DisplayOrder) VALUES ('Standard', 'StaffReports_ByStandard', 9)
-	INSERT INTO StaffReports (ReportName, StoredProcedure, DisplayOrder) VALUES ('Certificate count', 'StaffReports_CertificateCount', 10)
-END
-
-UPDATE CERTIFICATES
-set IsPrivatelyFunded = 0
-WHERE IsPrivatelyFunded IS NULL 
-
 
 -- Amend Organisations Type to have description
 
