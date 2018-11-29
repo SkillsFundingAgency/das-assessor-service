9e65c073d1023e704b2f0fedf7cf2a5dc751e497
David Gouge <davegouge@gmail.com>
2018-11-29 11:12:09 +0000

    Fixed build failure

--- src/SFA.DAS.AssessorService.Data.IntegrationTests/Services/TestWebConfiguration.cs
+++ src/SFA.DAS.AssessorService.Data.IntegrationTests/Services/TestWebConfiguration.cs
@@ -19,6 +19,7 @@ namespace SFA.DAS.AssessorService.Data.IntegrationTests.Services
         public string SpecflowDBTestConnectionString { get; set; }
         public string SessionRedisConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public AuthSettings StaffAuthentication { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ClientApiAuthentication ApplyApiAuthentication { get; set; }
     }
 
 }
