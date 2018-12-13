5f411c547a77e8f261ba96e355f1eb966007e622
MarkFCain <mark.cain@digital.education.gov.uk>
2018-12-13 12:38:59 +0000

    PFS adding a bit more config

--- src/SFA.DAS.AssessorService.EpaoImporter/PrintFunctionFlow.cs
+++ src/SFA.DAS.AssessorService.EpaoImporter/PrintFunctionFlow.cs
@@ -8,7 +8,8 @@ namespace SFA.DAS.AssessorService.EpaoImporter
     public static class PrintFunctionFlow
     {
         [FunctionName("PrintFunctionFlow")]
-        public static void Run([TimerTrigger("0 */10 * * * *", RunOnStartup = true)] TimerInfo myTimer, TraceWriter functionLogger,
+        //public static void Run([TimerTrigger("0 */10 * * * *", RunOnStartup = true)] TimerInfo myTimer, TraceWriter functionLogger,
+        public static void Run([TimerTrigger("0 */1 * * * *", RunOnStartup = true)] TimerInfo myTimer, TraceWriter functionLogger,
             ExecutionContext context)
         {
 
