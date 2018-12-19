0fbecf1984bae5b3e08b2db512382af44574a4ea
MarkFCain <mark.cain@digital.education.gov.uk>
2018-12-19 14:33:15 +0000

    ON-1196 minor change to BatchDetails

--- src/SFA.DAS.AssessorService.Domain/JsonData/Printing/BatchDetails.cs
+++ src/SFA.DAS.AssessorService.Domain/JsonData/Printing/BatchDetails.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace SFA.DAS.AssessorService.Domain.JsonData.Printing
 {
@@ -8,7 +9,9 @@ namespace SFA.DAS.AssessorService.Domain.JsonData.Printing
         public DateTime BatchDate { get; set; }
         public int PostalContactCount { get; set; }
         public int TotalCertificateCount { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public DateTime? PrintedDate { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public DateTime? PostedDate { get; set; }
     }
 }
