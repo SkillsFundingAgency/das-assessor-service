63e3767958b966748127d308cc3ae12067854fa6
ArshedMust <arshed@air2wire.com>
2019-01-25 12:11:18 +0000

    Added privilages check to API

--- src/SFA.DAS.AssessorService.Web.Extensions/TagHelpers/PolicyNotExistTagHelper.cs
+++ src/SFA.DAS.AssessorService.Web.Extensions/TagHelpers/PolicyNotExistTagHelper.cs
@@ -1,23 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace SFA.DAS.AssessorService.Web.Extensions.TagHelpers
 {
-    [HtmlTargetElement("div", Attributes = PolicyTagHelperAttributeName )]
-    [HtmlTargetElement("a", Attributes = PolicyTagHelperAttributeName )]
-    public class PolicyTagHelper : TagHelper
+    [HtmlTargetElement("div", Attributes = PolicyTagHelperAttributeName)]
+    [HtmlTargetElement("a", Attributes = PolicyTagHelperAttributeName)]
+    public class PolicyNotExistTagHelper : TagHelper
     {
         private readonly IHttpContextAccessor _contextAccessor;
         private readonly IAuthorizationService _authorizationService;
 
-        public PolicyTagHelper(IHttpContextAccessor contextAccessor, IAuthorizationService authorizationService)
+        public PolicyNotExistTagHelper(IHttpContextAccessor contextAccessor, IAuthorizationService authorizationService)
         {
             _contextAccessor = contextAccessor;
             _authorizationService = authorizationService;
         }
 
-        private const string PolicyTagHelperAttributeName = "sfa-policy-exists-show";
+        private const string PolicyTagHelperAttributeName = "sfa-policy-not-exists-show";
 
         [HtmlAttributeName(PolicyTagHelperAttributeName)]
         public string PolicyName { get; set; }
@@ -25,7 +28,7 @@ namespace SFA.DAS.AssessorService.Web.Extensions.TagHelpers
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             var allowed = _authorizationService.AuthorizeAsync(_contextAccessor.HttpContext.User, PolicyName).Result;
-            if (!allowed.Succeeded)
+            if (allowed.Succeeded)
             {
                 output.SuppressOutput();
             }
