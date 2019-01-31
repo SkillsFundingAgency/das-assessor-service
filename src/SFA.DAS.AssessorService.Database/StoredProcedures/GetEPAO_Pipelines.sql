a340d78cd2cc343c6281ae955769b568cb04880e
Alan Burns <adburns57@gmail.com>
2019-01-31 11:56:44 +0000

    Simply Pipeline query now we have IXU on stdcode and uln on ILRS table

--- src/SFA.DAS.AssessorService.Database/StoredProcedures/GetEPAO_Pipelines.sql
+++ src/SFA.DAS.AssessorService.Database/StoredProcedures/GetEPAO_Pipelines.sql
@@ -1,35 +1,32 @@
-﻿CREATE PROCEDURE [dbo].[GetEPAO_Pipelines]
-	-- Add the parameters for the stored procedure here
-	  @EPAOID NVARCHAR(12)
+CREATE PROCEDURE [dbo].[GetEPAO_Pipelines]
+    -- Add the parameters for the stored procedure here
+      @EPAOID NVARCHAR(12)
 AS
-BEGIN	
+BEGIN    
 -- Apparently much more efficient then COUNT(*) OVER() or two seperate queries one for count and one for data
 -- It does not create any temp table either, query cost is less when compared to other ways of getting count
 
 WITH Data_CTE 
 AS
 (
-    SELECT StdCode, Title, UkPrn, 
-	COUNT(*) Pipeline, EstimateDate
-	FROM (
-	SELECT StdCode, EOMONTH(DATEADD(month, std.Duration, LearnStartDate)) AS EstimateDate , Title, UkPrn
-	FROM (
-	-- The most recent (as far as we can tell) record from ilr
-	SELECT * FROM (
-	SELECT Uln, StdCode,UkPrn, FamilyName, EPAORGID, completionStatus, row_number() OVER (PARTITION BY Uln, StdCode ORDER BY source DESC, learnstartdate desc) rownumber, 
-	[LearnStartDate]
-	FROM ilrs
-	) ab1 WHERE ab1.rownumber = 1 AND ab1.CompletionStatus IN (1,2) 
-	AND EpaOrgId = @EPAOID
-	) il
-	JOIN (SELECT standardid, title, CONVERT(numeric,JSON_VALUE([StandardData],'$.Duration')) Duration FROM [dbo].[StandardCollation]) std ON std.StandardId = il.StdCode
-	-- ignore already created certs
-	LEFT JOIN (SELECT uln, StandardCode, [EndPointAssessorOrganisationId] FROM [Certificates] c1 JOIN [Organisations] og on c1.[OrganisationId] = og.[Id] )  
-	ce ON ce.uln = il.uln AND ce.StandardCode = il.StdCode
-	WHERE ce.EndPointAssessorOrganisationId IS NULL
-	) ab0 
-	GROUP BY StdCode,EstimateDate,title, UkPrn
-	
+    SELECT StdCode, Title, UkPrn,
+    COUNT(*) Pipeline, EstimateDate
+    FROM (
+    SELECT StdCode, CASE WHEN PlannedEndDate > GETDATE() THEN EOMONTH(PlannedEndDate) ELSE EOMONTH(DATEADD(month, std.Duration, LearnStartDate)) END AS EstimateDate , Title, UkPrn
+    FROM (
+    -- The active records from ilr
+    SELECT Uln, StdCode, UkPrn, EPAORGID, LearnStartDate, PlannedEndDate
+    FROM ilrs
+    WHERE CompletionStatus IN (1,2) AND EpaOrgId = @EPAOID
+    ) il
+    JOIN (SELECT standardid, title, CONVERT(numeric,JSON_VALUE([StandardData],'$.Duration')) Duration FROM [dbo].[StandardCollation]) std ON std.StandardId = il.StdCode
+    -- ignore already created certs
+    LEFT JOIN (SELECT uln, StandardCode, [EndPointAssessorOrganisationId] FROM [Certificates] c1 JOIN [Organisations] og on c1.[OrganisationId] = og.[Id] )
+    ce ON ce.uln = il.uln AND ce.StandardCode = il.StdCode
+    WHERE ce.EndPointAssessorOrganisationId IS NULL
+    ) ab0
+    GROUP BY StdCode,EstimateDate,title, UkPrn
+    
 ), 
 Count_CTE 
 AS 
