937d895fd49afc6bae0ea44404338fe0415adc46
David Gouge <davegouge@googlemail.com>
2019-02-04 10:55:02 +0000

    Added in lost }

--- src/SFA.DAS.AssessorService.Application.Api.External/Controllers/PingController.cs
+++ src/SFA.DAS.AssessorService.Application.Api.External/Controllers/PingController.cs
@@ -3,11 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace SFA.DAS.AssessorService.Application.Api.External.Controllers
 {
 //    public class PingController : Controller
-   //    {
-   //        [HttpGet("/Ping")]
-   //        public IActionResult Ping()
-   //        {
-   //            return Ok("Pong");
-   //        }
-   //    }
-   //}
+    //    {
+    //        [HttpGet("/Ping")]
+    //        public IActionResult Ping()
+    //        {
+    //            return Ok("Pong");
+    //        }
+    //    }
+    //}
+}
