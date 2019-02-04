0342493a016e8dd63e4c6dbb4d9dcab390edb399
David Gouge <davegouge@googlemail.com>
2019-02-04 11:15:23 +0000

    Re-added ping controller

--- src/SFA.DAS.AssessorService.Application.Api.External/Controllers/PingController.cs
+++ src/SFA.DAS.AssessorService.Application.Api.External/Controllers/PingController.cs
@@ -2,13 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SFA.DAS.AssessorService.Application.Api.External.Controllers
 {
-//    public class PingController : Controller
-    //    {
-    //        [HttpGet("/Ping")]
-    //        public IActionResult Ping()
-    //        {
-    //            return Ok("Pong");
-    //        }
-    //    }
-    //}
+    public class PingController : Controller
+    {
+        [HttpGet("/Ping")]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}
+
 }
