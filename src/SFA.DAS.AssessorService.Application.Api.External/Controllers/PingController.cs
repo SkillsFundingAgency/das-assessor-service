e4bccbfa2ecec0fda6e3391aaf928acd4737e239
David Gouge <davegouge@googlemail.com>
2019-02-04 10:52:17 +0000

    Removed Ping endpoint to try and pinpoint release issue

--- src/SFA.DAS.AssessorService.Application.Api.External/Controllers/PingController.cs
+++ src/SFA.DAS.AssessorService.Application.Api.External/Controllers/PingController.cs
@@ -2,12 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SFA.DAS.AssessorService.Application.Api.External.Controllers
 {
-    public class PingController : Controller
-    {
-        [HttpGet("/Ping")]
-        public IActionResult Ping()
-        {
-            return Ok("Pong");
-        }
-    }
-}
+//    public class PingController : Controller
+   //    {
+   //        [HttpGet("/Ping")]
+   //        public IActionResult Ping()
+   //        {
+   //            return Ok("Pong");
+   //        }
+   //    }
+   //}
