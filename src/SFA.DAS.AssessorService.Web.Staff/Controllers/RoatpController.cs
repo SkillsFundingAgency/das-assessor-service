d686288c39cf393cd06c37de3fca55f53f8c09f6
Dave Read <david.read@digital.education.gov.uk>
2019-02-15 17:03:29 +0000

    APR-161 add handling for RoATP admin role, non functional landing page for demo

---
+++ src/SFA.DAS.AssessorService.Web.Staff/Controllers/RoatpController.cs
@@ -0,0 +1,15 @@
+﻿namespace SFA.DAS.AssessorService.Web.Staff.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class RoatpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
