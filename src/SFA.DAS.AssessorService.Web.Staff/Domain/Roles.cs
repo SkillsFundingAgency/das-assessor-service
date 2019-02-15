a600d69bf53623961c7f4c98ab0adea48335c4b9
Dave Read <david.read@digital.education.gov.uk>
2019-02-15 17:03:29 +0000

    APR-161 add handling for RoATP admin role, non functional landing page for demo

--- src/SFA.DAS.AssessorService.Web.Staff/Domain/Roles.cs
+++ src/SFA.DAS.AssessorService.Web.Staff/Domain/Roles.cs
@@ -11,6 +11,7 @@ namespace SFA.DAS.AssessorService.Web.Staff.Domain
         public const string AssessmentDeliveryTeam = "EPA"; // AAD
         public const string ProviderRiskAssuranceTeam = "EPR"; // FHA
         public const string RegisterViewOnlyTeam = "EPV";
+        public const string RoatpGatewayTeam = "APR";
         
         public static bool HasValidRole(this ClaimsPrincipal user)
         {
@@ -18,8 +19,13 @@ namespace SFA.DAS.AssessorService.Web.Staff.Domain
                    || user.IsInRole(OperationsTeam)
                    || user.IsInRole(AssessmentDeliveryTeam)
                    || user.IsInRole(ProviderRiskAssuranceTeam)
-                   || user.IsInRole(RegisterViewOnlyTeam);
+                   || user.IsInRole(RegisterViewOnlyTeam)
+                   || user.IsInRole(RoatpGatewayTeam);
+        }
 
+        public static bool HasRoatpRole(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(RoatpGatewayTeam);
         }
     }
 }
