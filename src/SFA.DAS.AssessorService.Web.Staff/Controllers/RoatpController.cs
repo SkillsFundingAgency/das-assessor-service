e99212aeba04bc2dc4ed5f00232ea2fa239ede94
Dave Read <david.read@digital.education.gov.uk>
2019-02-19 11:28:09 +0000

    APR-162 retrieve complete register and history from RoATP API for download as Excel spreadsheet

--- src/SFA.DAS.AssessorService.Web.Staff/Controllers/RoatpController.cs
+++ src/SFA.DAS.AssessorService.Web.Staff/Controllers/RoatpController.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Threading.Tasks;
-    using Helpers;
     using Infrastructure;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
