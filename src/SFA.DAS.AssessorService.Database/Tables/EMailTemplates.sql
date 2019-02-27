a23a76ee5ecd1e0d99db13343d044d43d436e232
ArshedMust <arshed@air2wire.com>
2019-02-27 14:08:38 +0000

    Updated database tables with non destructive updates

--- src/SFA.DAS.AssessorService.Database/Tables/EMailTemplates.sql
+++ src/SFA.DAS.AssessorService.Database/Tables/EMailTemplates.sql
@@ -2,7 +2,7 @@
 	[Id] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
 	[TemplateName] [nvarchar](max) NOT NULL,
 	[TemplateId]  [nvarchar](max) NOT NULL,
-	[Recipients]  [nvarchar](max) NOT NULL,
+	[Recipients]  [nvarchar](max) NULL,
 	[CreatedAt] [datetime2](7) NOT NULL,
 	[DeletedAt] [datetime2](7) NULL,	
 	[UpdatedAt] [datetime2](7) NULL,	
