aa7416d877977431b23b07bd9064051cb31329f8
ArshedMust <arshed@air2wire.com>
2019-03-05 16:17:10 +0000

    Added validation to the search and result screens

--- src/SFA.DAS.AssessorService.Web/Controllers/OrganisationSearchController.cs
+++ src/SFA.DAS.AssessorService.Web/Controllers/OrganisationSearchController.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using NLog;
-using SFA.DAS.Apprenticeships.Api.Types.Exceptions;
 using SFA.DAS.AssessorService.Api.Types.Models;
 using SFA.DAS.AssessorService.Application.Api.Client.Clients;
 using SFA.DAS.AssessorService.Domain.Consts;
@@ -15,21 +13,22 @@ using SFA.DAS.AssessorService.Web.ViewModels.Organisation;
 
 namespace SFA.DAS.AssessorService.Web.Controllers
 {
+    [Authorize]
     public class OrganisationSearchController : Controller
     {
         private readonly IContactsApiClient _contactsApiClient;
         private readonly IHttpContextAccessor _contextAccessor;
         private readonly IOrganisationsApiClient _organisationsApiClient;
-        private readonly IContactsApiClient _contactApiClient;
         private readonly ILogger<OrganisationSearchController> _logger;
 
-        public OrganisationSearchController(ILogger<OrganisationSearchController> logger,IContactsApiClient contactsApiClient, IHttpContextAccessor contextAccessor,IOrganisationsApiClient organisationsApiClient, IContactsApiClient contactApiClient)
+        public OrganisationSearchController(ILogger<OrganisationSearchController> logger,
+            IHttpContextAccessor contextAccessor, IOrganisationsApiClient organisationsApiClient,
+            IContactsApiClient contactApiClient)
         {
             _logger = logger;
-            _contactsApiClient = contactsApiClient;
             _contextAccessor = contextAccessor;
             _organisationsApiClient = organisationsApiClient;
-            _contactApiClient = contactApiClient;
+            _contactsApiClient = contactApiClient;
         }
 
         [HttpGet]
@@ -37,26 +36,49 @@ namespace SFA.DAS.AssessorService.Web.Controllers
         {
             return View();
         }
-        
+
         [HttpGet]
         public async Task<IActionResult> Results(OrganisationSearchViewModel viewModel)
         {
             var signinId = _contextAccessor.HttpContext.User.Claims.First(c => c.Type == "sub")?.Value;
             var user = await _contactsApiClient.GetContactBySignInId(signinId);
 
-            if (user.EndPointAssessorOrganisationId != null && user.OrganisationId != null && user.Status == ContactStatus.Live)
+            if (user.EndPointAssessorOrganisationId != null && user.OrganisationId != null &&
+                user.Status == ContactStatus.Live)
                 return RedirectToAction("Index", "Dashboard");
 
             if (string.IsNullOrEmpty(viewModel.SearchString) || viewModel.SearchString.Length < 2)
             {
                 ModelState.AddModelError(nameof(viewModel.SearchString), "Enter a valid search string");
                 TempData["ShowErrors"] = true;
-                return RedirectToAction(nameof(Index));
+                return View(nameof(Index));
             }
 
             var apiResponse = await _organisationsApiClient.SearchForOrganisations(viewModel.SearchString);
             viewModel.Organisations = apiResponse?.GroupBy(r => r.Ukprn).Select(group => @group.First()).ToList();
             return View(viewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FromResults(OrganisationSearchViewModel viewModel)
+        {
+            var signinId = _contextAccessor.HttpContext.User.Claims.First(c => c.Type == "sub")?.Value;
+            var user = await _contactsApiClient.GetContactBySignInId(signinId);
+
+            if (user.EndPointAssessorOrganisationId != null && user.OrganisationId != null &&
+                user.Status == ContactStatus.Live)
+                return RedirectToAction("Index", "Dashboard");
+
+            if (string.IsNullOrEmpty(viewModel.SearchString) || viewModel.SearchString.Length < 2)
+            {
+                ModelState.AddModelError(nameof(viewModel.SearchString), "Enter a valid search string");
+                TempData["ShowErrors"] = true;
+                return View(nameof(Results), viewModel);
+            }
+
+            var apiResponse = await _organisationsApiClient.SearchForOrganisations(viewModel.SearchString);
+            viewModel.Organisations = apiResponse?.GroupBy(r => r.Ukprn).Select(group => @group.First()).ToList();
+            return View(nameof(Results), viewModel);
         }
 
         [HttpPost]
@@ -66,7 +88,8 @@ namespace SFA.DAS.AssessorService.Web.Controllers
             var signinId = _contextAccessor.HttpContext.User.Claims.First(c => c.Type == "sub")?.Value;
             var user = await _contactsApiClient.GetContactBySignInId(signinId);
 
-            if (user.EndPointAssessorOrganisationId != null && user.OrganisationId != null && user.Status == ContactStatus.Live )
+            if (user.EndPointAssessorOrganisationId != null && user.OrganisationId != null &&
+                user.Status == ContactStatus.Live)
                 return RedirectToAction("Index", "Dashboard");
 
             if (string.IsNullOrEmpty(viewModel.Name) || viewModel.SearchString.Length < 2)
@@ -75,15 +98,18 @@ namespace SFA.DAS.AssessorService.Web.Controllers
                 TempData["ShowErrors"] = true;
                 return RedirectToAction(nameof(Index));
             }
-            var organisationSearchResult = await GetOrganisation(viewModel.SearchString, viewModel.Name, viewModel.Ukprn, viewModel.OrganisationType, viewModel.Postcode);
+
+            var organisationSearchResult = await GetOrganisation(viewModel.SearchString, viewModel.Name,
+                viewModel.Ukprn, viewModel.OrganisationType, viewModel.Postcode);
 
             if (organisationSearchResult != null)
             {
-                viewModel.Organisations = new List<OrganisationSearchResult> { organisationSearchResult };
+                viewModel.Organisations = new List<OrganisationSearchResult> {organisationSearchResult};
                 viewModel.OrganisationTypes = await _organisationsApiClient.GetOrganisationTypes();
             }
+
             return View(viewModel);
-           
+
         }
 
         [HttpGet]
@@ -93,22 +119,23 @@ namespace SFA.DAS.AssessorService.Web.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(OrganisationSearchViewModel viewModel)
+        public async Task<IActionResult> OrganisationChosen(OrganisationSearchViewModel viewModel)
         {
-            var organisationSearchResult = await GetOrganisation(viewModel.SearchString, viewModel.Name, viewModel.Ukprn, viewModel.OrganisationType, viewModel.Postcode);
+            var organisationSearchResult = await GetOrganisation(viewModel.SearchString, viewModel.Name,
+                viewModel.Ukprn, viewModel.OrganisationType, viewModel.Postcode);
             if (organisationSearchResult != null)
             {
-                TempData["EpaoId"] = organisationSearchResult.OrganisationReferenceId;
-                return View(nameof(NoAccess), viewModel);
+                if (organisationSearchResult.OrganisationReferenceType == "RoEPAO")
+                {
+                    TempData["EpaoId"] = organisationSearchResult.OrganisationReferenceId;
+                    return View(nameof(NoAccess), viewModel);
+                }
+                
             }
-               
-           viewModel.OrganisationTypes = await _organisationsApiClient.GetOrganisationTypes();
-
-           //Forward to apply
-
-            return View(nameof(DealingWithRequest), viewModel);
+            viewModel.OrganisationTypes = await _organisationsApiClient.GetOrganisationTypes();
+            return View(nameof(Type), viewModel);
         }
-        
+
 
         [HttpPost]
         public async Task<IActionResult> DealingWithRequest(OrganisationSearchViewModel viewModel)
@@ -116,29 +143,45 @@ namespace SFA.DAS.AssessorService.Web.Controllers
             var signinId = _contextAccessor.HttpContext.User.Claims.First(c => c.Type == "sub")?.Value;
             var user = await _contactsApiClient.GetContactBySignInId(signinId);
 
-            if (user.EndPointAssessorOrganisationId != null && user.OrganisationId != null && user.Status == ContactStatus.Live)
+            if (user.EndPointAssessorOrganisationId != null && user.OrganisationId != null &&
+                user.Status == ContactStatus.Live)
                 return RedirectToAction("Index", "Dashboard");
 
-            var organisationSearchResult = await GetOrganisation(viewModel.SearchString, viewModel.Name, viewModel.Ukprn, viewModel.OrganisationType, viewModel.Postcode);
-            if (organisationSearchResult != null)
+            var organisationSearchResult = await GetOrganisation(viewModel.SearchString, viewModel.Name,
+                viewModel.Ukprn, viewModel.OrganisationType, viewModel.Postcode);
+            if (organisationSearchResult != null && organisationSearchResult.OrganisationReferenceType == "RoEPAO")
             {
-                var registeredOrganisation =  await _organisationsApiClient.Get(organisationSearchResult.Ukprn?.ToString());
-                if (registeredOrganisation != null)
-                {
-                    await _contactsApiClient.UpdateOrgAndStatus(new UpdateContactWithOrgAndStausRequest(
-                        user.Id.ToString(),
-                        registeredOrganisation.Id.ToString(),
-                        organisationSearchResult.Id,
-                        ContactStatus.InvitePending));
-                    await _organisationsApiClient.SendEmailsToOrgApprovedUsers( new EmailAllApprovedContactsRequest(user.DisplayName, organisationSearchResult
+                var registeredOrganisation =
+                    await _organisationsApiClient.Get(organisationSearchResult.Ukprn?.ToString());
+
+                await _contactsApiClient.UpdateOrgAndStatus(new UpdateContactWithOrgAndStausRequest(
+                    user.Id.ToString(),
+                    registeredOrganisation.Id.ToString(),
+                    organisationSearchResult.Id,
+                    ContactStatus.InvitePending));
+                await _organisationsApiClient.SendEmailsToOrgApprovedUsers(new EmailAllApprovedContactsRequest(
+                    user.DisplayName, organisationSearchResult
                         .OrganisationReferenceId));
-                }
             }
 
             return View(viewModel);
         }
 
-        private async Task<OrganisationSearchResult> GetOrganisation(string searchString, string name, int? ukprn, string organisationType, string postcode)
+        [HttpPost]
+        public async Task<IActionResult> Type(OrganisationSearchViewModel viewModel)
+        {
+            if (string.IsNullOrEmpty(viewModel.Name) || viewModel.SearchString.Length < 2)
+            {
+                ModelState.AddModelError(nameof(viewModel.Name), "Enter a valid search string");
+                TempData["ShowErrors"] = true;
+                return RedirectToAction(nameof(Index));
+            }
+
+            viewModel.OrganisationTypes = await _organisationsApiClient.GetOrganisationTypes();
+            return View(viewModel);
+        }
+        private async Task<OrganisationSearchResult> GetOrganisation(string searchString, string name, int? ukprn,
+            string organisationType, string postcode)
         {
             var searchResults = await _organisationsApiClient.SearchForOrganisations(searchString);
             // filter ukprn
@@ -155,7 +198,8 @@ namespace SFA.DAS.AssessorService.Web.Controllers
 
             // filter postcode
             searchResults = searchResults.Where(sr =>
-                string.IsNullOrEmpty(postcode) || (sr.Address?.Postcode.Equals(postcode, StringComparison.InvariantCultureIgnoreCase) ?? true));
+                string.IsNullOrEmpty(postcode) ||
+                (sr.Address?.Postcode.Equals(postcode, StringComparison.InvariantCultureIgnoreCase) ?? true));
 
             var organisationSearchResult = searchResults.FirstOrDefault();
 
@@ -167,8 +211,6 @@ namespace SFA.DAS.AssessorService.Web.Controllers
 
             return organisationSearchResult;
         }
-
-
 
     }
 }
