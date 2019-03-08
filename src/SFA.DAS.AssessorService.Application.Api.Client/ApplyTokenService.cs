68bc13becbccd1873e72344b71957da7b9ddcbf7
ArshedMust <arshed@air2wire.com>
2019-03-08 15:45:56 +0000

    Removed unused imports

--- src/SFA.DAS.AssessorService.Application.Api.Client/ApplyTokenService.cs
+++ src/SFA.DAS.AssessorService.Application.Api.Client/ApplyTokenService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using SFA.DAS.AssessorService.Settings;
+﻿using SFA.DAS.AssessorService.Settings;
 
 namespace SFA.DAS.AssessorService.Application.Api.Client
 {
