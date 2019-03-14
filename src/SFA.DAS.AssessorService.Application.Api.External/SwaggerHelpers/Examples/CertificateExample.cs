49b47d14893062e7f7ebbdb567654832ecf08361
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-03-14 09:59:41 +0000

    ON-1593 - Updated swagger example

--- src/SFA.DAS.AssessorService.Application.Api.External/SwaggerHelpers/Examples/CertificateExample.cs
+++ src/SFA.DAS.AssessorService.Application.Api.External/SwaggerHelpers/Examples/CertificateExample.cs
@@ -13,7 +13,7 @@ namespace SFA.DAS.AssessorService.Application.Api.External.SwaggerHelpers.Exampl
                 CertificateData = new CertificateData
                 {
                     CertificateReference = "09876543",
-                    Standard = new Standard { StandardCode = 1, Level = 1, StandardName = "Example Standard" },
+                    Standard = new Standard { StandardCode = 1, StandardReference = "ST0001", Level = 1, StandardName = "Example Standard" },
                     Learner = new Learner { GivenNames = "John", FamilyName = "Smith", Uln = 1234567890 },
                     LearningDetails = new LearningDetails { CourseOption = "French", OverallGrade = "Pass", AchievementDate = DateTime.UtcNow, LearningStartDate = DateTime.UtcNow.AddYears(-1), ProviderUkPrn = 123456, ProviderName = "Example Provider" },
                     PostalContact = new PostalContact { ContactName = "Shreya Smith", Department = "Human Resources", Organisation = "Contoso Ltd", AddressLine1 = "123 Test Road", AddressLine2 = "Green Park", City = "Townsville", PostCode = "ZY9 9ZZ" }
