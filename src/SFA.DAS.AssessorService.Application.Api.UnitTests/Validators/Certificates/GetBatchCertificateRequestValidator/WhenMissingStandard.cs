9b4ee929a091e388facdd4e7a1ea91258d66607e
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-03-14 12:54:07 +0000

    ON-1593 - Added missing Standard test

---
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/Validators/Certificates/GetBatchCertificateRequestValidator/WhenMissingStandard.cs
@@ -0,0 +1,33 @@
+﻿using FizzWare.NBuilder;
+using FluentAssertions;
+using FluentValidation.Results;
+using NUnit.Framework;
+using SFA.DAS.AssessorService.Api.Types.Models.Certificates.Batch;
+
+namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Validators.Certificates.GetBatchCertificateRequestValidator
+{
+    public class WhenMissingStandard : GetBatchCertificateRequestValidatorTestBase
+    {
+        private ValidationResult _validationResult;
+
+        [SetUp]
+        public void Arrange()
+        {
+            GetBatchCertificateRequest request = Builder<GetBatchCertificateRequest>.CreateNew()
+                .With(i => i.Uln = 1234567890)
+                .With(i => i.StandardCode = 0)
+                .With(i => i.StandardReference = null)
+                .With(i => i.UkPrn = 12345678)
+                .With(i => i.FamilyName = "Test")
+                .Build();
+
+            _validationResult = Validator.Validate(request);
+        }
+
+        [Test]
+        public void ThenValidationResultShouldBeFalse()
+        {
+            _validationResult.IsValid.Should().BeFalse();
+        }
+    }
+}
