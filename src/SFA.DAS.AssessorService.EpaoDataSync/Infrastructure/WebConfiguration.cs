241e5099f68b257a74b891250370e4483d9237f6
ArshedMust <arshed@air2wire.com>
2019-03-14 16:18:44 +0000

    Fixed reference issue

--- src/SFA.DAS.AssessorService.EpaoDataSync/Infrastructure/WebConfiguration.cs
+++ src/SFA.DAS.AssessorService.EpaoDataSync/Infrastructure/WebConfiguration.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using SFA.DAS.AssessorService.EpaoDataSync.Infrastructure.Settings;
 using SFA.DAS.AssessorService.Settings;
 using ProviderEventsClientConfiguration = SFA.DAS.AssessorService.EpaoDataSync.Infrastructure.Settings.ProviderEventsClientConfiguration;
 
