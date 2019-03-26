55745244381ff119f5febcc0907b154218ef5c15
David Gouge <davegouge@googlemail.com>
2019-03-26 14:20:21 +0000

    Added migration for existing users to ASLogin

---
+++ src/SFA.DAS.AssessorService.Web/Controllers/MigrateUsersController.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AssessorService.Application.Api.Client.Clients;
+
+namespace SFA.DAS.AssessorService.Web.Controllers
+{
+    public class MigrateUsersController : Controller
+    {
+        private readonly IContactsApiClient _contacts;
+
+        public MigrateUsersController(IContactsApiClient contacts)
+        {
+            _contacts = contacts;
+        }
+
+        [HttpPost("/MigrateUsers")]
+        public async Task<IActionResult> MigrateUsers()
+        {
+            await _contacts.MigrateUsers();
+            return Ok();
+        }
+    }
+}
