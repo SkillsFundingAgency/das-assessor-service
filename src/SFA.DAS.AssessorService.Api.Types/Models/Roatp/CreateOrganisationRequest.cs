e4fcf5a610f41f580fd59f264488fed5191bb089
MarkFCain <mark.cain@digital.education.gov.uk>
2019-04-05 12:08:29 +0100

    APR-388 added code to deal with slightly changed endpoint

--- src/SFA.DAS.AssessorService.Api.Types/Models/Roatp/CreateOrganisationRequest.cs
+++ src/SFA.DAS.AssessorService.Api.Types/Models/Roatp/CreateOrganisationRequest.cs
@@ -11,14 +11,12 @@ namespace SFA.DAS.AssessorService.Api.Types.Models.Roatp
         public long Ukprn { get; set; }
         public string LegalName { get; set; }
         public string TradingName { get; set; }
-        public int OrganisationStatusId { get; set; }
         public DateTime StatusDate { get; set; }
         public string CharityNumber { get; set; }
         public string CompanyNumber { get; set; }
         public bool ParentCompanyGuarantee { get; set; }
         public bool FinancialTrackRecord { get; set; }
         public bool NonLevyContract { get; set; }
-        public DateTime? StartDate { get; set; }
 
         public string Username { get; set; }
     }
