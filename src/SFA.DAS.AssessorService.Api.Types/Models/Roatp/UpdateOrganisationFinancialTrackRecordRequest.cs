4922fd8275121625301cd8ca2836fe81b2adc9f9
Dave Read <david.read@digital.education.gov.uk>
2019-04-09 12:53:50 +0100

    APR-215 replaced private mapping methods with AutoMapper calls

--- src/SFA.DAS.AssessorService.Api.Types/Models/Roatp/UpdateOrganisationFinancialTrackRecordRequest.cs
+++ src/SFA.DAS.AssessorService.Api.Types/Models/Roatp/UpdateOrganisationFinancialTrackRecordRequest.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using MediatR;
-
-namespace SFA.DAS.AssessorService.Api.Types.Models.Roatp
+﻿namespace SFA.DAS.AssessorService.Api.Types.Models.Roatp
 {
+    using System;
+    using System.Runtime.Serialization;
+    using MediatR;
+
     public class UpdateOrganisationFinancialTrackRecordRequest: IRequest
     {
+        [DataMember]
         public Guid OrganisationId { get; set; }
+        [DataMember]
         public bool FinancialTrackRecord { get; set; }
+        [DataMember]
         public string UpdatedBy { get; set; }
     }
 }
--- src/SFA.DAS.AssessorService.Api.Types/Models/Roatp/UpdateOrganisationParentCompanyGuaranteeRequest.cs
+++ src/SFA.DAS.AssessorService.Api.Types/Models/Roatp/UpdateOrganisationParentCompanyGuaranteeRequest.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using MediatR;
-
+﻿
 namespace SFA.DAS.AssessorService.Api.Types.Models.Roatp
 {
+    using System;
+    using System.Runtime.Serialization;
+    using MediatR;
+
     public class UpdateOrganisationParentCompanyGuaranteeRequest: IRequest
     {
+        [DataMember]
         public Guid OrganisationId { get; set; }
+        [DataMember]
         public bool ParentCompanyGuarantee { get; set; }
+        [DataMember]
         public string UpdatedBy { get; set; }
     }
 }
