50a06d91e4579e9147667ebcd0b70afd8151fdd0
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-04-17 12:20:58 +0100

    ON-1781 - Warn when Inadequate FHA and no feedback

--- src/SFA.DAS.AssessorService.Web.Staff/ViewModels/Apply/Applications/ApplicationSequenceAssessmentViewModel.cs
+++ src/SFA.DAS.AssessorService.Web.Staff/ViewModels/Apply/Applications/ApplicationSequenceAssessmentViewModel.cs
@@ -16,6 +16,8 @@ namespace SFA.DAS.AssessorService.Web.Staff.ViewModels.Apply.Applications
 
         public bool HasNewFeedback { get; }
 
+        public bool HasInadequateFhaButNoFeedbackGiven { get; }
+
         public string ReturnType { get; set; }
 
         public ApplicationSequenceAssessmentViewModel(ApplicationSequence sequence)
@@ -25,6 +27,13 @@ namespace SFA.DAS.AssessorService.Web.Staff.ViewModels.Apply.Applications
             ApplicationId = sequence.ApplicationId;
             SequenceId = sequence.SequenceId;
             HasNewFeedback = sequence.Sections.Any(s => s.HasNewPageFeedback);
+
+            var fhaSection = sequence.Sections.Where(s => s.SectionId == 3).FirstOrDefault();
+
+            if(fhaSection != null && !fhaSection.HasNewPageFeedback)
+            {
+                HasInadequateFhaButNoFeedbackGiven = fhaSection.QnAData?.FinancialApplicationGrade?.SelectedGrade == FinancialApplicationSelectedGrade.Inadequate;
+            }
         }
     }
 }
