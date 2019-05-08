21ae3ff37951839ac1395e39ed5e9ecc5c9f3505
ArshedMust <arshed@air2wire.com>
2019-05-08 18:31:16 +0100

    Further minor status checks

--- src/SFA.DAS.AssessorService.Application/Handlers/Search/SearchResultExtensions.cs
+++ src/SFA.DAS.AssessorService.Application/Handlers/Search/SearchResultExtensions.cs
@@ -59,6 +59,14 @@ namespace SFA.DAS.AssessorService.Application.Handlers.Search
                 logger.LogInformation("MatchUpExistingCompletedStandards After GetCertificateLogsFor");
                 var submittedLogEntry = certificateLogs.FirstOrDefault(l => l.Status == CertificateStatus.Submitted);
                 var createdLogEntry = certificateLogs.FirstOrDefault(l => l.Status == CertificateStatus.Draft);
+                if (request.IsPrivatelyFunded)
+                {
+                    var toBeApprovedLogEntry = certificateLogs.FirstOrDefault(l => l.Status == CertificateStatus.ToBeApproved);
+                    if (toBeApprovedLogEntry == null)
+                        continue;
+                    submittedLogEntry = toBeApprovedLogEntry;
+                }
+
                 if (submittedLogEntry == null) continue;
 
                 var submittingContact = contactRepository.GetContact(submittedLogEntry.Username).Result ?? contactRepository.GetContact(certificate.UpdatedBy).Result;
