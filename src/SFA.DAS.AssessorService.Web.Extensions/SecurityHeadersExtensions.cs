8e89e74d7b42fcd8fdc66527949d90cdb14c3c11
Dave Gouge <davegouge@gmail.com>
2019-05-09 15:10:58 +0100

    Added more google domains to allowed script locations

--- src/SFA.DAS.AssessorService.Web.Extensions/SecurityHeadersExtensions.cs
+++ src/SFA.DAS.AssessorService.Web.Extensions/SecurityHeadersExtensions.cs
@@ -12,7 +12,7 @@ namespace SFA.DAS.AssessorService.Web.Extensions
                 context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
                 context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
                 context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' *.googletagmanager.com *.postcodeanywhere.co.uk; font-src 'self' data:;");
+                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' *.googletagmanager.com *.postcodeanywhere.co.uk *.google-analytics.com *.googleapis.com; font-src 'self' data:;");
                 context.Response.Headers.Add("Referrer-Policy", "strict-origin");
                 context.Response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate");
                 context.Response.Headers.Add("Pragma", "no-cache");
