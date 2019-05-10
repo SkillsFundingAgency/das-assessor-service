caa40af414fe3fd573a1b142cbe07694bb897901
MarkFCain <mark.cain@digital.education.gov.uk>
2019-05-10 15:14:17 +0100

    APR-293 code and tests added

--- src/SFA.DAS.AssessorService.Web.Staff/Validators/Roatp/UpdateOrganisationCharityNumberViewModelValidator.cs
+++ src/SFA.DAS.AssessorService.Web.Staff/Validators/Roatp/UpdateOrganisationCharityNumberViewModelValidator.cs
@@ -10,14 +10,19 @@ namespace SFA.DAS.AssessorService.Web.Staff.Validators.Roatp
     public class UpdateOrganisationCharityNumberViewModelValidator : AbstractValidator<UpdateOrganisationCharityNumberViewModel>
     {
         private IRoatpOrganisationValidator _validator;
-
-        public UpdateOrganisationCharityNumberViewModelValidator(IRoatpOrganisationValidator validator)
+        private IUpdateOrganisationCharityNumberValidator _duplicateValidator;
+        public UpdateOrganisationCharityNumberViewModelValidator(IRoatpOrganisationValidator validator, IUpdateOrganisationCharityNumberValidator duplicateValidator)
         {
             _validator = validator;
+            _duplicateValidator = duplicateValidator;
 
             RuleFor(vm => vm).Custom((vm, context) =>
             {
                 var validationErrors = _validator.IsValidCharityNumber(vm.CharityNumber);
+                if (!validationErrors.Any())
+                {
+                    validationErrors = _duplicateValidator.IsDuplicateCharityNumber(vm);
+                }
 
                 if (!validationErrors.Any()) return;
                 foreach (var error in validationErrors)
