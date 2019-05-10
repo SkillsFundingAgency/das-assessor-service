4fb4d152bc11f4e35f38cba219c5fd766ba03424
MarkFCain <mark.cain@digital.education.gov.uk>
2019-05-10 10:44:26 +0100

    APR-292 messages for duplications added

--- src/SFA.DAS.AssessorService.Web.Staff/Validators/Roatp/UpdateOrganisationCompanyNumberViewModelValidator.cs
+++ src/SFA.DAS.AssessorService.Web.Staff/Validators/Roatp/UpdateOrganisationCompanyNumberViewModelValidator.cs
@@ -7,15 +7,20 @@ namespace SFA.DAS.AssessorService.Web.Staff.Validators.Roatp
     public class UpdateOrganisationCompanyNumberViewModelValidator : AbstractValidator<UpdateOrganisationCompanyNumberViewModel>
     {
         private IRoatpOrganisationValidator _validator;
+        private IUpdateOrganisationCompanyNumberValidator _duplicateValidator;
 
-        public UpdateOrganisationCompanyNumberViewModelValidator(IRoatpOrganisationValidator validator)
+        public UpdateOrganisationCompanyNumberViewModelValidator(IRoatpOrganisationValidator validator, IUpdateOrganisationCompanyNumberValidator duplicateValidator)
         {
             _validator = validator;
-    
+            _duplicateValidator = duplicateValidator;
+
             RuleFor(vm => vm).Custom((vm, context) =>
             {
                 var validationErrors = _validator.IsValidCompanyNumber(vm.CompanyNumber);
-                
+                if (!validationErrors.Any())
+                {
+                    validationErrors = _duplicateValidator.IsDuplicateCompanyNumber(vm);
+                }
                 if (!validationErrors.Any()) return;
                 foreach (var error in validationErrors)
                 {
