897a6659aa4efd0f9fe59b361ecf19c4102b68cc
Chris Woodcock <chris@humesoftware.com>
2019-05-16 10:53:32 +0100

    Show standard when there is no effective to date; also using date & time comparison to make this consistent with other stored procedures.

--- src/SFA.DAS.AssessorService.Database/StoredProcedures/EPAO_Registered_Standards.sql
+++ src/SFA.DAS.AssessorService.Database/StoredProcedures/EPAO_Registered_Standards.sql
@@ -11,7 +11,12 @@ select
 	INNER join organisations o on os.EndPointAssessorOrganisationId = o.EndPointAssessorOrganisationId and o.EndPointAssessorOrganisationId = @EPAOID
 	LEFT outer join StandardCollation sc on os.StandardCode = sc.StandardId
 	LEFT outer join contacts c on os.ContactId = c.Id
-	WHERE os.status = 'Live' and CONVERT(DATE, os.EffectiveTo) >= CONVERT(DATE, GETDATE())
+	WHERE os.status = 'Live' 
+	and (os.effectiveTo is null OR os.EffectiveTo > GETDATE())
+	and (
+		JSON_VALUE(StandardData,'$.EffectiveTo') is null OR
+		JSON_VALUE(StandardData,'$.EffectiveTo') > GETDATE()
+	)
 	ORDER BY 'StandardName'
 	OFFSET @Skip ROWS 
     FETCH NEXT @Take ROWS ONLY
