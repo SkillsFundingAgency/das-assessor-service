b9a0f0a79be1edea2b41932c9c58a5536c056b84
ArshedMust <arshed@air2wire.com>
2019-05-31 17:15:40 +0100

    Fixed the issue with options not reseting when standard changed to standard with no options

--- src/SFA.DAS.AssessorService.Web/Controllers/CertificateBaseController.cs
+++ src/SFA.DAS.AssessorService.Web/Controllers/CertificateBaseController.cs
@@ -85,6 +85,15 @@ namespace SFA.DAS.AssessorService.Web.Controllers
                 return View(returnToIfModelNotValid, vm);
             }
 
+            if (nextAction.ActionName == "Option")
+            {
+                var sessionString = RetrieveSessionString(username);
+                if (sessionString == null)
+                    return RedirectToAction("Index", "Search");
+                if (!IsOptionSet(sessionString))
+                    certData.CourseOption = null;
+            }
+
             var updatedCertificate = vm.GetCertificateFromViewModel(certificate, certData);
 
             await CertificateApiClient.UpdateCertificate(new UpdateCertificateRequest(updatedCertificate) { Username = username, Action = action});
@@ -95,15 +104,10 @@ namespace SFA.DAS.AssessorService.Web.Controllers
             {
                 if (nextAction.ActionName == "Option")
                 {
-                    var sessionString = SessionService.Get("CertificateSession");
+                    var sessionString = RetrieveSessionString(username);
                     if (sessionString == null)
-                    {
-                        Logger.LogInformation(
-                            $"Session for CertificateOptionViewModel requested by {username} has been lost. Redirecting to Search Index");
                         return RedirectToAction("Index", "Search");
-                    }
-                    var certSession = JsonConvert.DeserializeObject<CertificateSession>(sessionString);
-                    if (certSession.Options != null && certSession.Options.Any())
+                    if (IsOptionSet(sessionString))
                         return new RedirectToActionResult("Option", "CertificateOption", new { redirecttocheck = "true", isFromStandard = true });
                 }
 
@@ -121,6 +125,25 @@ namespace SFA.DAS.AssessorService.Web.Controllers
             var properties = typeof(T).GetProperties().ToList();
 
             return properties.Aggregate("", (current, prop) => current + $"{prop.Name}: {prop.GetValue(viewModel)}, ");
+        }
+
+        private string RetrieveSessionString(string username)
+        {
+            var sessionString = SessionService.Get("CertificateSession");
+            if (sessionString == null)
+            {
+                Logger.LogInformation(
+                    $"Session for CertificateOptionViewModel requested by {username} has been lost. Redirecting to Search Index");
+            }
+            return sessionString;
+        }
+
+        private bool IsOptionSet(string sessionString)
+        {
+            var certSession = JsonConvert.DeserializeObject<CertificateSession>(sessionString);
+            if (certSession.Options == null || !certSession.Options.Any())
+                return false;
+            return true;
         }
         
     }
