e56bf44d1bc66e68105b8989b3263124e97c50f5
David Gouge <davegouge@googlemail.com>
2019-06-05 13:09:23 +0100

    *sigh* *sigh* Fixed test

--- src/SFA.DAS.AssessorService.Web.UnitTests/ManageUsersTests/InviteUserControllerTests/When_Invite_is_called.cs
+++ src/SFA.DAS.AssessorService.Web.UnitTests/ManageUsersTests/InviteUserControllerTests/When_Invite_is_called.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.AssessorService.Application.Api.Client.Clients;
+using SFA.DAS.AssessorService.Domain.Entities;
 using SFA.DAS.AssessorService.Web.Controllers.ManageUsers;
 
 namespace SFA.DAS.AssessorService.Web.UnitTests.ManageUsersTests.InviteUserControllerTests
@@ -15,7 +17,9 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.ManageUsersTests.InviteUserContr
         [Test]
         public async Task Then_ViewResult_is_returned()
         {
-            var controller = new InviteUserController(new Mock<IContactsApiClient>().Object, new Mock<IHttpContextAccessor>().Object, new Mock<IOrganisationsApiClient>().Object);
+            var contactsApiClient = new Mock<IContactsApiClient>();
+            contactsApiClient.Setup(api => api.GetPrivileges()).ReturnsAsync(new List<Privilege>());
+            var controller = new InviteUserController(contactsApiClient.Object, new Mock<IHttpContextAccessor>().Object, new Mock<IOrganisationsApiClient>().Object);
             var result = await controller.Invite();
             result.Should().BeOfType<ViewResult>();
         }
