9c8f818045de33cbea5152e8275f9aeabd6090d2
Dave Read <david.read@digital.education.gov.uk>
2019-06-10 09:26:19 +0100

    APR-416 remove staff web project and tests

--- src/SFA.DAS.AssessorService.Web.Staff/ViewModels/Roatp/OrganisationSearchViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.AssessorService.Web.Staff.ViewModels.Roatp
-{
-    public class OrganisationSearchViewModel
-    {
-        public string SearchTerm { get; set; }
-
-        public string BannerMessage { get; set; }
-    }
-}
