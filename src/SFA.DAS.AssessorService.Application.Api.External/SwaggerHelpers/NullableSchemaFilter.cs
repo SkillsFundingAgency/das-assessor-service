4dfd6e1218ab2836f303ce3142730b0fe9bc912d
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-06-13 10:11:24 +0100

    Code tidy-up

--- src/SFA.DAS.AssessorService.Application.Api.External/SwaggerHelpers/NullableSchemaFilter.cs
+++ src/SFA.DAS.AssessorService.Application.Api.External/SwaggerHelpers/NullableSchemaFilter.cs
@@ -18,7 +18,7 @@ namespace SFA.DAS.AssessorService.Application.Api.External.SwaggerHelpers
                 if (property != null && property.PropertyType.IsConstructedGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
                 {
                     schemaProperty.Value.Default = null;
-                    //schemaProperty.Value.Extensions.Add("nullable", true); <-- APIM hasn't moved onto OpenAPI 3.0 so cannot use this at the moment
+                    //schemaProperty.Value.Extensions.Add("nullable", true); <-- Swashbuckle hasn't moved onto OpenAPI 3.0 so cannot use this at the moment. It is in BETA though...
                     schemaProperty.Value.Extensions.Add("x-nullable", true);
                     schemaProperty.Value.Example = null;
                 }
