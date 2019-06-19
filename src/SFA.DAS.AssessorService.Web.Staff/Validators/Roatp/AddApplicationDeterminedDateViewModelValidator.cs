22d5bd1ca9e60f3e4fbd49fc6dff071721dd1543
MarkFCain <mark.cain@digital.education.gov.uk>
2019-06-19 15:05:03 +0100

    APR-495 final finesse

--- src/SFA.DAS.AssessorService.Web.Staff/Validators/Roatp/AddApplicationDeterminedDateViewModelValidator.cs
+++ src/SFA.DAS.AssessorService.Web.Staff/Validators/Roatp/AddApplicationDeterminedDateViewModelValidator.cs
@@ -34,60 +34,6 @@ namespace SFA.DAS.AssessorService.Web.Staff.Validators.Roatp
         {
             return _applicationDeterminedDateValidationService.ValidateApplicationDeterminedDate(viewModel.Day,
                 viewModel.Month, viewModel.Year);
-
-            //    var dateControlName = "ApplicationDeterminedDate";
-
-            //    var validationResult = new ValidationResponse
-            //    {
-            //        Errors = new List<ValidationErrorDetail>()
-            //    };
-
-            //    if (viewModel.Day == null && viewModel.Month == null && viewModel.Year == null)
-            //    {
-            //        validationResult.Errors.Add(new ValidationErrorDetail(dateControlName, RoatpOrganisationValidation.ApplicationDeterminedDateNoFieldsEntered));
-            //        validationResult.Errors.Add(new ValidationErrorDetail("Day", RoatpOrganisationValidation.ApplicationDeterminedDateFutureDay));
-            //        validationResult.Errors.Add(new ValidationErrorDetail("Month", RoatpOrganisationValidation.ApplicationDeterminedDateFutureMonth));
-            //        validationResult.Errors.Add(new ValidationErrorDetail("Year", RoatpOrganisationValidation.ApplicationDeterminedDateFutureYear));
-            //        return validationResult;
-            //    }
-
-
-            //    if (string.IsNullOrWhiteSpace(viewModel?.Day.ToString()) || 
-            //            string.IsNullOrWhiteSpace(viewModel?.Month.ToString()) || 
-            //            string.IsNullOrWhiteSpace(viewModel?.Year.ToString()))
-            //        {
-            //            validationResult.Errors.Add(
-            //                new ValidationErrorDetail(dateControlName, RoatpOrganisationValidation.ApplicationDeterminedDateFieldsNotEntered));
-
-            //        if (viewModel?.Day == null)
-            //            validationResult.Errors.Add(new ValidationErrorDetail("Day", RoatpOrganisationValidation.ApplicationDeterminedDateFutureDay));
-            //        if (viewModel?.Month == null)
-            //            validationResult.Errors.Add(new ValidationErrorDetail("Month", RoatpOrganisationValidation.ApplicationDeterminedDateFutureMonth));
-            //        if (viewModel?.Year == null)
-            //            validationResult.Errors.Add(new ValidationErrorDetail("Year", RoatpOrganisationValidation.ApplicationDeterminedDateFutureYear));
-            //        return validationResult;
-            //    }
-
-            //    var formatStrings = new string[] { "d/M/yyyy" };
-            //    var yearWithCentury = viewModel?.Year;
-            //    if (yearWithCentury!=null && yearWithCentury <= 99)
-            //        yearWithCentury += 2000;
-
-            //    if (!DateTime.TryParseExact($"{viewModel?.Day}/{viewModel?.Month}/{yearWithCentury}", formatStrings, null, DateTimeStyles.None, out _))
-            //    {
-            //        validationResult.Errors.Add(
-            //            new ValidationErrorDetail(dateControlName, RoatpOrganisationValidation.ApplicationDeterminedDateInvalidDates));
-            //        return validationResult;
-            //    }
-
-            //    if (viewModel.ApplicationDeterminedDate > DateTime.Today)
-            //    {
-            //        validationResult.Errors.Add(
-            //            new ValidationErrorDetail(dateControlName, RoatpOrganisationValidation.ApplicationDeterminedDateFutureDate));
-            //    }
-
-            //    return validationResult;
-            //}
         }
     }
 }
