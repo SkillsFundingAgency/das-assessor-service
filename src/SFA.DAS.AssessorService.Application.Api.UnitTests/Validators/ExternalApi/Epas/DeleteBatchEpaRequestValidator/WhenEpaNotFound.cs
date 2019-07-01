0423be04fd0f0c951b2bccd8d7da36945b4e1b7e
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-07-01 14:17:53 +0100

    Added DeleteBatchEpaRequestValidator tests

---
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/Validators/ExternalApi/Epas/DeleteBatchEpaRequestValidator/WhenEpaNotFound.cs
@@ -0,0 +1,39 @@
+﻿using FizzWare.NBuilder;
+using FluentAssertions;
+using FluentValidation.Results;
+using NUnit.Framework;
+using SFA.DAS.AssessorService.Api.Types.Models.ExternalApi.Epas;
+using SFA.DAS.AssessorService.Domain.JsonData;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Validators.ExternalApi.Epas.DeleteBatchEpaRequestValidator
+{
+    public class WhenEpaNotFound : DeleteBatchEpaRequestValidatorTestBase
+    {
+        private ValidationResult _validationResult;
+
+        [SetUp]
+        public async Task Arrange()
+        {
+            var request = Builder<DeleteBatchEpaRequest>.CreateNew()
+                .With(i => i.Uln = 1234567890)
+                .With(i => i.StandardCode = 99)
+                .With(i => i.StandardReference = null)
+                .With(i => i.UkPrn = 12345678)
+                .With(i => i.FamilyName = "Test")
+                .With(i => i.EpaReference = "1234567890-99")
+                .Build();
+
+            _validationResult = await Validator.ValidateAsync(request);
+        }
+
+        [Test]
+        public void ThenValidationResultShouldBeFalse()
+        {
+            _validationResult.IsValid.Should().BeFalse();
+            _validationResult.Errors.Count.Should().Be(1);
+        }
+    }
+}
