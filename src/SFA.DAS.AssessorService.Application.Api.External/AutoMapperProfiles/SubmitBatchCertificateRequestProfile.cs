7c4e151f751f73b71d29c4663818f4a49610cf74
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-07-17 11:33:36 +0100

    Fixed bad conversion if StandardCode is null

--- src/SFA.DAS.AssessorService.Application.Api.External/AutoMapperProfiles/SubmitBatchCertificateRequestProfile.cs
+++ src/SFA.DAS.AssessorService.Application.Api.External/AutoMapperProfiles/SubmitBatchCertificateRequestProfile.cs
@@ -11,7 +11,7 @@ namespace SFA.DAS.AssessorService.Application.Api.External.AutoMapperProfiles
             .ForMember(dest => dest.RequestId, opt => opt.MapFrom(source => source.RequestId))
             .ForMember(dest => dest.Uln, opt => opt.MapFrom(source => source.Uln))
             .ForMember(dest => dest.FamilyName, opt => opt.MapFrom(source => source.FamilyName))
-            .ForMember(dest => dest.StandardCode, opt => opt.MapFrom(source => source.StandardCode))
+            .ForMember(dest => dest.StandardCode, opt => opt.MapFrom(source => source.StandardCode ?? 0))
             .ForMember(dest => dest.StandardReference, opt => opt.MapFrom(source => source.StandardReference))
             .ForMember(dest => dest.CertificateReference, opt => opt.MapFrom(source => source.CertificateReference))            
             .ForMember(dest => dest.UkPrn, opt => opt.MapFrom(source => source.UkPrn))
