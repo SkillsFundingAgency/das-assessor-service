caac1933b984cacaf3ce880f4c8520d1a59f62be
Alan Burns <adburns57@gmail.com>
2019-07-26 15:03:45 +0100

    Revert "On 1999 v1 print function not updating certificates"

---
+++ src/SFA.DAS.AssessorService.PrintFunction.Tests/AssessorServiceApi/WhenSystemChangesStatusesToPrinted.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using FizzWare.NBuilder;
+using NUnit.Framework;
+using RichardSzalay.MockHttp;
+using SFA.DAS.AssessorService.Api.Types.Models.Certificates;
+
+namespace SFA.DAS.AssessorService.PrintFunction.Tests.AssessorServiceApi
+{
+    public class WhenSystemChangesStatusesToPrinted : AssessorServiceTestBase
+    {
+        [SetUp]
+        public void Arrange()
+        {
+            Setup();
+
+            var certificateResponses = Builder<CertificateResponse>.CreateListOfSize(10).Build();
+            
+            MockHttp.When(HttpMethod.Put, "http://localhost:59022/api/v1/certificates/*")                         
+                .Respond(System.Net.HttpStatusCode.OK, "application/json", "{'status' : 'Boo'}");
+
+            AssessorServiceApi.ChangeStatusToPrinted(1, certificateResponses).ConfigureAwait(false);
+        }
+
+        [Test]
+        public void ThenItShouldUpdateCertificates()
+        {
+            MockHttp.VerifyNoOutstandingExpectation();
+        }
+    }
+}
