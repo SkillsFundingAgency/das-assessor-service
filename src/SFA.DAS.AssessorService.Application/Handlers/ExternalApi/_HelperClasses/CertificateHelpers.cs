fc835c1d3d0d123b2b27cc824e46844c4f14fe1c
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-08-14 15:09:30 +0100

    Added helper classes in order to refactor out commonly used code

---
+++ src/SFA.DAS.AssessorService.Application/Handlers/ExternalApi/_HelperClasses/CertificateHelpers.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using SFA.DAS.AssessorService.Api.Types.Models.Standards;
+using SFA.DAS.AssessorService.Application.Interfaces;
+using SFA.DAS.AssessorService.Domain.Consts;
+using SFA.DAS.AssessorService.Domain.Entities;
+using SFA.DAS.AssessorService.Domain.Extensions;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.AssessorService.Application.Handlers.ExternalApi._HelperClasses
+{
+    internal static class CertificateHelpers
+    {
+        public static async Task<Certificate> ApplyStatusInformation(ICertificateRepository certificateRepository, IContactQueryRepository contactQueryRepository, Certificate certificate)
+        {
+            if (certificateRepository is null || contactQueryRepository is null || certificate is null) return certificate;
+
+            var json = JsonConvert.SerializeObject(certificate);
+            var cert = JsonConvert.DeserializeObject<Certificate>(json);
+
+            var certificateLogs = await certificateRepository.GetCertificateLogsFor(cert.Id);
+            certificateLogs = certificateLogs?.Where(l => l.ReasonForChange is null).ToList(); // this removes any admin changes done within staff app
+
+            var createdLogEntry = certificateLogs?.OrderByDescending(l => l.EventTime).FirstOrDefault(l => l.Status == CertificateStatus.Draft && l.Action == CertificateActions.Start);
+            if (createdLogEntry != null)
+            {
+                var createdContact = await contactQueryRepository.GetContact(createdLogEntry.Username);
+                cert.CreatedAt = createdLogEntry.EventTime.UtcToTimeZoneTime();
+                cert.CreatedBy = createdContact != null ? createdContact.DisplayName : createdLogEntry.Username;
+            }
+
+            var submittedLogEntry = certificateLogs?.OrderByDescending(l => l.EventTime).FirstOrDefault(l => l.Status == CertificateStatus.Submitted);
+
+            // NOTE: THIS IS A DATA FRIG FOR EXTERNAL API AS WE NEED SUBMITTED INFORMATION!
+            if (submittedLogEntry != null)
+            {
+                var submittedContact = await contactQueryRepository.GetContact(submittedLogEntry.Username);
+                cert.UpdatedAt = submittedLogEntry.EventTime.UtcToTimeZoneTime();
+                cert.UpdatedBy = submittedContact != null ? submittedContact.DisplayName : submittedLogEntry.Username;
+            }
+            else
+            {
+                cert.UpdatedAt = null;
+                cert.UpdatedBy = null;
+            }
+
+            return cert;
+        }
+
+        public static string NormalizeOverallGrade(string overallGrade)
+        {
+            var grades = new string[] { CertificateGrade.Pass, CertificateGrade.Credit, CertificateGrade.Merit, CertificateGrade.Distinction, CertificateGrade.PassWithExcellence, CertificateGrade.NoGradeAwarded };
+            return grades.FirstOrDefault(g => g.Equals(overallGrade, StringComparison.InvariantCultureIgnoreCase)) ?? overallGrade;
+        }
+
+        public static string NormalizeCourseOption(StandardCollation standard, string courseOption)
+        {
+            if (standard.Options is null)
+            {
+                return courseOption;
+            }
+            else
+            {
+                return standard.Options.FirstOrDefault(g => g.Equals(courseOption, StringComparison.InvariantCultureIgnoreCase)) ?? courseOption;
+            }
+        }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Application/Handlers/ExternalApi/_HelperClasses/EpaHelpers.cs
@@ -0,0 +1,15 @@
+﻿using SFA.DAS.AssessorService.Domain.Consts;
+using System;
+using System.Linq;
+
+namespace SFA.DAS.AssessorService.Application.Handlers.ExternalApi._HelperClasses
+{
+    internal static class EpaHelpers
+    {
+        public static string NormalizeEpaOutcome(string epaOutcome)
+        {
+            var outcomes = new string[] { EpaOutcome.Pass, EpaOutcome.Fail, EpaOutcome.Withdrawn };
+            return outcomes.FirstOrDefault(g => g.Equals(epaOutcome, StringComparison.InvariantCultureIgnoreCase)) ?? epaOutcome;
+        }
+    }
+}
