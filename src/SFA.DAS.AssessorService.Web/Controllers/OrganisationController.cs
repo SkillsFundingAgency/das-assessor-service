685811033dca10f083caaff3c37e60a74e35f3f4
Greg Barnes <gregdbarnes@gmail.com>
2019-08-23 10:48:55 +0100

    Add tabs, include link fragments where relevant (to link to specific tabbed content), styling

--- src/SFA.DAS.AssessorService.Web/Controllers/OrganisationController.cs
+++ src/SFA.DAS.AssessorService.Web/Controllers/OrganisationController.cs
@@ -19,142 +19,142 @@ using SFA.DAS.AssessorService.Web.ViewModels;
 
 namespace SFA.DAS.AssessorService.Web.Controllers
 {
-    [Authorize]
-    [CheckSession]
-    public class OrganisationController : Controller
+  [Authorize]
+  [CheckSession]
+  public class OrganisationController : Controller
+  {
+    private readonly IHttpContextAccessor _contextAccessor;
+    private readonly IOrganisationsApiClient _apiClient;
+    private readonly IAzureApiClient _externalApiClient;
+    private readonly ILogger<OrganisationController> _logger;
+    private readonly IWebConfiguration _webConfiguration;
+
+    public OrganisationController(ILogger<OrganisationController> logger, IHttpContextAccessor contextAccessor, IWebConfiguration webConfiguration, IOrganisationsApiClient apiClient, IAzureApiClient externalApiClient)
     {
-        private readonly IHttpContextAccessor _contextAccessor;
-        private readonly IOrganisationsApiClient _apiClient;
-        private readonly IAzureApiClient _externalApiClient;
-        private readonly ILogger<OrganisationController> _logger;
-        private readonly IWebConfiguration _webConfiguration;        
-
-        public OrganisationController(ILogger<OrganisationController> logger, IHttpContextAccessor contextAccessor, IWebConfiguration webConfiguration, IOrganisationsApiClient apiClient, IAzureApiClient externalApiClient)
-        {
-            _contextAccessor = contextAccessor;
-            _webConfiguration = webConfiguration;
-            _apiClient = apiClient;
-            _externalApiClient = externalApiClient;
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var ukprn = _contextAccessor.HttpContext.User.FindFirst("http://schemas.portal.com/ukprn")?.Value;
-            var epaoid = _contextAccessor.HttpContext.User.FindFirst("http://schemas.portal.com/epaoid")?.Value;
-
-            OrganisationResponse organisation;
-
-            try
-            {
-                if (ukprn != null)
-                    organisation = await _apiClient.Get(ukprn);
-                else
-                {
-                    var epaOrganisation = await _apiClient.GetEpaOrganisation(epaoid);
-                    organisation = new OrganisationResponse
-                    {
-                        EndPointAssessorName = epaOrganisation.Name,
-                        EndPointAssessorOrganisationId = epaoid,
-                        EndPointAssessorUkprn = null,
-                        Id = epaOrganisation.Id,
-                        PrimaryContact = epaOrganisation.PrimaryContact,
-                        Status = epaOrganisation.Status
-                    };
-                }
-            }
-            catch (EntityNotFoundException e)
-            {
-                _logger.LogWarning(e, "Failed to find organisation");
-                return RedirectToAction("NotRegistered", "Home");
-            }
-
-            return View(organisation);
-        }
-
-        [HttpGet]
-        [TypeFilter(typeof(MenuFilter), Arguments = new object[] { Pages.Organisations })]
-        public async Task<IActionResult> OrganisationDetails()
-        {
-            var epaoid = _contextAccessor.HttpContext.User.FindFirst("http://schemas.portal.com/epaoid")?.Value;
-            var ukprn = _contextAccessor.HttpContext.User.FindFirst("http://schemas.portal.com/ukprn")?.Value;
-
-            try
-            {
-                var organisation = await _apiClient.GetEpaOrganisation(epaoid);
-
-                var viewModel = MapOrganisationModel(organisation);
-                viewModel.ExternalApiSubscriptions = await GetExternalApiSubscriptions(_webConfiguration.AzureApiAuthentication.ProductId, ukprn);
-
-                return View(viewModel);
-            }
-            catch (EntityNotFoundException e)
-            {
-                _logger.LogWarning(e, "Failed to find organisation");
-                return RedirectToAction("NotRegistered", "Home");
-            }
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> EnableApiAccess()
-        {
-            var ukprn = _contextAccessor.HttpContext.User.Claims.FirstOrDefault(c => c.Type == "http://schemas.portal.com/ukprn")?.Value;
-            await _externalApiClient.CreateUser(ukprn);
-            return RedirectToAction(nameof(OrganisationDetails));
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> RenewApiKey(string subscriptionId)
-        {
-            await _externalApiClient.RegeneratePrimarySubscriptionKey(subscriptionId);
-            return RedirectToAction(nameof(OrganisationDetails));
-        }
-
-        private async Task<List<AzureSubscription>> GetExternalApiSubscriptions(string productId, string ukprn)
-        {
-            var users = await _externalApiClient.GetUserDetailsByUkprn(ukprn, true);      
-            return users.SelectMany(u => u.Subscriptions.Where(s => s.IsActive && s.ProductId == productId)).ToList();
-        }
-
-        private ViewAndEditOrganisationViewModel MapOrganisationModel(EpaOrganisation organisation)
-        {
-            const string notSetDescription = "Not set";
-            var viewModel = new ViewAndEditOrganisationViewModel
-            {
-                OrganisationId = organisation.OrganisationId,
-                Name = organisation.Name,
-                Ukprn = organisation.Ukprn,
-                OrganisationTypeId = organisation.OrganisationTypeId,
-                OrganisationType = notSetDescription,
-                LegalName = organisation.OrganisationData?.LegalName,
-                TradingName = organisation.OrganisationData?.TradingName,
-                WebsiteLink = organisation.OrganisationData?.WebsiteLink,
-                Address1 = organisation.OrganisationData?.Address1,
-                Address2 = organisation.OrganisationData?.Address2,
-                Address3 = organisation.OrganisationData?.Address3,
-                Address4 = organisation.OrganisationData?.Address4,
-                Postcode = organisation.OrganisationData?.Postcode,
-                PrimaryContact = !string.IsNullOrEmpty(organisation.PrimaryContact)
-                    ? organisation.PrimaryContact
-                    : notSetDescription,
-                PrimaryContactName = !string.IsNullOrEmpty(organisation.PrimaryContactName)
-                    ? organisation.PrimaryContactName
-                    : notSetDescription,
-                CharityNumber = organisation.OrganisationData?.CharityNumber,
-                CompanyNumber = organisation.OrganisationData?.CompanyNumber,
-                Status = organisation.Status,
-                OrganisationTypes = _apiClient.GetOrganisationTypes().Result
-            };
-
-
-            if (viewModel.OrganisationTypeId == null) return viewModel;
-            var organisationTypes = viewModel.OrganisationTypes;
-            viewModel.OrganisationType =
-                organisationTypes.FirstOrDefault(x => x.Id == viewModel.OrganisationTypeId)?.Type;
-
-            return viewModel;
-        }
-
+      _contextAccessor = contextAccessor;
+      _webConfiguration = webConfiguration;
+      _apiClient = apiClient;
+      _externalApiClient = externalApiClient;
+      _logger = logger;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+      var ukprn = _contextAccessor.HttpContext.User.FindFirst("http://schemas.portal.com/ukprn")?.Value;
+      var epaoid = _contextAccessor.HttpContext.User.FindFirst("http://schemas.portal.com/epaoid")?.Value;
+
+      OrganisationResponse organisation;
+
+      try
+      {
+        if (ukprn != null)
+          organisation = await _apiClient.Get(ukprn);
+        else
+        {
+          var epaOrganisation = await _apiClient.GetEpaOrganisation(epaoid);
+          organisation = new OrganisationResponse
+          {
+            EndPointAssessorName = epaOrganisation.Name,
+            EndPointAssessorOrganisationId = epaoid,
+            EndPointAssessorUkprn = null,
+            Id = epaOrganisation.Id,
+            PrimaryContact = epaOrganisation.PrimaryContact,
+            Status = epaOrganisation.Status
+          };
+        }
+      }
+      catch (EntityNotFoundException e)
+      {
+        _logger.LogWarning(e, "Failed to find organisation");
+        return RedirectToAction("NotRegistered", "Home");
+      }
+
+      return View(organisation);
+    }
+
+    [HttpGet]
+    [TypeFilter(typeof(MenuFilter), Arguments = new object[] { Pages.Organisations })]
+    public async Task<IActionResult> OrganisationDetails()
+    {
+      var epaoid = _contextAccessor.HttpContext.User.FindFirst("http://schemas.portal.com/epaoid")?.Value;
+      var ukprn = _contextAccessor.HttpContext.User.FindFirst("http://schemas.portal.com/ukprn")?.Value;
+
+      try
+      {
+        var organisation = await _apiClient.GetEpaOrganisation(epaoid);
+
+        var viewModel = MapOrganisationModel(organisation);
+        viewModel.ExternalApiSubscriptions = await GetExternalApiSubscriptions(_webConfiguration.AzureApiAuthentication.ProductId, ukprn);
+
+        return View(viewModel);
+      }
+      catch (EntityNotFoundException e)
+      {
+        _logger.LogWarning(e, "Failed to find organisation");
+        return RedirectToAction("NotRegistered", "Home");
+      }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EnableApiAccess()
+    {
+      var ukprn = _contextAccessor.HttpContext.User.Claims.FirstOrDefault(c => c.Type == "http://schemas.portal.com/ukprn")?.Value;
+      await _externalApiClient.CreateUser(ukprn);
+      return RedirectToAction(nameof(OrganisationDetails));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RenewApiKey(string subscriptionId)
+    {
+      await _externalApiClient.RegeneratePrimarySubscriptionKey(subscriptionId);
+      return RedirectToAction(nameof(OrganisationDetails), "Organisation", "api-subscription");
+    }
+
+    private async Task<List<AzureSubscription>> GetExternalApiSubscriptions(string productId, string ukprn)
+    {
+      var users = await _externalApiClient.GetUserDetailsByUkprn(ukprn, true);
+      return users.SelectMany(u => u.Subscriptions.Where(s => s.IsActive && s.ProductId == productId)).ToList();
+    }
+
+    private ViewAndEditOrganisationViewModel MapOrganisationModel(EpaOrganisation organisation)
+    {
+      const string notSetDescription = "Not set";
+      var viewModel = new ViewAndEditOrganisationViewModel
+      {
+        OrganisationId = organisation.OrganisationId,
+        Name = organisation.Name,
+        Ukprn = organisation.Ukprn,
+        OrganisationTypeId = organisation.OrganisationTypeId,
+        OrganisationType = notSetDescription,
+        LegalName = organisation.OrganisationData?.LegalName,
+        TradingName = organisation.OrganisationData?.TradingName,
+        WebsiteLink = organisation.OrganisationData?.WebsiteLink,
+        Address1 = organisation.OrganisationData?.Address1,
+        Address2 = organisation.OrganisationData?.Address2,
+        Address3 = organisation.OrganisationData?.Address3,
+        Address4 = organisation.OrganisationData?.Address4,
+        Postcode = organisation.OrganisationData?.Postcode,
+        PrimaryContact = !string.IsNullOrEmpty(organisation.PrimaryContact)
+              ? organisation.PrimaryContact
+              : notSetDescription,
+        PrimaryContactName = !string.IsNullOrEmpty(organisation.PrimaryContactName)
+              ? organisation.PrimaryContactName
+              : notSetDescription,
+        CharityNumber = organisation.OrganisationData?.CharityNumber,
+        CompanyNumber = organisation.OrganisationData?.CompanyNumber,
+        Status = organisation.Status,
+        OrganisationTypes = _apiClient.GetOrganisationTypes().Result
+      };
+
+
+      if (viewModel.OrganisationTypeId == null) return viewModel;
+      var organisationTypes = viewModel.OrganisationTypes;
+      viewModel.OrganisationType =
+          organisationTypes.FirstOrDefault(x => x.Id == viewModel.OrganisationTypeId)?.Type;
+
+      return viewModel;
+    }
+
+  }
 }
