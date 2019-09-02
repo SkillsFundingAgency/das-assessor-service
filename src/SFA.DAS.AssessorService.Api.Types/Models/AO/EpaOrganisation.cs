e028dfd4f4e17e27795e1287a1ec9e5f74ca9c14
Chris Woodcock <chris@humesoftware.com>
2019-09-02 10:08:56 +0100

    Removed old code

--- src/SFA.DAS.AssessorService.Api.Types/Models/AO/EpaOrganisation.cs
+++ src/SFA.DAS.AssessorService.Api.Types/Models/AO/EpaOrganisation.cs
@@ -5,7 +5,7 @@ using System;
 namespace SFA.DAS.AssessorService.Api.Types.Models.AO
 {
     [AuditFilter(typeof(EpaOrganisationAuditFilter))]
-    public class EpaOrganisation //: IAuditFilter
+    public class EpaOrganisation
     {
         public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; }
@@ -21,15 +21,5 @@ namespace SFA.DAS.AssessorService.Api.Types.Models.AO
         public OrganisationData OrganisationData { get; set; }
         public bool ApiEnabled { get; set; }
         public string ApiUser { get; set; }
-
-        /*public string FilterAuditDiff(string propertyChanged)
-        {
-            if (propertyChanged == $"{nameof(OrganisationData)}.{nameof(OrganisationData.PhoneNumber)}")
-            {
-                return "Contact number";
-            }
-
-            return null;
-        }*/
     }
 }
