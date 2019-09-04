52cb63059948d1b6616f80984f306eb84897b9bc
Chris Woodcock <chris@humesoftware.com>
2019-09-04 18:44:46 +0100

    Layout changes for opportunity finder index page

---
+++ src/SFA.DAS.AssessorService.Web/Controllers/OppFinder/OppFinderController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.AssessorService.Web.Controllers.OppFinder
+{
+    public class OppFinderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
