d6cc5cd05d7780667d9b02be3389b214cb9f6e0f
ArshedMust <arshed@air2wire.com>
2019-09-17 17:06:59 +0100

    Removed reduntant fields in Financial Grade Json

--- src/SFA.DAS.AssessorService.ApplyTypes/FinancialGrade.cs
+++ src/SFA.DAS.AssessorService.ApplyTypes/FinancialGrade.cs
@@ -8,29 +8,10 @@ namespace SFA.DAS.AssessorService.ApplyTypes
         public string ApplicationReference { get; set; }
         public string SelectedGrade { get; set; }
         public string InadequateMoreInformation { get; set; }
-
-        public FinancialDueDate OutstandingFinancialDueDate { get; set; }
-        public FinancialDueDate GoodFinancialDueDate { get; set; }
-        public FinancialDueDate SatisfactoryFinancialDueDate { get; set; }
-
         public DateTime? FinancialDueDate { get; set; }
-        
         public string GradedBy { get; set; }
         public DateTime GradedDateTime { get; set; }
-
         public List<FinancialEvidence> FinancialEvidences { get; set; }
-    }
-
-    public class FinancialDueDate
-    {
-        public string Day { get; set; }
-        public string Month { get; set; }
-        public string Year { get; set; }
-
-        public DateTime ToDateTime()
-        {
-            return new DateTime(int.Parse(Year), int.Parse(Month), int.Parse(Day));
-        }
     }
 
     public class FinancialEvidence
