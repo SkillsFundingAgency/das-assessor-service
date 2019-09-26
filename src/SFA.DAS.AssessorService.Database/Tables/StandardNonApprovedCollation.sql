f6c883d7564d99e0c8da220fc079089244efc248
Chris Woodcock <chris@humesoftware.com>
2019-09-26 09:56:16 +0100

    Schema updates to create new non-approved standard collation table

---
+++ src/SFA.DAS.AssessorService.Database/Tables/StandardNonApprovedCollation.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[StandardNonApprovedCollation]
+(
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ReferenceNumber] [nvarchar](10) NULL,
+	[Title] [nvarchar](500) NOT NULL,
+	[StandardData] [nvarchar](max) NULL,
+	[DateAdded] DateTime NOT NULL DEFAULT GETUTCDATE(),
+	[DateUpdated] [datetime] NULL,
+	[DateRemoved] [datetime] NULL,
+	[IsLive] BIT DEFAULT 1
+	CONSTRAINT [PK_StandardNonApprovedCollation] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)
+)
+GO
