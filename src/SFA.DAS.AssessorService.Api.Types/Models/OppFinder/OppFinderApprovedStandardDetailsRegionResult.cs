942324db4ef5dbaa37dbb8ef56d2b6b55775bffb
Greg Barnes <gregdbarnes@gmail.com>
2019-09-27 14:22:29 +0100

    NonApprovedDetails and code to add 💊

--- src/SFA.DAS.AssessorService.Api.Types/Models/OppFinder/OppFinderApprovedStandardDetailsRegionResult.cs
+++ src/SFA.DAS.AssessorService.Api.Types/Models/OppFinder/OppFinderApprovedStandardDetailsRegionResult.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace SFA.DAS.AssessorService.Api.Types.Models
 {
-    public class OppFinderApprovedStandardDetailsRegionResult
-    {
-        public string Region { get; set; }
-        public string EndPointAssessorsNames { get; set; }
-        public int EndPointAssessors { get; set; }
-        public int ActiveApprentices { get; set; }
-        public int CompletedAssessments { get; set; }
-    }
+  public class OppFinderApprovedStandardDetailsRegionResult
+  {
+    public string Region { get; set; }
+    public string[] EndPointAssessorsNames { get; set; }
+    public int EndPointAssessors { get; set; }
+    public int ActiveApprentices { get; set; }
+    public int CompletedAssessments { get; set; }
+  }
 }
