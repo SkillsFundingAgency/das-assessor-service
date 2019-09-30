ac750ea2976d9bff3c15826ec32ed68294aab55b
Alan Burns <adburns57@gmail.com>
2019-09-30 10:54:50 +0100

    rename PK

--- src/SFA.DAS.AssessorService.Database/Tables/ExpressionsOfInterest.sql
+++ src/SFA.DAS.AssessorService.Database/Tables/ExpressionsOfInterest.sql
@@ -9,7 +9,7 @@ CREATE TABLE [ExpressionsOfInterest](
 	[StandardReference] [nvarchar](10) NULL,
 	[Status] [nvarchar](20) NOT NULL DEFAULT 'New',
 	[CreatedAt] [datetime2](7) NOT NULL DEFAULT GETDATE(),
- CONSTRAINT [PK_ExpressionofInterest] PRIMARY KEY  
+ CONSTRAINT [PK_ExpressionsofInterest] PRIMARY KEY  
  (
 	[Id] ASC
  )
