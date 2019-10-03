bad349c6ee9e8f188d04c9c6533d39b9933e652a
Martin Lush <martin-hornsby@ntlworld.com>
2019-10-03 10:54:41 +0100

    FHA to have Graded status when FHA Review is done

--- src/SFA.DAS.AssessorService.ApplyTypes/FinancialReviewStatus.cs
+++ src/SFA.DAS.AssessorService.ApplyTypes/FinancialReviewStatus.cs
@@ -7,6 +7,7 @@
 
         public const string New = "New";
         public const string InProgress = "In Progress";
+        public const string Graded = "Graded";
         public const string Rejected = "Rejected";
         public const string Closed = "Closed";  
     }
--- src/SFA.DAS.AssessorService.Data/Apply/ApplyRepository.cs
+++ src/SFA.DAS.AssessorService.Data/Apply/ApplyRepository.cs
@@ -111,7 +111,7 @@ namespace SFA.DAS.AssessorService.Data.Apply
             {
                 using (var connection = new SqlConnection(_configuration.SqlConnectionString))
                 {
-                    var financialReviewStatus = (financialGrade.SelectedGrade == FinancialApplicationSelectedGrade.Inadequate) ? FinancialReviewStatus.Rejected : FinancialReviewStatus.Closed;
+                    var financialReviewStatus = (financialGrade.SelectedGrade == FinancialApplicationSelectedGrade.Inadequate) ? FinancialReviewStatus.Rejected : FinancialReviewStatus.Graded;
 
                     var result = await connection.ExecuteAsync(@"UPDATE Apply
                                                 SET FinancialGrade = @financialGrade, FinancialReviewStatus = @financialReviewStatus
@@ -237,9 +237,10 @@ namespace SFA.DAS.AssessorService.Data.Apply
                             CROSS APPLY OPENJSON(ApplyData,'$.Sequences[0].Sections') WITH (SectionNo INT, Status VARCHAR(20), NotRequired BIT) section
                             CROSS APPLY OPENJSON(ApplyData,'$.Apply') WITH (ClosedDate VARCHAR(30) '$.InitSubmissionClosedDate', SubmissionCount INT '$.InitSubmissionCount') apply
                         WHERE sequence.SequenceNo = 1 AND section.SectionNo = 3 AND section.NotRequired = 0
-                            AND ap1.FinancialReviewStatus = @financialReviewStatusClosed -- NOTE: Not showing Exempt",
+                            AND ap1.FinancialReviewStatus IN (@financialReviewStatusGraded, @financialReviewStatusClosed) -- NOTE: Not showing Exempt",
                         new
                         {
+                            financialReviewStatusGraded = FinancialReviewStatus.Graded,
                             financialReviewStatusClosed = FinancialReviewStatus.Closed,
                             applicationSequenceStatusApproved = ApplicationSequenceStatus.Approved,
                             applicationSequenceStatusRejected = ApplicationSequenceStatus.Rejected                            
