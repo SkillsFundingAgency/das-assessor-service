f7db00ebff036da8f2c1ab09c777e3ae43dfb187
Martin Lush <martin-hornsby@ntlworld.com>
2019-10-18 16:45:19 +0100

    Fixed that pesky ;

--- src/SFA.DAS.AssessorService.Application.Api.Client/Clients/IQnaApiClient.cs
+++ src/SFA.DAS.AssessorService.Application.Api.Client/Clients/IQnaApiClient.cs
@@ -20,7 +20,7 @@ namespace SFA.DAS.AssessorService.Application.Api.Client.Clients
         Task<Sequence> GetSequenceBySequenceNo(Guid applicationId, int sequenceNo);
         Task<List<Section>> GetSections(Guid applicationId, Guid sequenceId);
         Task<Section> GetSection(Guid applicationId, Guid sectionId);
-        Task<Section> GetSectionBySectionNo(Guid applicationId, int sequenceNo, int sectionNo)
+        Task<Section> GetSectionBySectionNo(Guid applicationId, int sequenceNo, int sectionNo);
         Task<Page> GetPage(Guid applicationId, Guid sectionId, string pageId);
         Task<SetPageAnswersResponse> AddPageAnswer(Guid applicationId, Guid sectionId, string pageId, List<Answer> answer);
         Task<AddPageAnswerResponse> AddAnswersToMultipleAnswerPage(Guid applicationId, Guid sectionId, string pageId, List<Answer> answer);
