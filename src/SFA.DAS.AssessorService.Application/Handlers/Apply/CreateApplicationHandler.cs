23366cbd384b9912885b05dd2693dda320febf5b
Alan Burns <adburns57@gmail.com>
2019-10-25 15:12:17 +0100

    Allow 'Live' organisations to Apply for Standard

--- src/SFA.DAS.AssessorService.Application/Handlers/Apply/CreateApplicationHandler.cs
+++ src/SFA.DAS.AssessorService.Application/Handlers/Apply/CreateApplicationHandler.cs
@@ -96,7 +96,7 @@ namespace SFA.DAS.AssessorService.Application.Handlers.Apply
         {
             if (org?.OrganisationData == null) return false;
 
-            return org.OrganisationData.RoEPAOApproved;
+            return org.OrganisationData.RoEPAOApproved  || org.Status == 'Live';
         }
 
         private void RemoveSequenceOne(List<ApplySequence> sequences)
