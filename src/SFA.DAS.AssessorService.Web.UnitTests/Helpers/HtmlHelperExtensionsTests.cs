3c9d9d487ba44d86becf8994ff552a43bbee0543
vas <vasantha.kasirajan@digital.education.gov.uk>
2019-12-05 15:12:36 +0000

    CON-1112 - EPAO Assessor Service - Contextual widget

---
+++ src/SFA.DAS.AssessorService.Web.UnitTests/Helpers/HtmlHelperExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using SFA.DAS.AssessorService.Web.Extensions;
+
+namespace SFA.DAS.AssessorService.Web.UnitTests.Helpers
+{
+    [TestFixture]
+    public class HtmlHelperExtensionsTests
+    {
+        private string _expectedOutput;
+        private string _expectedLabelOne;
+        private string _expectedLabelTwo;
+        private string _actualOutput;      
+
+        [Test]
+        public void WhenICallSetZenDeskLabelsWithMultipleLabelsWithMultipleWordsAndApostrophes_ThenTheOutputIsCorrect()
+        {
+            //Arrange
+            _expectedLabelOne = "a string with multiple words";
+            _expectedLabelTwo = "the title of another page";
+            _expectedOutput =
+                $"<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', {{ labels: ['{_expectedLabelOne}','{_expectedLabelTwo}'] }});</script>";
+
+            //Act
+            _actualOutput = HtmlHelperExtensions.SetZenDeskLabels(null, new string[] { _expectedLabelOne, _expectedLabelTwo }).ToString();
+
+            //Assert
+            Assert.AreEqual(_expectedOutput, _actualOutput);
+        }
+
+        [Test]
+        public void WhenICallSetZenDeskLabelsWithLabel_ThenTheOutputIsCorrect()
+        {
+            //Arrange
+            _expectedLabelOne = "ass-dashboard";
+            _expectedOutput =
+                $"<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', {{ labels: ['{_expectedLabelOne}'] }});</script>";
+            
+            //Act
+            _actualOutput = HtmlHelperExtensions.SetZenDeskLabels(null, new string[] { _expectedLabelOne}).ToString();
+
+            //Assert
+            Assert.AreEqual(_expectedOutput, _actualOutput);
+
+        }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Web/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SFA.DAS.AssessorService.Web.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlContent SetZenDeskSuggestion(this IHtmlHelper html, string suggestion)
+        {            
+            return new HtmlString($"<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', {{  search: '{suggestion}' }});</script>");
+        }
+
+        public static IHtmlContent SetZenDeskLabels(this IHtmlHelper html, params string[] labels)
+        {
+            var apiCallString = "<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', { labels: [";
+
+            var first = true;
+            foreach (var label in labels)
+            {
+                if (!first) apiCallString += ",";
+                first = false;
+
+                apiCallString += $"'{ EscapeApostrophes(label) }'";
+            }
+
+            apiCallString += "] });</script>";
+
+            return new HtmlString(apiCallString);
+        }
+
+        private static string EscapeApostrophes(string input)
+        {
+            return input.Replace("'", @"\'");
+        }
+    }
+}
