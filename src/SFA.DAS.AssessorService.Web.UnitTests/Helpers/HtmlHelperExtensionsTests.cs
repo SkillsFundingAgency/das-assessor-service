0b25c2f57564ba89f280163c91747eef87708a3d
vas <vasantha.kasirajan@digital.education.gov.uk>
2019-12-09 16:14:33 +0000

    Added TestCaseSource

--- src/SFA.DAS.AssessorService.Web.UnitTests/Helpers/HtmlHelperExtensionsTests.cs
+++ src/SFA.DAS.AssessorService.Web.UnitTests/Helpers/HtmlHelperExtensionsTests.cs
@@ -1,47 +1,14 @@
 ﻿using NUnit.Framework;
 using SFA.DAS.AssessorService.Web.Extensions;
+using System.Collections;
 
 namespace SFA.DAS.AssessorService.Web.UnitTests.Helpers
 {
     [TestFixture]
     public class HtmlHelperExtensionsTests
-    {        
-        private string expectedLabelOne;
-        private string expectedLabelTwo;
+    { 
         private string expectedOutput;
         private string actualOutput;      
-
-        [Test]
-        public void WhenICallSetZenDeskLabelsWithMultipleLabelsWithMultipleWordsAndApostrophes_ThenTheOutputIsCorrect()
-        {
-            //Arrange
-            expectedLabelOne = "a string with multiple words";
-            expectedLabelTwo = "the title of another page";
-            expectedOutput =
-                $"<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', {{ labels: ['{expectedLabelOne}','{expectedLabelTwo}'] }});</script>";
-
-            //Act
-            actualOutput = HtmlHelperExtensions.SetZenDeskLabels(null, new string[] { expectedLabelOne, expectedLabelTwo }).ToString();
-
-            //Assert
-            Assert.AreEqual(expectedOutput, actualOutput);
-        }
-
-        [Test]
-        public void WhenICallSetZenDeskLabelsWithLabel_ThenTheOutputIsCorrect()
-        {
-            //Arrange
-            expectedLabelOne = "ass-dashboard";
-            expectedOutput =
-                $"<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', {{ labels: ['{expectedLabelOne}'] }});</script>";
-            
-            //Act
-            actualOutput = HtmlHelperExtensions.SetZenDeskLabels(null, new string[] { expectedLabelOne}).ToString();
-
-            //Assert
-            Assert.AreEqual(expectedOutput, actualOutput);
-
-        }
 
         [Test]
         public void WhenICallSetZenDeskLabelsWithOutLabel_ThenTheOutputIsCorrect()
@@ -55,21 +22,50 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.Helpers
 
             //Assert
             Assert.AreEqual(expectedOutput, actualOutput);
-        }
+        }      
 
-        [Test]
-        public void WhenICallSetZenDeskLabelsWithNullLabel_ThenTheOutputIsCorrect()
+        [TestCaseSource(typeof(StringArrayTestDataSource))]
+        public void WhenICallSetZenDeskLabelsWithLabels_ThenTheOutputIsCorrect(string[] labels)
         {
-            //Arrange            
-            expectedOutput =
-                $"<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', {{ labels: [] }});</script>";
-            string[] label = new string[] { null };
+            //Arrange
+            var test = labels;
+            var actualOutput = "<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', { labels: [";
 
-            //Act            
-            actualOutput = HtmlHelperExtensions.SetZenDeskLabels(null, label).ToString();
+            var first = true;
+            foreach (var label in labels)
+            {
+                if (!string.IsNullOrEmpty(label))
+                {
+                    if (!first) actualOutput += ",";
+                    first = false;
+
+                    actualOutput += $"'{ EscapeApostrophes(label) }'";
+                }
+            }
+
+            actualOutput += "] });</script>";
+
+            //Act
+            this.actualOutput = HtmlHelperExtensions.SetZenDeskLabels(null, labels).ToString();
 
             //Assert
-            Assert.AreEqual(expectedOutput, actualOutput);
+            Assert.AreEqual(actualOutput, this.actualOutput);
+        }
+
+        private static string EscapeApostrophes(string input)
+        {
+            return input.Replace("'", @"\'");
+        }
+
+    }
+
+    public class StringArrayTestDataSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {            
+            yield return new string[] { "a string with multiple words", "the title of another page" };
+            yield return new string[] { "ass-dashboard" };
+            yield return new string[] { null };
         }
     }
 }
