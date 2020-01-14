8b77139c66b05a95fda9a3d00ffe2a24b93fd071
Martin Lush <martin-hornsby@ntlworld.com>
2020-01-14 13:58:01 +0000

    Correctly filter out all special QnA Inputs from answers

--- src/SFA.DAS.AssessorService.Web/Controllers/Apply/ApplicationController.cs
+++ src/SFA.DAS.AssessorService.Web/Controllers/Apply/ApplicationController.cs
@@ -896,8 +896,11 @@ namespace SFA.DAS.AssessorService.Web.Controllers.Apply
         {
             List<Answer> answers = new List<Answer>();
 
+            // These are special in that they drive other things and thus should not be deemed as an answer
+            var exludedInputs = new List<string> { "postcodeSearch", "checkAll" };
+
             // Add answers from the Form post
-            foreach (var keyValuePair in HttpContext.Request.Form.Where(f => !f.Key.StartsWith("__") && !f.Key.EndsWith("postcodeSearch")))
+            foreach (var keyValuePair in HttpContext.Request.Form.Where(f => !f.Key.StartsWith("__") && !exludedInputs.Contains(f.Key, StringComparer.InvariantCultureIgnoreCase)))
             {
                 answers.Add(new Answer() { QuestionId = keyValuePair.Key, Value = keyValuePair.Value });
             }
