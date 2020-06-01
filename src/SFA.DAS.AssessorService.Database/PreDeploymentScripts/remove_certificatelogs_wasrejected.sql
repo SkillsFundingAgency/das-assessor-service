778d1615a067176d9b45a287dd75ec762096fe01
Chris Woodcock <chris@humesoftware.com>
2020-06-01 17:14:05 +0100

    Added schema to support tracking certficate print status

---
+++ src/SFA.DAS.AssessorService.Database/PreDeploymentScripts/remove_certificatelogs_wasrejected.sql
@@ -0,0 +1,14 @@
+﻿-- This script was extracted from the PreDeploymentScript it has currently worked its way 
+-- into PROD and so has been removed from the PreDeploymentScript
+
+IF EXISTS(
+  SELECT *
+  FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE 
+    TABLE_NAME = 'CertificateLogs'
+    AND COLUMN_NAME = 'WasRejected')
+BEGIN
+  ALTER TABLE [dbo].[CertificateLogs]
+    DROP COLUMN [WasRejected]
+END;
+GO
---
+++ src/SFA.DAS.AssessorService.Database/Tables/CertificateBatchLogs.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABLE [dbo].[CertificateBatchLogs]
+(
+	[Id] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
+	[CertificateReference] VARCHAR(50) NOT NULL,
+	[BatchNumber] [int] NOT NULL,
+	[CertificateData] [nvarchar](max) NOT NULL,
+	[Status] [nvarchar](20) NOT NULL,
+	[StatusAt] [datetime2](7) NOT NULL,
+	[CreatedAt] [datetime2](7) NOT NULL,
+	[CreatedBy] [nvarchar](256) NOT NULL,
+	[UpdatedAt] [datetime2](7) NULL,
+	[UpdatedBy] [nvarchar](256) NULL, 
+	[DeletedAt] [datetime2](7) NULL,
+	[DeletedBy] [nvarchar](256) NULL,
+	CONSTRAINT [PK_CertificateBatchLogs] PRIMARY KEY CLUSTERED 
+	(
+		[CertificateReference],
+		[BatchNumber]
+	)
+)
+GO
+
+ALTER TABLE [dbo].[CertificateBatchLogs] ADD CONSTRAINT [FK_CertificateBatchLogs_BatchLogs] FOREIGN KEY([BatchNumber])
+REFERENCES [dbo].[BatchLogs] ([BatchNumber])
+GO
+
+ALTER TABLE [dbo].[CertificateBatchLogs] CHECK CONSTRAINT [FK_CertificateBatchLogs_BatchLogs];
+GO
+
