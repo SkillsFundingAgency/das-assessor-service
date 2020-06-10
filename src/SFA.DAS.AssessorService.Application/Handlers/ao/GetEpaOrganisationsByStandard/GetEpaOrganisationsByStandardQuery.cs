0f257843b9d231ce3e55013b3392fbe22ebb550f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-10 11:36:13 +0100

    Add controller action
    Added controller action for getting EPAOs by a standard

---
+++ src/SFA.DAS.AssessorService.Application/Handlers/ao/GetEpaOrganisationsByStandard/GetEpaOrganisationsByStandardQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace SFA.DAS.AssessorService.Application.Handlers.ao.GetEpaOrganisationsByStandard
+{
+    public class GetEpaOrganisationsByStandardQuery : IRequest<GetEpaOrganisationsByStandardResponse>
+    {
+        public int Standard { get; set; }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Application/Handlers/ao/GetEpaOrganisationsByStandard/GetEpaOrganisationsByStandardResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.AssessorService.Api.Types.Models.AO;
+
+namespace SFA.DAS.AssessorService.Application.Handlers.ao.GetEpaOrganisationsByStandard
+{
+    public class GetEpaOrganisationsByStandardResponse
+    {
+        public List<EpaOrganisation> EpaOrganisations { get; set; }
+    }
+}
