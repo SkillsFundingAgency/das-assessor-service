30c55536ba88b4db0e9f37b0a424ffc7182a91e9
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-06-15 16:17:50 +0100

    CON-1950 - Update recipient of print assessor remove APS add Xerox

--- src/SFA.DAS.AssessorService.Database/PostDeploymentScripts/con-1950-update-print-recipient.sql
+++ src/SFA.DAS.AssessorService.Database/PostDeploymentScripts/con-1950-update-print-recipient.sql
@@ -6,14 +6,14 @@ BEGIN TRANSACTION
 
 IF (SELECT COUNT(*) FROM [EMailTemplates] WHERE TemplateName = N'PrintAssessorCoverLetters') = 1
 BEGIN
-	UPDATE [EMailTemplates] SET Recipients='xeroxsupport@funasset.com', UpdatedAt= GETDATE() WHERE TemplateName= 'PrintAssessorCoverLetters'
+	UPDATE [EMailTemplates] SET Recipients='xeroxsupport@funasset.com;epao.helpdesk@education.gov.uk', UpdatedAt= GETDATE() WHERE TemplateName= 'PrintAssessorCoverLetters'
 END
 
 -- insert the email template if it does not already exist
  IF NOT EXISTS (SELECT * FROM [EMailTemplates] WHERE TemplateName = N'PrintAssessorCoverLetters')
 BEGIN
 	INSERT INTO [EMailTemplates] ([TemplateName] ,[TemplateId] ,[Recipients] ,[CreatedAt] ,[DeletedAt] ,[UpdatedAt] ,[RecipientTemplate])
-     VALUES ('PrintAssessorCoverLetters' ,'5b171b91-d406-402a-a651-081cce820acb' ,'xeroxsupport@funasset.com' , GETDATE() ,NULL ,NULL ,NULL)
+     VALUES ('PrintAssessorCoverLetters' ,'5b171b91-d406-402a-a651-081cce820acb' ,'xeroxsupport@funasset.com;epao.helpdesk@education.gov.uk' , GETDATE() ,NULL ,NULL ,NULL)
 END
 
 COMMIT TRANSACTION
