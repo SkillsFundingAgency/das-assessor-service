4980c1d0a4559a11187ef059b519685c5c9ab383
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-07-20 20:27:59 +0100

    Build Issue

--- src/SFA.DAS.AssessorService.Domain/DTOs/EmailTemplateSummary.cs
+++ src/SFA.DAS.AssessorService.Domain/DTOs/EmailTemplateSummary.cs
@@ -7,7 +7,6 @@ namespace SFA.DAS.AssessorService.Domain.DTOs
         public Guid Id { get; set; }
         public string TemplateName { get; set; }
         public string TemplateId { get; set; }
-        public string Recipients { get; set; }
-        //public string RecipientTemplate { get; set; }
+        public string Recipients { get; set; }        
     }
 }
