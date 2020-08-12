49dd9ad2e1e69f0622c53913cc84374a330de878
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-08-12 13:11:19 +0100

    CON-2061-Updated the order of post deployment scripts

--- src/SFA.DAS.AssessorService.Database/Script.PostDeployment1.sql
+++ src/SFA.DAS.AssessorService.Database/Script.PostDeployment1.sql
@@ -120,6 +120,6 @@ END
 :r .\Update-Staff-Reports-Config.sql
 
 /* START OF CON-2061-Extract [Recipients] column from [EmailTemplates]*/
-:r .\PostDeploymentScripts\con-2061-email-templates-recipients.sql
 :r .\PostDeploymentScripts\con-2061-clear-entries-recipienttemplate.sql
+:r .\PostDeploymentScripts\con-2061-email-templates-recipients.sql
 /* END OF CON-2061-Extract [Recipients] column from [EmailTemplates] */
