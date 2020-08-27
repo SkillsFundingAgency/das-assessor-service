6a7c0b1fb6522cee40955afa54de464022d11c22
Chris Woodcock <chris@humesoftware.com>
2020-08-27 16:42:29 +0100

    Displaying certificate status and reason for not delivered in completed certificates

---
+++ src/SFA.DAS.AssessorService.Web/ViewModels/Certificate/CertificateHistoryViewModel.cs
@@ -0,0 +1,48 @@
+﻿using SFA.DAS.AssessorService.Api.Types.Models.Certificates;
+using SFA.DAS.AssessorService.Domain.Consts;
+using SFA.DAS.AssessorService.Domain.Paging;
+using System.Linq;
+
+namespace SFA.DAS.AssessorService.Web.ViewModels.Certificate
+{
+    public class CertificateHistoryViewModel
+    {
+        public PaginatedList<CertificateSummaryResponse> Certificates { get; set; }
+
+        public bool CanShowCertificateDetails(CertificateSummaryResponse certificateSummaryResponse)
+        {
+            var allowed = new[] 
+            { 
+                CertificateStatus.Submitted, CertificateStatus.SentToPrinter, 
+                CertificateStatus.Printed, CertificateStatus.Reprint, 
+                CertificateStatus.Delivered, CertificateStatus.NotDelivered  
+            };
+            
+            return 
+                allowed.Contains(certificateSummaryResponse.Status) &&
+                certificateSummaryResponse.OverallGrade != CertificateGrade.Fail;
+        }
+
+        public bool IsCertificateWaitingToBeDelivered(CertificateSummaryResponse certificateSummaryResponse)
+        {
+            var allowed = new[]
+            {
+                CertificateStatus.Submitted, CertificateStatus.SentToPrinter,
+                CertificateStatus.Printed, CertificateStatus.Reprint
+            };
+
+            return
+                allowed.Contains(certificateSummaryResponse.Status);
+        }
+
+        public bool IsCertificateDelivered(CertificateSummaryResponse certificateSummaryResponse)
+        {
+            return certificateSummaryResponse.Status == CertificateStatus.Delivered;
+        }
+
+        public bool IsCertificateNotDelivered(CertificateSummaryResponse certificateSummaryResponse)
+        {
+            return certificateSummaryResponse.Status == CertificateStatus.NotDelivered;
+        }
+    }
+}
