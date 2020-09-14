51c7f79a3ce49e788fbf6ce11a2401af17e3750b
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-09-14 13:56:54 +0100

    CON-2259: Tidy up

--- src/SFA.DAS.AssessorService.Data/CertificateBatchLogRepository.cs
+++ src/SFA.DAS.AssessorService.Data/CertificateBatchLogRepository.cs
@@ -1,24 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SFA.DAS.AssessorService.Application.Interfaces;
 using SFA.DAS.AssessorService.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.AssessorService.Data
 {
     public class CertificateBatchLogRepository : ICertificateBatchLogRepository
     {
-        private readonly AssessorDbContext _context;
-        private readonly IDbConnection _connection;
+        private readonly AssessorDbContext _context;        
 
-        public CertificateBatchLogRepository(AssessorDbContext context,
-            IDbConnection connection)
+        public CertificateBatchLogRepository(AssessorDbContext context)
         {
-            _context = context;
-            _connection = connection;
+            _context = context;            
         }
 
         public async Task<CertificateBatchLog> GetCertificateBatchLog(string certificateReference, int batchNumber)
