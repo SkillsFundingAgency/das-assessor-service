30e7effde7616b61efe077e8d09eaf9d76f195b5
Chris Woodcock <chris@humesoftware.com>
2020-09-24 18:06:55 +0100

    Apply for withdrawal screen changes

---
+++ src/SFA.DAS.AssessorService.Web/Controllers/ApplyForWithdrawal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AssessorService.Api.Types.Models;
+using SFA.DAS.AssessorService.Application.Api.Client.Clients;
+using SFA.DAS.AssessorService.Domain.Consts;
+using SFA.DAS.AssessorService.Domain.Entities;
+using SFA.DAS.AssessorService.Settings;
+using SFA.DAS.AssessorService.Web.Constants;
+using SFA.DAS.AssessorService.Web.Controllers.Apply;
+using SFA.DAS.AssessorService.Web.Extensions;
+using SFA.DAS.AssessorService.Web.Infrastructure;
+using SFA.DAS.AssessorService.Web.StartupConfiguration;
+
+namespace SFA.DAS.AssessorService.Web.Controllers
+{
+    [PrivilegeAuthorize(Privileges.ChangeOrganisationDetails)]
+    [CheckSession]
+    public class ApplyForWithdrawal : Controller
+    {
+        private readonly IApplicationApiClient _applicationApiClient;
+        private readonly IContactsApiClient _contactsApiClient;
+
+        public ApplyForWithdrawal(IApplicationApiClient applicationApiClient, IContactsApiClient contactsApiClient)
+        {
+            _applicationApiClient = applicationApiClient;
+            _contactsApiClient = contactsApiClient;
+        }
+
+        [HttpGet]
+        [TypeFilter(typeof(MenuFilter), Arguments = new object[] { Pages.Organisations })]
+        public async Task<IActionResult> Index()
+        {
+            var userId = await GetUserId();
+            var applications = await _applicationApiClient.GetOrganisationWithdrawalApplications(userId);
+
+            return View(applications?.Count() == 0);
+        }
+
+        private async Task<Guid> GetUserId()
+        {
+            var signinId = User.Claims.First(c => c.Type == "sub")?.Value;
+            var contact = await GetUserContact(signinId);
+
+            return contact?.Id ?? Guid.Empty;
+        }
+
+        private async Task<ContactResponse> GetUserContact(string signinId)
+        {
+            return await _contactsApiClient.GetContactBySignInId(signinId);
+        }
+    }
+}
