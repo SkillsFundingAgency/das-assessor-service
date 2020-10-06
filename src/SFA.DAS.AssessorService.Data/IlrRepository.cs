bf6bcc875149a8daf07f6480133cbac0469196c9
Chris Woodcock <chris@humesoftware.com>
2020-10-06 15:31:29 +0100

    Added a IlrsImport table, which is used for the LearnerDetailsImport

--- src/SFA.DAS.AssessorService.Data/IlrRepository.cs
+++ src/SFA.DAS.AssessorService.Data/IlrRepository.cs
@@ -29,7 +29,15 @@ namespace SFA.DAS.AssessorService.Data
         public async Task<Ilr> Get(long uln, int stdCode)
         {
             return await _unitOfWork.Connection.QueryFirstOrDefaultAsync<Ilr>(
-                @"SELECT * FROM Ilrs WHERE [Uln] = @uln AND [StdCode] = @stdCode",
+                @"SELECT * FROM [Ilrs] WHERE [Uln] = @uln AND [StdCode] = @stdCode",
+                  param: new { uln, stdCode },
+                  transaction: _unitOfWork.Transaction);
+        }
+
+        public async Task<Ilr> GetImport(long uln, int stdCode)
+        {
+            return await _unitOfWork.Connection.QueryFirstOrDefaultAsync<Ilr>(
+                @"SELECT * FROM [IlrsImport] WHERE [Uln] = @uln AND [StdCode] = @stdCode",
                   param: new { uln, stdCode },
                   transaction: _unitOfWork.Transaction);
         }
@@ -77,10 +85,80 @@ namespace SFA.DAS.AssessorService.Data
                   transaction: _unitOfWork.Transaction);
         }
 
+        public async Task CreateImport(Ilr ilr)
+        {
+            await _unitOfWork.Connection.ExecuteAsync(
+                @"INSERT INTO [IlrsImport] (Uln, GivenNames, FamilyName, UkPrn, StdCode, LearnStartDate, EpaOrgId, FundingModel, 
+                  ApprenticeshipId, EmployerAccountId, Source, CreatedAt, UpdatedAt, LearnRefNumber, CompletionStatus, EventId, PlannedEndDate, DelLocPostCode, 
+                  LearnActEndDate, WithdrawReason, Outcome, AchDate, OutGrade) 
+                  VALUES (@uln, @givenNames, @familyName, @ukprn, @stdCode, @learnStartDate, @epaOrgId, @fundingModel, null, null,
+                        @source, @createdAt, null, @learnRefNumber, @completionStatus, null, @plannedEndDate, @delLocPostCode, 
+                        @learnActEndDate, @withdrawReason, @outcome, @achDate, @outGrade )",
+                  param: new
+                  {
+                      ilr.Uln,
+                      ilr.GivenNames,
+                      ilr.FamilyName,
+                      ilr.UkPrn,
+                      ilr.StdCode,
+                      ilr.LearnStartDate,
+                      ilr.EpaOrgId,
+                      ilr.FundingModel,
+                      ilr.Source,
+                      createdAt = DateTime.UtcNow,
+                      ilr.LearnRefNumber,
+                      ilr.CompletionStatus,
+                      ilr.PlannedEndDate,
+                      ilr.DelLocPostCode,
+                      ilr.LearnActEndDate,
+                      ilr.WithdrawReason,
+                      ilr.Outcome,
+                      ilr.AchDate,
+                      ilr.OutGrade
+                  },
+                  transaction: _unitOfWork.Transaction);
+        }
+
         public async Task Update(Ilr ilr)
         {
             await _unitOfWork.Connection.ExecuteAsync(
                 @"UPDATE [Ilrs] SET [GivenNames] = @givenNames, [FamilyName] = @familyName, [UkPrn] = @ukprn, 
+                                    [LearnStartDate] = @learnStartDate, [EpaOrgId] = @epaOrgId, [FundingModel] = @fundingModel,
+                                    [ApprenticeshipId] = null, [EmployerAccountId] = null, [Source] = @source, [CreatedAt] = [CreatedAt], [UpdatedAt] = @updatedAt,
+                                    [LearnRefNumber] = @learnRefNumber, [CompletionStatus] = @completionStatus, [EventId] = null, [PlannedEndDate] = @plannedEndDate, 
+                                    [DelLocPostCode] = @delLocPostCode, [LearnActEndDate] = @learnActEndDate, [WithdrawReason] = @withdrawReason, 
+                                    [Outcome] = @outcome, [AchDate] = @achDate, [OutGrade] = @outGrade
+                                WHERE
+                                    [Uln] = @uln AND [StdCode] = @stdCode",
+                  param: new
+                  {
+                      ilr.Uln,
+                      ilr.GivenNames,
+                      ilr.FamilyName,
+                      ilr.UkPrn,
+                      ilr.StdCode,
+                      ilr.LearnStartDate,
+                      ilr.EpaOrgId,
+                      ilr.FundingModel,
+                      ilr.Source,
+                      updatedAt = DateTime.UtcNow,
+                      ilr.LearnRefNumber,
+                      ilr.CompletionStatus,
+                      ilr.PlannedEndDate,
+                      ilr.DelLocPostCode,
+                      ilr.LearnActEndDate,
+                      ilr.WithdrawReason,
+                      ilr.Outcome,
+                      ilr.AchDate,
+                      ilr.OutGrade
+                  },
+                  transaction: _unitOfWork.Transaction);
+        }
+
+        public async Task UpdateImport(Ilr ilr)
+        {
+            await _unitOfWork.Connection.ExecuteAsync(
+                @"UPDATE [IlrsImport] SET [GivenNames] = @givenNames, [FamilyName] = @familyName, [UkPrn] = @ukprn, 
                                     [LearnStartDate] = @learnStartDate, [EpaOrgId] = @epaOrgId, [FundingModel] = @fundingModel,
                                     [ApprenticeshipId] = null, [EmployerAccountId] = null, [Source] = @source, [CreatedAt] = [CreatedAt], [UpdatedAt] = @updatedAt,
                                     [LearnRefNumber] = @learnRefNumber, [CompletionStatus] = @completionStatus, [EventId] = null, [PlannedEndDate] = @plannedEndDate, 
