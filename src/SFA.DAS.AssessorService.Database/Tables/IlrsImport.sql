69b8a122862ffcf4620d882934fbe8f03dd61179
Chris Woodcock <chris@humesoftware.com>
2020-10-06 16:06:47 +0100

    Corrected indentation

--- src/SFA.DAS.AssessorService.Database/Tables/IlrsImport.sql
+++ src/SFA.DAS.AssessorService.Database/Tables/IlrsImport.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[IlrsImport]
 (
-	[Id] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY, 
+    [Id] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY, 
     [Uln] BIGINT NULL, 
     [GivenNames] NVARCHAR(250) NULL, 
     [FamilyName] NVARCHAR(250) NULL, 
@@ -8,22 +8,22 @@
     [StdCode] INT NULL, 
     [LearnStartDate] DATETIME2 NULL, 
     [EpaOrgId] NVARCHAR(50) NULL,     
-	[FundingModel] INT NULL,
+    [FundingModel] INT NULL,
     [ApprenticeshipId] BIGINT NULL,
     [EmployerAccountId] BIGINT NULL,
     [Source] NVARCHAR(10) NULL, 
     [CreatedAt] DATETIME2 NOT NULL, 
     [UpdatedAt] DATETIME2 NULL,
-	[LearnRefNumber] NVARCHAR(12) NULL,
-	[CompletionStatus] [int] NULL,
-	[EventId] BIGINT NULL, 
+    [LearnRefNumber] NVARCHAR(12) NULL,
+    [CompletionStatus] [int] NULL,
+    [EventId] BIGINT NULL, 
     [PlannedEndDate] DATETIME2 NULL,
-	[DelLocPostCode] [nvarchar](50) NULL,
-	[LearnActEndDate] DATETIME2 NULL,
-	[WithdrawReason] [int] NULL,
-	[Outcome] [int] NULL,
-	[AchDate] [datetime] NULL,
-	[OutGrade] [nvarchar](50) NULL	
+    [DelLocPostCode] [nvarchar](50) NULL,
+    [LearnActEndDate] DATETIME2 NULL,
+    [WithdrawReason] [int] NULL,
+    [Outcome] [int] NULL,
+    [AchDate] [datetime] NULL,
+    [OutGrade] [nvarchar](50) NULL	
 )
 
 GO
