3d8d2cc6e3b8a64e9baac1b55b082b1a526867e8
Shahzad <hkf-tech@users.noreply.github.com>
2020-10-21 12:58:51 +0100

    unit test added

---
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/Controllers/Schedule/Command/WhenUpdateLastRunStatus.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.AssessorService.Application.Api.Controllers;
+using SFA.DAS.AssessorService.Application.Interfaces;
+using System;
+using System.Threading.Tasks;
+using SFA.DAS.AssessorService.Api.Types.Models.ScheduleRun;
+using FluentAssertions;
+
+namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Controllers.Schedule.Command
+{
+    public class WhenUpdateLastRunStatus
+    {
+        private ScheduleController _sut;
+        private IActionResult _result;
+        private Mock<IScheduleRepository> _repository;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _repository = new Mock<IScheduleRepository>();
+            _sut = new ScheduleController(_repository.Object);
+        }
+
+        [Test]
+        public async Task ThenShouldReturnOk()
+        {
+            var request = new UpdateLastRunStatusRequest { LastRunStatus = LastRunStatus.Completed, ScheduleRunId = Guid.NewGuid() };
+
+            await _sut.UpdateLastRunStatus(request);
+
+            var result = _result as OkObjectResult;
+            result.Should().NotBeNull();
+        }
+
+        [TestCase(LastRunStatus.Failed)]
+        [TestCase(LastRunStatus.Restarted)]
+        [TestCase(LastRunStatus.Started)]
+        public async Task ThenShouldUpdateLastRunStatus(LastRunStatus lastRunStatus)
+        {
+            var request = new UpdateLastRunStatusRequest { LastRunStatus = lastRunStatus, ScheduleRunId = Guid.NewGuid() };
+
+            await _sut.UpdateLastRunStatus(request);
+
+            _repository.Verify(x => 
+            x.UpdateLastRunStatus(It.Is<UpdateLastRunStatusRequest>(y => y.ScheduleRunId == request.ScheduleRunId && y.LastRunStatus == lastRunStatus))
+               ,  Times.Once());
+        }
+    }
+}
