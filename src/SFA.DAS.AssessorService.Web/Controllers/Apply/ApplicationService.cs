52b77bb15922b9c04cd9be43511d989c74e2419f
Chris Woodcock <chris@humesoftware.com>
2020-12-09 15:01:04 +0000

    Extracted interface from class file

--- src/SFA.DAS.AssessorService.Web/Controllers/Apply/ApplicationService.cs
+++ src/SFA.DAS.AssessorService.Web/Controllers/Apply/ApplicationService.cs
@@ -115,11 +115,4 @@ namespace SFA.DAS.AssessorService.Web.Controllers.Apply
             };
         }
     }
-
-    public interface IApplicationService
-    {
-        Task<CreateApplicationRequest> BuildStandardWithdrawalCreateApplicationRequest(ContactResponse contact, OrganisationResponse organisation, int standardCode, string referenceFormat);
-        Task<CreateApplicationRequest> BuildOrganisationWithdrawalCreateApplicationRequest(ContactResponse contact, OrganisationResponse organisation, string referenceFormat);
-        Task<CreateApplicationRequest> BuildCombinedCreateApplicationRequest(ContactResponse contact, OrganisationResponse org, string referenceFormat);
-    }
 }
