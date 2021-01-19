30dd40ea5a2d573542316bb63f2cd3da0eb681f1
Chris Woodcock <chris@humesoftware.com>
2021-01-19 15:20:08 +0000

    Add database role

---
+++ src/SFA.DAS.AssessorService.Database/PostDeploymentScripts/GrantOrDenyPermissions.sql
@@ -0,0 +1,9 @@
+﻿/*
+	Grant or deny specific database permissions to database roles
+*/
+
+GRANT ALTER ON [dbo].[Certificates] TO [database_maintenance] AS [dbo]
+GO
+
+GRANT EXECUTE ON [dbo].[DatabaseMaintenace] TO [database_maintenance] AS [dbo]
+GO
