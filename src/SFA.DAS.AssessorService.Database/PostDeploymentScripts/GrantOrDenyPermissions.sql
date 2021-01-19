dd30b8422fb9383bc8b605f8ec3d2a1d7308365f
Chris Woodcock <chris@humesoftware.com>
2021-01-19 16:11:22 +0000

    Corrected sp name

--- src/SFA.DAS.AssessorService.Database/PostDeploymentScripts/GrantOrDenyPermissions.sql
+++ src/SFA.DAS.AssessorService.Database/PostDeploymentScripts/GrantOrDenyPermissions.sql
@@ -5,5 +5,5 @@
 GRANT ALTER ON [dbo].[Certificates] TO [database_maintenance] AS [dbo]
 GO
 
-GRANT EXECUTE ON [dbo].[DatabaseMaintenace] TO [database_maintenance] AS [dbo]
+GRANT EXECUTE ON [dbo].[DatabaseMaintenance] TO [database_maintenance] AS [dbo]
 GO
