4f0208258cc60a33ff86e44f2b16ad3796fa8e6a
Ben Stone <ben1.stone@education.gov.uk>
2021-03-11 16:51:55 +0000

    SV-535 Add test for when outer api returns null

--- src/SFA.DAS.AssessorService.Application.Api.UnitTests/Services/StandardServiceTests.cs
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/Services/StandardServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using AutoFixture.NUnit3;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Internal;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.AssessorService.Api.Types.Models.Standards;
@@ -19,18 +20,19 @@ namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Services
     public class StandardServiceTests
     {
         private Mock<IOuterApiClient> _mockOuterApiClient;
-
+        private Mock<ILogger<StandardService>> _mockLogger;
         private StandardService _standardService;
 
         [SetUp]
         public void Setup()
         {
             _mockOuterApiClient = new Mock<IOuterApiClient>();
+            _mockLogger = new Mock<ILogger<StandardService>>();
 
             _standardService = new StandardService(new CacheService(Mock.Of<IDistributedCache>()),
                 _mockOuterApiClient.Object,
                 Mock.Of<IIfaStandardsApiClient>(),
-                Mock.Of<ILogger<StandardService>>(),
+                _mockLogger.Object,
                 Mock.Of<IStandardRepository>());
         }
 
@@ -44,6 +46,23 @@ namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Services
 
             Assert.IsInstanceOf<IEnumerable<StandardOptions>>(result);
             Assert.AreEqual(result.Count(), response.StandardOptions.Count());
+        }
+
+        [Test]
+        public async Task When_GettingStandardOptions_And_OuterApiDoesNotReturnResponse_Then_LogError_And_ReturnNull()
+        {
+            _mockOuterApiClient.Setup(client => client.Get<GetStandardOptionsListResponse>(It.IsAny<GetStandardOptionsRequest>()))
+                .ReturnsAsync((GetStandardOptionsListResponse)null);
+
+            var result = await _standardService.GetStandardOptions();
+
+            Assert.IsNull(result);
+
+            _mockLogger.Verify(logger => logger.Log(LogLevel.Error, It.IsAny<EventId>(), 
+                    It.IsAny<FormattedLogValues>(),
+                    It.IsAny<Exception>(), 
+                    It.IsAny<Func<object, Exception, string>>()), 
+                Times.Once, "STANDARD OPTIONS: Failed to get standard options");
         }
     }
 }
