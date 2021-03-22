93f10f315ae7b02fdab3b58337eb0bcff8923b3e
dev4valtech <dev4valtech@gmail.com>
2021-03-22 09:27:11 +0000

    :heavy_check_mark: SV-487 more tests on StandardImportService

---
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/Services/StandardImportServiceTests/WhenUpsertingStandardNonApprovedCollations.cs
@@ -0,0 +1,36 @@
+﻿using AutoFixture;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.AssessorService.Api.Types.Models.Standards;
+using SFA.DAS.AssessorService.Application.Api.Services;
+using SFA.DAS.AssessorService.Application.Infrastructure.OuterApi;
+using SFA.DAS.AssessorService.Application.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Services.StandardImportServiceTests
+{
+    public class WhenUpsertingStandardNonApprovedCollations
+    {
+        Mock<IStandardRepository> standardRepositoryMock;
+        IEnumerable<GetStandardByIdResponse> standards;
+
+        [SetUp]
+        public async Task Initialize()
+        {
+            var fixture = new Fixture();
+            standards = fixture.Build<GetStandardByIdResponse>().CreateMany();
+            standardRepositoryMock = new Mock<IStandardRepository>();
+
+            var sut = new StandardImportService(standardRepositoryMock.Object);
+
+            await sut.UpsertStandardCollations(standards);
+        }
+
+        [Test]
+        public void Then_Inserts_Data_Into_Standards_Table()
+        {
+            standardRepositoryMock.Verify(r => r.UpsertNonApprovedStandards(It.IsAny<List<StandardCollation>>()));
+        }
+    }
+}
