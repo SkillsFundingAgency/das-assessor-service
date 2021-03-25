b28dd2abc97ef7238052dbdfed9b16bbcb44a7ff
Alan Burns <adburns57@gmail.com>
2021-03-25 10:14:37 +0000

    Simplify the Part4 Migration script for Apply

--- src/SFA.DAS.AssessorService.Database/PostDeploymentScripts/EPAO_MIgration_Part4_Apply.sql
+++ src/SFA.DAS.AssessorService.Database/PostDeploymentScripts/EPAO_MIgration_Part4_Apply.sql
@@ -1,18 +1,5 @@
 ﻿-- Migrate Apply table - set the Standardreference
 
-MERGE INTO Apply MasterApply
-USING (
-SELECT ap1.Id ApplyId, st1.StandardReference
-FROM Apply ap1 -- 986
-JOIN
-(SELECT [IFateReferenceNumber] StandardReference, [LarsCode] StandardCode
-FROM (
-SELECT [IFateReferenceNumber]  ,[LarsCode], ROW_NUMBER() OVER (PARTITION BY [LarsCode] ORDER BY  [VersionEarliestStartDate] DESC) rownumber
-  FROM [dbo].[Standards]
-  WHERE larscode != 0  -- 731
-  AND [VersionEarliestStartDate] is not null
-  ) ab WHERE rownumber = 1) st1 ON st1.StandardCode = ap1.StandardCode
-  ) upd
-  ON MasterApply.Id = upd.ApplyId
-  WHEN MATCHED AND MasterApply.StandardReference IS NULL THEN 
-  UPDATE SET MasterApply.StandardReference = upd.StandardReference;
+UPDATE Apply
+SET StandardReference = JSON_VALUE(ApplyData,'$.Apply.StandardReference') 
+WHERE StandardReference IS NULL AND StandardCode IS NULL
