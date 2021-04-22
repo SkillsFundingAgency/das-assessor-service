d978acc7edfab6cb8ce87c736e040c3b95239768
Alan Burns <adburns57@gmail.com>
2021-04-22 14:04:10 +0100

    SV-635 Patch for "version" data field in CertificateData (#1160)

---
+++ src/SFA.DAS.AssessorService.Database/PostDeploymentScripts/EPAO_Migration_Part0_Patch.sql
@@ -0,0 +1,13 @@
+﻿/*
+patch Certificates.CertificateData "version" to "Version"					
+*/
+
+UPDATE  Certificates
+SET CertificateData =
+CASE WHEN  JSON_VALUE(CertificateData,'$.Version')  IS NULL
+THEN JSON_MODIFY(JSON_MODIFY(CertificateData,'$.version', null),'$.Version','')
+ELSE JSON_MODIFY(CertificateData,'$.version', null)
+END
+where JSON_VALUE(CertificateData,'$.version')  = '' 
+and StandardUId IS NOT NULL
+
