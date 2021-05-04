4b77338597859b03fe95799a42ccb94e647c977a
Ben Stone <ben1.stone@education.gov.uk>
2021-05-04 13:26:25 +0100

    SV-652 Additional test cases

--- src/SFA.DAS.AssessorService.Data.UnitTests/Extensions/VersionToStringExtensionsTests.cs
+++ src/SFA.DAS.AssessorService.Data.UnitTests/Extensions/VersionToStringExtensionsTests.cs
@@ -6,8 +6,10 @@ namespace SFA.DAS.AssessorService.Data.UnitTests.Extensions
 {
     public class VersionToStringExtensionsTests
     {
+        [TestCase(.0, "0.0")]
         [TestCase(0.0, "0.0")]
         [TestCase(1, "1.0")]
+        [TestCase(1.0, "1.0")]
         [TestCase(1.1, "1.1")]
         public void When_ConvertingVersionDecimalToString_Then_ReturnStringWithAtleaseOneDecimalPlace(decimal? versionDecimal, string expected)
         {
