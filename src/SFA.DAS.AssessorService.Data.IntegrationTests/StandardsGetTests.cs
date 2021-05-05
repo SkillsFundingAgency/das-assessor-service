3b9a53ccae7b0c4248f34b38c6e39da77b6aa3ab
codewolf86 <chris@codewolfconsulting.com>
2021-05-05 16:49:35 +0100

    PR Comments

--- src/SFA.DAS.AssessorService.Data.IntegrationTests/StandardsGetTests.cs
+++ src/SFA.DAS.AssessorService.Data.IntegrationTests/StandardsGetTests.cs
@@ -37,6 +37,7 @@ namespace SFA.DAS.AssessorService.Data.IntegrationTests
         [TestCase("ST0001", "1.00", "ST0001_1.0")]
         [TestCase("ST0001", "1.1", "ST0001_1.1")]
         [TestCase("ST0001", "1.2", "ST0001_1.2")]
+        [TestCase("ST0001", "", "ST0001_1.2")]
         [TestCase("ST0001", "1.10", "ST0001_1.10", Ignore = "Test will fail until version is stored as string (SV-568)")]
         [TestCase("ST0001", "1.12", "ST0001_1.12", Ignore = "Test will fail until version is stored as string (SV-568)")]
         public async Task GetStandardByStandardReferenceAndVersion_ReturnsCorrectStandard(string standardReference, string version, string standardUId)
