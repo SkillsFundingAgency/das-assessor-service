839e219c1a5118b70320d034fcc9000f9eda2eba
codewolf86 <chris@codewolfconsulting.com>
2021-05-07 11:25:56 +0100

    Add Unit Tests for Handlers

---
+++ src/SFA.DAS.AssessorService.Application.UnitTests/Handlers/Standards/WhenHandlingGetStandardVersionRequest.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.AssessorService.Api.Types.Models;
+using SFA.DAS.AssessorService.Application.Handlers.Standards;
+using SFA.DAS.AssessorService.Application.Interfaces;
+using SFA.DAS.AssessorService.Domain.Entities;
+using SFA.DAS.Testing.AutoFixture;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.AssessorService.Application.UnitTests.Handlers.ImportStandards
+{
+    public class WhenHandlingGetStandardVersionRequest
+    {
+        [Test, MoqAutoData]
+        public async Task ThenGetsLatestStandardVersion(
+            [Frozen] Mock<IStandardService> standardService, 
+            GetStandardVersionRequest request, 
+            Standard standard,
+            GetStandardVersionHandler sut)
+        {
+            //Arrange
+            standardService.Setup(s => s.GetStandardVersionById(request.StandardId, request.Version)).ReturnsAsync(standard);
+
+            //Act
+            var result = await sut.Handle(request, new CancellationToken());
+
+            //Assert
+            result.Should().BeEquivalentTo(standard);
+        }
+    }
+}
