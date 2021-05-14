11220272cb79c110c198f2f0f3c4150190c8f0e8
codewolf86 <chris@codewolfconsulting.com>
2021-05-14 14:24:34 +0100

    Add Delete Epa Handler Tests

--- src/SFA.DAS.AssessorService.Application/Handlers/ExternalApi/Epas/DeleteBatchEpaHandler.cs
+++ src/SFA.DAS.AssessorService.Application/Handlers/ExternalApi/Epas/DeleteBatchEpaHandler.cs
@@ -16,13 +16,11 @@ namespace SFA.DAS.AssessorService.Application.Handlers.ExternalApi.Epas
     public class DeleteBatchEpaHandler : IRequestHandler<DeleteBatchEpaRequest>
     {
         private readonly ICertificateRepository _certificateRepository;
-        private readonly IContactQueryRepository _contactQueryRepository;
         private readonly ILogger<DeleteBatchEpaHandler> _logger;
 
-        public DeleteBatchEpaHandler(ICertificateRepository certificateRepository, IContactQueryRepository contactQueryRepository, ILogger<DeleteBatchEpaHandler> logger)
+        public DeleteBatchEpaHandler(ICertificateRepository certificateRepository, ILogger<DeleteBatchEpaHandler> logger)
         {
             _certificateRepository = certificateRepository;
-            _contactQueryRepository = contactQueryRepository;
             _logger = logger;
         }
 
