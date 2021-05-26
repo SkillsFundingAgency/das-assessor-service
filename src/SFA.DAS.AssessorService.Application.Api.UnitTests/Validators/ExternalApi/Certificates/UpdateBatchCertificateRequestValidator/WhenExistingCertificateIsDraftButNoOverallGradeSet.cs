e52106f33d6e8af48f01ec9362bce267389a1187
codewolf86 <chris@codewolfconsulting.com>
2021-05-26 15:58:47 +0100

    Update Validators for Create / Update

--- src/SFA.DAS.AssessorService.Application.Api.UnitTests/Validators/ExternalApi/Certificates/UpdateBatchCertificateRequestValidator/WhenExistingCertificateIsDraftButNoOverallGradeSet.cs
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/Validators/ExternalApi/Certificates/UpdateBatchCertificateRequestValidator/WhenExistingCertificateIsDraftButNoOverallGradeSet.cs
@@ -6,29 +6,30 @@ using SFA.DAS.AssessorService.Api.Types.Models.ExternalApi.Certificates;
 using SFA.DAS.AssessorService.Domain.Consts;
 using SFA.DAS.AssessorService.Domain.JsonData;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
-namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Validators.ExternalApi.Certificates.CreateBatchCertificateRequestValidator
+namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Validators.ExternalApi.Certificates.UpdateBatchCertificateRequestValidator
 {
-    public class WhenLatestEpaOutcomeInvalid : CreateBatchCertificateRequestValidatorTestBase
+    public class WhenExistingCertificateIsDraftButNoOverallGradeSet : UpdateBatchCertificateRequestValidatorTestBase
     {
         private ValidationResult _validationResult;
 
         [SetUp]
         public async Task Arrange()
         {
-            CreateBatchCertificateRequest request = Builder<CreateBatchCertificateRequest>.CreateNew()
-                .With(i => i.Uln = 9999999999)
-                .With(i => i.StandardCode = 101)
+            UpdateBatchCertificateRequest request = Builder<UpdateBatchCertificateRequest>.CreateNew()
+                .With(i => i.Uln = 1234567890)
+                .With(i => i.StandardCode = 99)
                 .With(i => i.StandardReference = null)
-                .With(i => i.UkPrn = 99999999)
+                .With(i => i.UkPrn = 12345678)
                 .With(i => i.FamilyName = "Test")
-                .With(i => i.CertificateReference = null)
+                .With(i => i.CertificateReference = "1234567890-99")
                 .With(i => i.CertificateData = Builder<CertificateData>.CreateNew()
                                 .With(cd => cd.ContactPostCode = "AA11AA")
                                 .With(cd => cd.AchievementDate = DateTime.UtcNow)
                                 .With(cd => cd.OverallGrade = CertificateGrade.Pass)
-                                .With(cd => cd.CourseOption = null)
+                                .With(cd => cd.CourseOption = "English")
                                 .With(cd => cd.Version = "1.0")
                                 .Build())
                 .Build();
@@ -41,6 +42,7 @@ namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Validators.ExternalA
         {
             _validationResult.IsValid.Should().BeFalse();
             _validationResult.Errors.Count.Should().Be(1);
+            _validationResult.Errors.First().ErrorMessage.Should().Be("Certificate not found");
         }
     }
 
