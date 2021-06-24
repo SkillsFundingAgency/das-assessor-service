b4d5ae40b8fa4b9eca118635e14ca2e94a4d8e6a
Matt Daniels <matt@techtoniq.com>
2021-06-24 10:55:07 +0100

    Add StandardReference and list of versions to the API enpoint. Also populate the DateStandardApprovedOnRegister on the model.

--- src/SFA.DAS.AssessorService.Api.Types/Models/Register/CreateEpaOrganisationStandardRequest.cs
+++ src/SFA.DAS.AssessorService.Api.Types/Models/Register/CreateEpaOrganisationStandardRequest.cs
@@ -4,10 +4,13 @@ using MediatR;
 
 namespace SFA.DAS.AssessorService.Api.Types.Models.Register
 {
-    public class CreateEpaOrganisationStandardRequest: IRequest<string>
+    public class CreateEpaOrganisationStandardRequest : IRequest<string>
     {
         public string OrganisationId { get; set; }
         public int StandardCode { get; set; }
+        public string StandardReference { get; set; }
+        public List<string> StandardVersions { get; set; }
+        public DateTime DateStandardApprovedOnRegister { get; set; }
         public DateTime? EffectiveFrom { get; set; }
         public DateTime? EffectiveTo { get; set; }
         public string Comments { get; set; }
