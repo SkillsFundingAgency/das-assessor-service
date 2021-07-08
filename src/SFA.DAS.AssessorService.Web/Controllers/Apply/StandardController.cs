8dd97f838e7867085faa6ec0543c4ed251152bfe
Ben Stone <ben1.stone@education.gov.uk>
2021-07-08 16:56:05 +0100

    SV-949 Set the standard version

--- src/SFA.DAS.AssessorService.Web/Controllers/Apply/StandardController.cs
+++ src/SFA.DAS.AssessorService.Web/Controllers/Apply/StandardController.cs
@@ -165,7 +165,7 @@ namespace SFA.DAS.AssessorService.Web.Controllers.Apply
             if(string.IsNullOrWhiteSpace(version))
             {
                 selectedStandard = standardVersions.LastOrDefault();
-                versions = model.SelectedVersions;
+                versions = model.SelectedVersions ?? new List<string> { selectedStandard.Version.VersionToString() }; 
                 if (model.SelectedVersions != null)
                     applicationStatus = await ApplicationStandardStatus(application, standardReference, model.SelectedVersions);
             }
