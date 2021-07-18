a022f6cd47c557479be18820228af960adc237a7
Matt Daniels <matt@techtoniq.com>
2021-07-18 08:33:19 +0100

    Add standard reference and version. Note - issue TBC: - Requirement on screeen is that there is one row per standard per version - Requirement in Excel is that one row per standard, with versions as CSV Current code for reports uses same SP to populate both screen and Excel

--- src/SFA.DAS.AssessorService.Database/StoredProcedures/StaffReports_ByStandard.sql
+++ src/SFA.DAS.AssessorService.Database/StoredProcedures/StaffReports_ByStandard.sql
@@ -2,7 +2,8 @@
 AS
   SELECT
 		REPLACE(UPPER(REPLACE(JSON_VALUE(ce.[CertificateData], '$.StandardName'), NCHAR(0x00A0), ' ')), 'Á', ' ') AS 'Standard Name',
-		CONVERT(CHAR(10), ce.[StandardCode]) AS 'Standard Code',
+		JSON_VALUE(ce.CertificateData, '$.StandardReference') 'Standard Reference',
+		JSON_VALUE(ce.CertificateData, '$.Version') 'Standard Version',
 		COUNT(*) AS 'Total',
 		SUM(CASE WHEN ce.[CertificateReferenceId] < 10000 THEN 1 ELSE 0 END) AS 'Manual Total',
 		SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 THEN 1 ELSE 0 END) AS 'EPA Service Total',
@@ -13,12 +14,13 @@ AS
 		SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 AND ce.[DeletedAt] IS NULL AND ce.[Status] = 'Printed' THEN 1 ELSE 0 END) AS 'EPA Printed',
 		SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 AND ce.[DeletedAt] IS NOT NULL THEN 1 ELSE 0 END) AS 'Deleted'
   FROM [dbo].[Certificates] ce
-  GROUP BY REPLACE(UPPER(REPLACE(JSON_VALUE(ce.[CertificateData], '$.StandardName'), NCHAR(0x00A0), ' ')), 'Á', ' '), ce.[StandardCode]
+  GROUP BY REPLACE(UPPER(REPLACE(JSON_VALUE(ce.[CertificateData], '$.StandardName'), NCHAR(0x00A0), ' ')), 'Á', ' '), ce.[StandardCode],  ce.CertificateData
 
   UNION 
   SELECT
 		' Summary' AS 'Standard Name',
-		''  AS 'Standard Code',
+		''  AS 'Standard Reference',
+		''  AS 'Standard Version',
 		COUNT(*) AS 'Total',
 		SUM(CASE WHEN ce.[CertificateReferenceId] < 10000 THEN 1 ELSE 0 END) AS 'Manual Total',
 		SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 THEN 1 ELSE 0 END) AS 'EPA Service Total',
