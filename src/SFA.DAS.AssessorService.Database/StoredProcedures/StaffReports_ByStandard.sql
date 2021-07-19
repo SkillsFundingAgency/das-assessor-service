b23f80be4001f800f8efa981f8881b1df17f8eaf
Matt Daniels <matt@techtoniq.com>
2021-07-19 11:20:01 +0100

    Group versions as CSV following review/agreement with Faizal

--- src/SFA.DAS.AssessorService.Database/StoredProcedures/StaffReports_ByStandard.sql
+++ src/SFA.DAS.AssessorService.Database/StoredProcedures/StaffReports_ByStandard.sql
@@ -1,20 +1,41 @@
 ﻿CREATE PROCEDURE [dbo].[StaffReports_ByStandard]
 AS
-  SELECT
-		REPLACE(UPPER(REPLACE(JSON_VALUE(ce.[CertificateData], '$.StandardName'), NCHAR(0x00A0), ' ')), 'Á', ' ') AS 'Standard Name',
-		JSON_VALUE(ce.CertificateData, '$.StandardReference') 'Standard Reference',
-		JSON_VALUE(ce.CertificateData, '$.Version') 'Standard Version',
-		COUNT(*) AS 'Total',
-		SUM(CASE WHEN ce.[CertificateReferenceId] < 10000 THEN 1 ELSE 0 END) AS 'Manual Total',
-		SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 THEN 1 ELSE 0 END) AS 'EPA Service Total',
-		SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 And ce.[CreatedBy] <> 'manual' THEN 1 ELSE 0 END) AS 'EPA Service',
-		SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 And ce.[CreatedBy] = 'manual' THEN 1 ELSE 0 END ) AS 'EPA Service Manual',
-		SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 AND ce.[DeletedAt] IS NULL AND ce.[Status] = 'Draft' THEN 1 ELSE 0 END) AS 'EPA Draft',
-		SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 AND ce.[DeletedAt] IS NULL AND ce.[Status] = 'Submitted' THEN 1 ELSE 0 END) AS 'EPA Submitted',
-		SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 AND ce.[DeletedAt] IS NULL AND ce.[Status] = 'Printed' THEN 1 ELSE 0 END) AS 'EPA Printed',
-		SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 AND ce.[DeletedAt] IS NOT NULL THEN 1 ELSE 0 END) AS 'Deleted'
-  FROM [dbo].[Certificates] ce
-  GROUP BY REPLACE(UPPER(REPLACE(JSON_VALUE(ce.[CertificateData], '$.StandardName'), NCHAR(0x00A0), ' ')), 'Á', ' '), ce.[StandardCode],  ce.CertificateData
+select 
+st.[Standard Name], 
+st.[Standard Reference], 
+STRING_AGG(st.[Standard Version], ', ') WITHIN GROUP (ORDER BY CONVERT(decimal(5,2), st.[Standard Version]) ASC) [Standard Version], 
+SUM(st.Total) [Total], 
+SUM(st.[Manual Total]) [Manual Total], 
+SUM(st.[EPA Service Total]) [EPA Service Total], 
+SUM(st.[EPA Service]) [EPA Service], 
+SUM(st.[EPA Service Manual]) [EPA Service Manual], 
+SUM(st.[EPA Draft]) [EPA Draft], 
+SUM(st.[EPA Submitted]) [EPA Submitted], 
+SUM(st.[EPA Printed]) [EPA Printed], 
+SUM(st.Deleted) [Deleted]
+from
+(
+
+SELECT
+			REPLACE(UPPER(REPLACE(JSON_VALUE(ce.[CertificateData], '$.StandardName'), NCHAR(0x00A0), ' ')), 'Á', ' ') AS 'Standard Name',
+			JSON_VALUE(ce.CertificateData, '$.StandardReference') 'Standard Reference',
+			JSON_VALUE(ce.CertificateData, '$.Version') 'Standard Version',
+			COUNT(*) AS 'Total',
+			SUM(CASE WHEN ce.[CertificateReferenceId] < 10000 THEN 1 ELSE 0 END) AS 'Manual Total',
+			SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 THEN 1 ELSE 0 END) AS 'EPA Service Total',
+			SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 And ce.[CreatedBy] <> 'manual' THEN 1 ELSE 0 END) AS 'EPA Service',
+			SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 And ce.[CreatedBy] = 'manual' THEN 1 ELSE 0 END ) AS 'EPA Service Manual',
+			SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 AND ce.[DeletedAt] IS NULL AND ce.[Status] = 'Draft' THEN 1 ELSE 0 END) AS 'EPA Draft',
+			SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 AND ce.[DeletedAt] IS NULL AND ce.[Status] = 'Submitted' THEN 1 ELSE 0 END) AS 'EPA Submitted',
+			SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 AND ce.[DeletedAt] IS NULL AND ce.[Status] = 'Printed' THEN 1 ELSE 0 END) AS 'EPA Printed',
+			SUM(CASE WHEN ce.[CertificateReferenceId] >= 10000 AND ce.[DeletedAt] IS NOT NULL THEN 1 ELSE 0 END) AS 'Deleted'
+	  FROM [dbo].[Certificates] ce
+	  GROUP BY REPLACE(UPPER(REPLACE(JSON_VALUE(ce.[CertificateData], '$.StandardName'), NCHAR(0x00A0), ' ')), 'Á', ' '), ce.[StandardCode],  ce.CertificateData
+)
+as st
+group by st.[Standard Name], st.[Standard Reference]
+
+
 
   UNION 
   SELECT
