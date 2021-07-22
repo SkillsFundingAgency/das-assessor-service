3270ca036ea2c6b02dc7aa48302efda279deadcb
Chris Woodcock <chris@humesoftware.com>
2021-07-22 15:22:58 +0100

    Allow an Ilr import to proceed if a certificate exists but is deleted.

--- src/SFA.DAS.AssessorService.Application.UnitTests/Handlers/LearnerDetails/ImportLearnerDetailHandlerTests/When_called_exist_later_source_without_certificate.cs
+++ src/SFA.DAS.AssessorService.Application.UnitTests/Handlers/LearnerDetails/ImportLearnerDetailHandlerTests/When_called_exist_later_source_without_certificate.cs
@@ -1,64 +1,55 @@
-﻿using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.AssessorService.Api.Types.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.AssessorService.Application.UnitTests.Handlers.Learner
 {
     [TestFixture]
-    public class When_called_for_existing_learner_having_later_source_without_certificate : ImportLearnerDetailHandlerTestsBase
+    public class When_called_exist_later_source_without_certificate : ImportLearnerDetailHandlerTestsBase
     {
         [SetUp]
         public void Arrange()
         {
             BaseArrange();
 
-            ImportLearnerDetail = CreateImportLearnerDetail(LearnerTwo);
-
-            // a later source to which should trigger a replacement, the replacement values are null
-            ImportLearnerDetail.Source = "2021";
+            // Arrange
+            ImportLearnerDetail = CreateImportLearnerDetail(LearnerWithoutCertificate);
+            ImportLearnerDetail.Source = "2021"; // a later source to which should trigger a replacement, the replacement values are null
             ImportLearnerDetail.EpaOrgId = null;
             ImportLearnerDetail.LearnActEndDate = null;
             ImportLearnerDetail.WithdrawReason = null;
             ImportLearnerDetail.Outcome = null;
             ImportLearnerDetail.AchDate = null;
             ImportLearnerDetail.OutGrade = null;
+
+            Request = new ImportLearnerDetailRequest
+            {
+                ImportLearnerDetails = new List<ImportLearnerDetail>
+                {
+                    ImportLearnerDetail
+                }
+            };
         }
 
         [Test]
         public async Task Then_an_existing_learner_record_is_replaced()
         {
-            ImportLearnerDetailRequest request = new ImportLearnerDetailRequest
-            {
-                ImportLearnerDetails = new List<ImportLearnerDetail>
-                {
-                    ImportLearnerDetail
-                }
-            };
-
             // Act
-            Response = await Sut.Handle(request, new CancellationToken());
+            Response = await Sut.Handle(Request, new CancellationToken());
 
             // Assert
-            VerifyIlrReplaced(request.ImportLearnerDetails[0], Times.Once);
+            VerifyIlrReplaced(Request.ImportLearnerDetails[0], Times.Once);
         }
 
         [Test]
         public async Task Then_result_is_replacement()
         {
-            ImportLearnerDetailRequest request = new ImportLearnerDetailRequest
-            {
-                ImportLearnerDetails = new List<ImportLearnerDetail>
-                {
-                    ImportLearnerDetail
-                }
-            };
-
             // Act
-            Response = await Sut.Handle(request, new CancellationToken());
+            Response = await Sut.Handle(Request, new CancellationToken());
 
             // Assert
             Response.LearnerDetailResults.Count.Should().Be(1);
