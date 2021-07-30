0af30308358f6f0cee0e1df1a0009c83ac5c403c
Stuart Reeve <stuart.reeve@digital.education.gov.uk>
2021-07-30 15:20:43 +0100

    SV-431

--- src/SFA.DAS.AssessorService.Api.Types/Models/OppFinder/OppFinderApprovedStandardDetailsVersionResult.cs
+++ src/SFA.DAS.AssessorService.Api.Types/Models/OppFinder/OppFinderApprovedStandardDetailsVersionResult.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SFA.DAS.AssessorService.Api.Types.Models
+﻿namespace SFA.DAS.AssessorService.Api.Types.Models
 {
-  public class OppFinderApprovedStandardDetailsVersionResult
+    public class OppFinderApprovedStandardDetailsVersionResult
     {
     public string Version { get; set; }
     public int EndPointAssessors { get; set; }
