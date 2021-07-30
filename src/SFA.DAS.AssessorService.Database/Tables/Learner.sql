32178fc35c3daabfd618a70ba1b990cadd87f8bd
Alan Burns <adburns57@gmail.com>
2021-07-30 14:17:51 +0100

    SV-1122_Learner_table

---
+++ src/SFA.DAS.AssessorService.Database/Tables/Learner.sql
@@ -0,0 +1,24 @@
+CREATE TABLE [dbo].[Learner]
+(
+    [ApprenticeshipId] BIGINT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(100) NULL, 
+    [LastName] NVARCHAR(100) NULL, 
+    [ULN] NVARCHAR(50) NULL, 
+    [StandardCode] INT NULL, 
+    [TrainingCourseVersion] NVARCHAR(10) NULL,
+    [TrainingCourseVersionConfirmed] BIT NOT NULL DEFAULT 0,
+    [TrainingCourseOption] NVARCHAR(126) NULL,
+    [StandardUId] NVARCHAR(20) NULL,
+    [StartDate] DATETIME NULL, 
+    [EndDate] DATETIME NULL, 
+    [CreatedOn] DATETIME NULL, 
+    [UpdatedOn] DATETIME NULL, 
+    [AgreedOn] DATETIME NULL, 
+    [StopDate] DATE NULL, 
+    [PauseDate] DATE NULL, 
+    [StandardReference] NVARCHAR(10) NULL
+)
+GO
+
+CREATE UNIQUE INDEX [IXU_Learner_Uln_StandardCode] ON [Learner] ([Uln], [StandardCode]) INCLUDE ([LastName])
+GO
