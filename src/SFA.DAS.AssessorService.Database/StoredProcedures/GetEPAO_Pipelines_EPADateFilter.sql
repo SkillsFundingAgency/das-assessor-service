f8ff5669cf8758a59d2472b194e1b9a948bae884
Matt <matt.daniels@digital.education.gov.uk>
2021-08-06 14:58:17 +0100

    3 stored procs to return pipeline filter values

---
+++ src/SFA.DAS.AssessorService.Database/StoredProcedures/GetEPAO_Pipelines_EPADateFilter.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [GetEPAO_Pipelines_EPADateFilter]
+      @epaOrgId NVARCHAR(12)
+AS
+BEGIN 
+    SELECT 
+        DISTINCT FORMAT(EstimateDate, 'yyyy-MM-dd') as [Id], EstimateDate as [Value]
+    FROM 
+        [dbo].[EPAO_Func_Get_PipelineInfo] (@epaOrgId, NULL)
+    ORDER BY Id DESC
+END
+GO
