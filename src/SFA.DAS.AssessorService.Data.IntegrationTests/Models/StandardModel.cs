526e4c16ba648805decccc569f6c050fc426421e
codewolf86 <chris@codewolfconsulting.com>
2021-08-12 14:37:31 +0100

    Fix unit tests

--- src/SFA.DAS.AssessorService.Data.IntegrationTests/Models/StandardModel.cs
+++ src/SFA.DAS.AssessorService.Data.IntegrationTests/Models/StandardModel.cs
@@ -7,6 +7,8 @@ namespace SFA.DAS.AssessorService.Data.IntegrationTests.Models
         public string IFateReferenceNumber { get; set; }
         public int LarsCode { get; set; }
         public string Version { get; set; }
+        public int VersionMajor { get; set; }
+        public int VersionMinor { get; set; }
         public int Level { get; set; }
         public string Title { get; set; }
         public string Status { get; set; }
