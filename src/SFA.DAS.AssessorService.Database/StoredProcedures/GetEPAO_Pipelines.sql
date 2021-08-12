1c513fcbc6fea8954b595fc6ca58a7b99b33f5f9
Matt <matt.daniels@digital.education.gov.uk>
2021-08-12 09:09:30 +0100

    Set filter paging row result in the stored proc

--- src/SFA.DAS.AssessorService.Database/StoredProcedures/GetEPAO_Pipelines.sql
+++ src/SFA.DAS.AssessorService.Database/StoredProcedures/GetEPAO_Pipelines.sql
@@ -4,31 +4,31 @@ CREATE PROCEDURE [GetEPAO_Pipelines]
     @providerFilterId NVARCHAR(12),
     @epaDateFilterId NVARCHAR(12)
 AS
-BEGIN
-    WITH Data_CTE 
-    AS
-    (
-        SELECT 
-            StdCode, Title, Version, UkPrn, ProviderName, COUNT(*) Pipeline, EstimateDate
-        FROM 
-            [dbo].[EPAO_Func_Get_PipelineInfo] (@epaOrgId, NULL)
-        GROUP BY 
-            StdCode, UkPrn, ProviderName, EstimateDate, Title, Version
-    ), 
-    Count_CTE 
-    AS 
-    (
-       SELECT COUNT(*) AS TotalRows FROM Data_CTE
-    )
+    BEGIN
+        WITH Data_CTE 
+            AS
+            (
+                SELECT 
+                    StdCode, Title, Version, UkPrn, ProviderName, COUNT(*) Pipeline, EstimateDate
+                FROM 
+                    [dbo].[EPAO_Func_Get_PipelineInfo] ('EPA0200', NULL)
+		        WHERE
+			        (StdCode = @standardFilterId OR @standardFilterId IS NULL) AND (UkPrn = @providerFilterId OR @providerFilterId IS NULL)  AND (FORMAT(EstimateDate, 'yyyyMM') = @epaDateFilterId OR @epaDateFilterId IS NULL)
+                GROUP BY 
+                    StdCode, UkPrn, ProviderName, EstimateDate, Title, Version
+            ), 
+            Count_CTE 
+            AS 
+            (
+               SELECT COUNT(*) AS TotalRows FROM Data_CTE
+            )
     
-    SELECT 
-        *
-    FROM 
-        Data_CTE
-        CROSS JOIN Count_CTE
-    WHERE
-        (StdCode = @standardFilterId OR @standardFilterId IS NULL) AND (UkPrn = @providerFilterId OR @providerFilterId IS NULL)  AND (FORMAT(EstimateDate, 'yyyyMM') = @epaDateFilterId OR @epaDateFilterId IS NULL)
-    ORDER BY 
-        Title, EstimateDate
-END
+            SELECT 
+                *
+            FROM 
+                Data_CTE
+                CROSS JOIN Count_CTE
+            ORDER BY 
+                Title, EstimateDate
+    END
 GO
