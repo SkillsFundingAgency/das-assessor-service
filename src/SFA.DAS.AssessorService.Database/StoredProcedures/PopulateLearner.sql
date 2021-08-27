762579134a31b74186f9c7b55aa85b1ac5401185
Alan Burns <adburns57@gmail.com>
2021-08-27 11:11:35 +0100

    Reworked Populate learner to handle duplicated learner records in ApprovalsExtract for Appenticeship

--- src/SFA.DAS.AssessorService.Database/StoredProcedures/PopulateLearner.sql
+++ src/SFA.DAS.AssessorService.Database/StoredProcedures/PopulateLearner.sql
@@ -18,8 +18,7 @@ BEGIN
 		----------------------------------------------------------------------------------------------------------------------
 		----------------------------------------------------------------------------------------------------------------------
 		WITH LatestVersions
-		AS
-		(
+		AS (
 			SELECT IFateReferenceNumber StandardReference, Title, Version, StandardUId, Larscode, Duration
 			  FROM (
 			   SELECT IFateReferenceNumber, Title, Version, Level, StandardUId, Larscode, ProposedTypicalDuration Duration, 
@@ -31,18 +30,18 @@ BEGIN
 		,
 		----------------------------------------------------------------------------------------------------------------------
 		il1
-		AS
-		( SELECT *
+		AS (
+			SELECT *
 			-- check for ILR records that are for Apprenticeships that have significantly overrun the end date
 			,CASE WHEN EstimatedEndDate < dateadd(month, @expiretime, GETDATE()) THEN 1 ELSE 0 END Expired
 			-- check for "Continuing" ILR records that have not been updated for a long time - they should be updated every month.
 			,CASE WHEN CompletionStatus = 1 AND LastUpdated < DATEADD(month, @lapsedtime, GETDATE()) THEN 1 ELSE 0 END Lapsed
-		   FROM (
-			  SELECT Ilrs.*, ISNULL(UpdatedAt,CreatedAt) LastUpdated
-                      -- if could only be version 1.0 then this can be assumed as confirmed
+			FROM (
+				SELECT Ilrs.*, ISNULL(UpdatedAt,CreatedAt) LastUpdated
+					  -- if could only be version 1.0 then this can be assumed as confirmed
 					  ,CASE WHEN lv1.Version = '1.0' THEN '1.0' ELSE [dbo].[GetVersionFromLarsCode](LearnStartDate,StdCode) END Version
 					  ,CASE WHEN lv1.Version = '1.0' THEN 1 ELSE 0 END VersionConfirmed
-                      -- use StandardUId for version 1.0 (if appropriate) or estimate based on startdate when unknown
+					  -- use StandardUId for version 1.0 (if appropriate) or estimate based on startdate when unknown
 					  ,CASE WHEN lv1.Version = '1.0' THEN lv1.StandardUId ELSE [dbo].[GetStandardUidFromLarsCode](LearnStartDate,StdCode) END StandardUId
 					  ,lv1.StandardReference
 					  ,lv1.Title StandardName
@@ -53,19 +52,87 @@ BEGIN
 		 ,
 		----------------------------------------------------------------------------------------------------------------------
 		 ax1
-		 AS
-		( SELECT Apx.*, ISNULL(UpdatedOn,CreatedOn) LastUpdated
-                  -- map Approvals date to ILR CompletionStatus value
-				  ,CASE WHEN StopDate IS NOT NULL THEN 3
-						WHEN PauseDate IS NOT NULL THEN 6
-						WHEN CompletionDate IS NOT NULL THEN 2 
-						ELSE 1 END CompletionStatus 
+		 AS (
+		 SELECT Apx.*, ISNULL(UpdatedOn,CreatedOn) LastUpdated
 				  -- leave version null if not confirmed in Approvals, as will derive from full LearnStartDate in ILRs or EOMonth of StartDate in Approvals
 				  ,CASE WHEN TrainingCourseVersionConfirmed = 1 THEN TrainingCourseVersion ELSE null END Version
 				  ,TrainingCourseVersionConfirmed VersionConfirmed
-				  ,lv1.Title StandardName
-				  ,CASE WHEN EndDate > GETDATE() THEN EOMONTH(EndDate) ELSE EOMONTH(DATEADD(month, lv1.Duration, StartDate)) END EstimatedEndDate
-			FROM ApprovalsExtract Apx JOIN LatestVersions lv1 on lv1.LarsCode = apx.TrainingCode
+			FROM (
+			SELECT ApprenticeshipId
+				  ,FirstName
+				  ,LastName
+				  ,ULN
+				  ,TrainingCode
+				  ,TrainingCourseVersion
+				  ,TrainingCourseVersionConfirmed
+				  ,TrainingCourseOption
+				  ,StandardUId
+				  ,StartDate
+				  ,EndDate
+				  ,CreatedOn
+				  ,UpdatedOn
+				  ,StopDate
+				  ,PauseDate
+				  ,CompletionDate
+				  ,StandardReference
+				  ,UKPRN 
+				  ,LearnRefNumber
+				  ,CompletionStatus
+			FROM (
+				SELECT *
+                    -- apply rules to determine the chronologically latest Apprenticeship record
+					,ROW_NUMBER() OVER (PARTITION BY ULN, TrainingCode ORDER BY 
+					   CASE 
+					   -- if StopDate of previous record is before Startdate of current record then prefer current record
+					   WHEN StopDate_1 IS NOT NULL AND EOMONTH(StartDate) >= EOMONTH(StopDate_1) THEN 0 
+					   -- if StopDate of current record is before Startdate of previous record then prefer previous record
+					   WHEN StopDate IS NOT NULL AND EOMONTH(StopDate) <= EOMONTH(StartDate_1) THEN 1 
+					   -- if both have StopDate then most recent one is the likely latest record
+					   WHEN StopDate IS NOT NULL AND StopDate_1 IS NOT NULL AND EOMONTH(StopDate_1) > EOMONTH(Stopdate) THEN 1 
+					   ELSE 0 END
+					  ,CASE 
+					   WHEN UKPRN_1 != 0 AND UKPRN != UKPRN_1 THEN
+					   -- different provider, watch-out for retro-Approvals for earlier training period
+						   (CASE WHEN StartDate < StartDate_1 AND EndDate < Enddate_1 THEN 1 ELSE 0 END)
+					   -- same Provider or only one record, so use latest CreatedOn unless	
+					   ELSE 0 END 
+					  ,CreatedOn DESC ) as rownumber
+				FROM (
+				-- inner query gets all records for each learner, ORDER BY CreatedOn desc - there can be many but two iterations should be sufficient
+					SELECT ApprenticeshipId
+					  ,FirstName
+					  ,LastName
+					  ,ULN
+					  ,TrainingCode
+					  ,TrainingCourseVersion
+					  ,TrainingCourseVersionConfirmed
+					  ,TrainingCourseOption
+					  ,StandardUId
+					  ,StartDate
+					  ,EndDate
+					  ,CreatedOn
+					  ,UpdatedOn
+					  ,StopDate
+					  ,PauseDate
+					  ,CompletionDate
+					  ,StandardReference
+					  ,UKPRN
+					  ,LearnRefNumber
+					  -- map Approvals date to ILR CompletionStatus value
+					  ,CASE WHEN StopDate IS NOT NULL THEN 3
+							WHEN PauseDate IS NOT NULL THEN 6
+							WHEN CompletionDate IS NOT NULL THEN 2 
+							ELSE (CASE WHEN PaymentStatus = 1 THEN 1 ELSE 0 END) END CompletionStatus 
+					  ,LAG(UKPRN, 1,0) OVER (PARTITION BY ULN, TrainingCode ORDER BY CreatedOn ) AS UKPRN_1
+					  ,LAG(StartDate, 1,0) OVER (PARTITION BY ULN, TrainingCode ORDER BY CreatedOn ) AS StartDate_1
+					  ,LAG(EndDate, 1,0) OVER (PARTITION BY ULN, TrainingCode ORDER BY CreatedOn ) AS EndDate_1
+					  ,LAG(CONVERT(datetime,StopDate), 1,0) OVER (PARTITION BY ULN, TrainingCode ORDER BY CreatedOn ) AS StopDate_1
+					FROM ApprovalsExtract ap1
+					WHERE 1=1
+					  AND NOT (StopDate IS NOT NULL AND EOMONTH(StopDate) = EOMONTH(StartDate) AND PaymentStatus = 3) -- cancelled, not started, effectively deleted
+				) ab2
+			) ab3 WHERE rownumber = 1
+			) Apx  WHERE CompletionStatus != 0  -- ignore where Apprenticeship is "PendingApproval"
 		)
 		----------------------------------------------------------------------------------------------------------------------
 		----------------------------------------------------------------------------------------------------------------------
@@ -74,21 +141,24 @@ BEGIN
 		Outcome, AchDate, OutGrade, Version, VersionConfirmed, CourseOption, StandardUId, StandardReference, StandardName, LastUpdated, EstimatedEndDate )
 
 		----------------------------------------------------------------------------------------------------------------------
-		-- using Approvals Extract as master, except for some key fields that can be updated via ILR, and full Start&End dates 
+		-- using ILR with Approvals Extract for some key fields that can be updated in Approvals 
 		----------------------------------------------------------------------------------------------------------------------
 		SELECT  il1.Id, 
 				il1.Uln, 
-				CASE WHEN ax1.LastUpdated > il1.LastUpdated THEN ax1.FirstName ELSE il1.GivenNames END GivenNames, 
-				CASE WHEN ax1.LastUpdated > il1.LastUpdated THEN ax1.LastName ELSE il1.FamilyName END FamilyName, 
-				ax1.UkPrn, 
-				ax1.TrainingCode StdCode, 
+				CASE WHEN ax1.LastUpdated < il1.LastUpdated THEN il1.GivenNames ELSE ax1.FirstName END GivenNames, 
+				CASE WHEN ax1.LastUpdated < il1.LastUpdated THEN il1.FamilyName ELSE ax1.LastName END FamilyName, 
+				CASE WHEN ax1.LastUpdated < il1.LastUpdated THEN il1.UkPrn ELSE ax1.UkPrn END UkPrn, 
+				il1.StdCode, 
 				il1.LearnStartDate, 
 				il1.EpaOrgId,	 
 				il1.FundingModel,
-				ax1.ApprenticeshipId,
-				'Approvals+' Source,
-				ax1.LearnRefNumber,
-				CASE WHEN ax1.LastUpdated > il1.LastUpdated THEN ax1.CompletionStatus ELSE il1.CompletionStatus END CompletionStatus,
+				ax1.ApprenticeshipId, 
+				Source+'+App' Source,
+				CASE WHEN ax1.LastUpdated < il1.LastUpdated THEN il1.LearnRefNumber ELSE ax1.LearnRefNumber END LearnRefNumber, 
+				-- Approval Stop or Pause to overridde Active ILR - otherwise use ILR
+				CASE WHEN (ax1.StopDate IS NOT NULL OR ax1.PauseDate IS NOT NULL) AND il1.CompletionStatus = 1 
+					 THEN ax1.CompletionStatus 
+					 ELSE il1.CompletionStatus END CompletionStatus,
 				il1.PlannedEndDate,
 				il1.DelLocPostCode,
 				il1.LearnActEndDate,
@@ -101,52 +171,18 @@ BEGIN
 				ax1.TrainingCourseOption CourseOption,
 				ISNULL(ax1.StandardUId,il1.StandardUId) StandardUid,
 				ISNULL(ax1.StandardReference,il1.StandardReference) StandardReference,
-				ax1.StandardName,
+				il1.StandardName,
 				CASE WHEN ax1.LastUpdated > il1.LastUpdated THEN ax1.LastUpdated ELSE il1.LastUpdated END LastUpdated, 
 				il1.EstimatedEndDate
 		  FROM ax1 
 		  JOIN il1 ON ax1.ULN = il1.ULN  AND il1.StdCode = ax1.TrainingCode	
-		  
-		----------------------------------------------------------------------------------------------------------------------
-		-- just Apporovals	
-		----------------------------------------------------------------------------------------------------------------------
-		UNION 
-		SELECT  NEWID() Id, 
-				ax1.Uln, 
-				ax1.FirstName GivenNames, 
-				ax1.LastName FamilyName, 
-				ax1.UkPrn, 
-				ax1.TrainingCode StdCode, 
-				ax1.StartDate LearnStartDate, 
-				null EpaOrgId,	 
-				36 FundingModel,
-				ax1.ApprenticeshipId,
-				'Approvals' Source,
-				ax1.LearnRefNumber,
-				ax1.CompletionStatus,
-				ax1.EndDate PlannedEndDate,
-				null DelLocPostCode,
-				null LearnActEndDate,
-				null WithdrawReason,
-				null Outcome,
-				null AchDate,
-				null OutGrade,
-				ISNULL(ax1.Version, [dbo].[GetVersionFromLarsCode](EOMONTH(ax1.StartDate),ax1.TrainingCode) ) Version,
-				ax1.VersionConfirmed,
-				ax1.TrainingCourseOption CourseOption,
-				ISNULL(ax1.StandardUId, [dbo].[GetStandardUidFromLarsCode](EOMONTH(ax1.StartDate),ax1.TrainingCode) ) StandardUId,
-				ax1.StandardReference, 
-				ax1.StandardName,
-				ax1.LastUpdated,
-				ax1.EstimatedEndDate
-		  FROM ax1 
-		  LEFT JOIN il1 ON ax1.ULN = il1.ULN  AND il1.StdCode = ax1.TrainingCode
-		  WHERE il1.ULN IS NULL
+		  WHERE NOT ( (ax1.StopDate IS NOT NULL OR ax1.PauseDate IS NOT NULL) AND il1.FundingModel = 99 )
 		  
 		----------------------------------------------------------------------------------------------------------------------
 		-- just ILrs (trimmed Expired and Lapsed ILR records!)  
+		-- or FM99 Ilr to override Stopped/Paused Approval as data source
 		----------------------------------------------------------------------------------------------------------------------
-		UNION 
+		UNION ALL
 		SELECT  il1.Id, 
 				il1.Uln, 
 				il1.GivenNames, 
@@ -177,7 +213,7 @@ BEGIN
 				il1.EstimatedEndDate
 		  FROM il1 
 		  LEFT JOIN ax1 ON ax1.ULN = il1.ULN  AND il1.StdCode = ax1.TrainingCode
-		  WHERE ax1.ULN IS NULL
+		  WHERE (ax1.ULN IS NULL OR ( (ax1.StopDate IS NOT NULL OR ax1.PauseDate IS NOT NULL) AND il1.FundingModel = 99) )
 		  AND Lapsed = 0 
 		  AND Expired = 0
 		  
