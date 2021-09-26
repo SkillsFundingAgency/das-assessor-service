43873840011142ae5a44f2f170c76bd6d7b7ba2a
anilkdhiman6 <anil.dhiman@digital.education.gov.uk>
2021-09-26 10:52:45 +0100

    Add script to alter datatype of Standard Version - from decimal to varchar(20). Post database deployment

---
+++ src/SFA.DAS.AssessorService.Database/PostDeploymentScripts/AlterTableOrgStandardVersionToVarchar.sql
@@ -0,0 +1,19 @@
+﻿/*
+Post-Deployment Script 							
+-----------------------------------------------------------------------------------------------------
+Legacy decimal(18,1) datatype for version field on OrganisationStandardVersion needs to be varchar(20)
+-----------------------------------------------------------------------------------------------------
+*/
+
+IF (EXISTS (SELECT * 
+            FROM INFORMATION_SCHEMA.TABLES 
+            WHERE TABLE_SCHEMA = 'dbo' 
+            AND  TABLE_NAME = 'OrganisationStandardVersion'))
+BEGIN
+  ALTER TABLE  [dbo].[OrganisationStandardVersion]
+  ALTER COLUMN [Version] VARCHAR(20)
+END
+
+
+
+
