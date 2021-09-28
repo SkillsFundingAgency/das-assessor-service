33d2ee0f117e3dad047fa9cf1bc582cb51774beb
anilkdhiman6 <anil.dhiman@digital.education.gov.uk>
2021-09-28 10:37:55 +0100

    Updates following Code review

--- src/SFA.DAS.AssessorService.Database/Script.PostDeployment1.sql
+++ src/SFA.DAS.AssessorService.Database/Script.PostDeployment1.sql
@@ -28,7 +28,7 @@
 :r .\PostDeploymentScripts\CON-3359_Remove_IlrsImport.sql
 
 -- SV-1249
-:r .\PostDeploymentScripts\AlterTableOrgStandardVersionToVarchar.sql
+:r .\PostDeploymentScripts\RecreateStandardVersion.sql
 
 -- ON-613 Patch Certificates with STxxxx StandardReference, where it is not yet included. 
 -- AB 11/03/19 Keep this active for new deployments, for now
