d02bda30b8b7f52d59807e323a040dabe9ed2e9b
Matt <matt.daniels@digital.education.gov.uk>
2021-10-04 19:08:29 +0100

    Helper to select list of values

--- src/SFA.DAS.AssessorService.Data.IntegrationTests/Services/DatabaseService.cs
+++ src/SFA.DAS.AssessorService.Data.IntegrationTests/Services/DatabaseService.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using Dapper;
@@ -83,7 +84,18 @@ namespace SFA.DAS.AssessorService.Data.IntegrationTests.Services
             }    
         }
 
-        
+        public IEnumerable<T> GetList<T>(string sql)
+        {
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("SqlConnectionStringTest")))
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                var result = connection.Query<T>(sql);
+                connection.Close();
+                return result;
+            }
+        }
+
         public object ExecuteScalar(string sql)
         {
             using (var connection = new SqlConnection(Configuration.GetConnectionString("SqlConnectionStringTest")))
