8a266db9a4f15dde94d35ddef5a635530ca5b767
codewolf86 <chris@codewolfconsulting.com>
2021-10-05 22:44:00 +0100

    Switch Truncate to Delete From

--- src/SFA.DAS.AssessorService.Data/ApprovalsExtractRepository.cs
+++ src/SFA.DAS.AssessorService.Data/ApprovalsExtractRepository.cs
@@ -98,7 +98,7 @@ namespace SFA.DAS.AssessorService.Data
 
         public async Task ClearApprovalsExtractStaging()
         {
-            await _unitOfWork.Connection.ExecuteAsync("TRUNCATE TABLE [dbo].ApprovalsExtract_Staging");
+            await _unitOfWork.Connection.ExecuteAsync("DELETE FROM [dbo].ApprovalsExtract_Staging");
         }
 
         /// <summary>
