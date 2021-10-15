3533dcae4a1eae89853be25f8f689f1b5a039699
Ben Stone <ben1.stone@education.gov.uk>
2021-10-15 14:15:45 +0100

    SV-867 Add GetOppFinderNonApprovedStandard

--- src/SFA.DAS.AssessorService.Application/Handlers/OppFinder/GetOppFinderNonApprovedStandardDetailsHandler.cs
+++ src/SFA.DAS.AssessorService.Application/Handlers/OppFinder/GetOppFinderNonApprovedStandardDetailsHandler.cs
@@ -13,33 +13,34 @@ namespace SFA.DAS.AssessorService.Application.Handlers.Standards
     public class GetOppFinderNonApprovedStandardDetailsHandler : IRequestHandler<GetOppFinderNonApprovedStandardDetailsRequest, GetOppFinderNonApprovedStandardDetailsResponse>
     {
         private readonly ILogger<GetOppFinderNonApprovedStandardDetailsHandler> _logger;
-        private readonly IStandardRepository _standardRepository;
+        private readonly IOppFinderRepository _oppFinderRepository;
 
-        public GetOppFinderNonApprovedStandardDetailsHandler(ILogger<GetOppFinderNonApprovedStandardDetailsHandler> logger, IStandardRepository standardRepository)
+        public GetOppFinderNonApprovedStandardDetailsHandler(ILogger<GetOppFinderNonApprovedStandardDetailsHandler> logger, IOppFinderRepository oppFinderRepository)
         {
             _logger = logger;
-            _standardRepository = standardRepository;
+            _oppFinderRepository = oppFinderRepository;
         }
 
         public async Task<GetOppFinderNonApprovedStandardDetailsResponse> Handle(GetOppFinderNonApprovedStandardDetailsRequest request, CancellationToken cancellationToken)
         {
             _logger.LogInformation($"Retreiving non approved standard details: {request.StandardReference}");
-            //var result = await _standardRepository.GetStandardNonApprovedCollationByReferenceNumber(request.StandardReference);
 
-            //if (result == null)
+            var result = await _oppFinderRepository.GetOppFinderNonApprovedStandardDetails(request.StandardReference);
+
+            if (result == null)
                 return null;
 
-            //return new GetOppFinderNonApprovedStandardDetailsResponse
-            //{
-            //    Title = result.Title,
-            //    OverviewOfRole = result.StandardData.OverviewOfRole,
-            //    StandardLevel = (result.StandardData?.Level > 0 ? result.StandardData?.Level.ToString() : "To be confirmed"),
-            //    StandardReference = result.ReferenceNumber,
-            //    Sector = result.StandardData.Category,
-            //    TypicalDuration = result.StandardData?.Duration ?? 0,
-            //    Trailblazer = result.StandardData.Trailblazer,
-            //    StandardPageUrl = result.StandardData.StandardPageUrl
-            //};
+            return new GetOppFinderNonApprovedStandardDetailsResponse
+            {
+                Title = result.Title,
+                OverviewOfRole = result.OverviewOfRole,
+                StandardLevel = int.Parse(result.Level) > 0 ? result.Level : "To be confirmed",
+                StandardReference = result.IFateReferenceNumber,
+                Sector = result.Route,
+                TypicalDuration = result.TypicalDuration,
+                Trailblazer = result.TrailblazerContact,
+                StandardPageUrl = result.StandardPageUrl
+            };
         }
     }
 }
--- src/SFA.DAS.AssessorService.Database/StoredProcedures/StaffReports_ExpressionsOfInterest.sql
+++ src/SFA.DAS.AssessorService.Database/StoredProcedures/StaffReports_ExpressionsOfInterest.sql
@@ -1,21 +1,22 @@
--- Rework without standard collation and non approved collation tables
-CREATE PROCEDURE [StaffReports_ExpressionsOfInterest]
+-- Commented out as Expression of interest as it has been temporarily removed and would require rework if readded
 
-AS
+--CREATE PROCEDURE [StaffReports_ExpressionsOfInterest]
 
-SELECT
-	ISNULL(sc1.[Title], sc2.[Title]) "Name of standard",
-	StandardReference "Standard reference",
-	CASE WHEN sc1.ReferenceNumber IS NOT NULL THEN JSON_VALUE(sc1.[StandardData],'$.Level') ELSE JSON_VALUE(sc2.[StandardData],'$.Level') END "Standard level",
-	[Email] "Email address",
-	[OrganisationName] "Organisation name",
-	ISNULL(ContactName,'') "Contact name",
-	ISNULL(ContactPhone,'') "Contact number",
-	CONVERT(VARCHAR(10),CreatedAt) SubmissionDate
-FROM [ExpressionsOfInterest] eo1
-LEFT JOIN [StandardCollation] sc1 ON sc1.ReferenceNumber = eo1.StandardReference
-LEFT JOIN [StandardNonApprovedCollation] sc2 ON sc2.ReferenceNumber = eo1.StandardReference
-WHERE eo1.Status = 'New'
-ORDER BY CreatedAt DESC;
+--AS
 
-RETURN 0
+--SELECT
+--	ISNULL(sc1.[Title], sc2.[Title]) "Name of standard",
+--	StandardReference "Standard reference",
+--	CASE WHEN sc1.ReferenceNumber IS NOT NULL THEN JSON_VALUE(sc1.[StandardData],'$.Level') ELSE JSON_VALUE(sc2.[StandardData],'$.Level') END "Standard level",
+--	[Email] "Email address",
+--	[OrganisationName] "Organisation name",
+--	ISNULL(ContactName,'') "Contact name",
+--	ISNULL(ContactPhone,'') "Contact number",
+--	CONVERT(VARCHAR(10),CreatedAt) SubmissionDate
+--FROM [ExpressionsOfInterest] eo1
+--LEFT JOIN [StandardCollation] sc1 ON sc1.ReferenceNumber = eo1.StandardReference
+--LEFT JOIN [StandardNonApprovedCollation] sc2 ON sc2.ReferenceNumber = eo1.StandardReference
+--WHERE eo1.Status = 'New'
+--ORDER BY CreatedAt DESC;
+
+--RETURN 0
