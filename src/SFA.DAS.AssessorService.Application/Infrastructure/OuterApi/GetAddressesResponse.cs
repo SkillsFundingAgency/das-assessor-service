6af17f2ee03d0a4db67c67ba535e52196679adc3
Paul Howes <paul.howes@cognetics.net>
2021-10-15 13:19:13 +0100

    Added locations to API

---
+++ src/SFA.DAS.AssessorService.Application/Infrastructure/OuterApi/GetAddressesResponse.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.AssessorService.Application.Infrastructure.OuterApi
+{
+    public class GetAddressesResponse
+    {
+        [JsonProperty("addresses")]
+        public IEnumerable<GetAddressResponse> Addresses { get; set; }
+    }
+
+    public class GetAddressResponse
+    {
+        [JsonProperty("uprn")]
+        public string Uprn { get; set; }
+
+        [JsonProperty("house")]
+        public string House { get; set; }
+
+        [JsonProperty("street")]
+        public string Street { get; set; }
+
+        [JsonProperty("locality")]
+        public string Locality { get; set; }
+
+        [JsonProperty("posttown")]
+        public string PostTown { get; set; }
+
+        [JsonProperty("county")]
+        public string County { get; set; }
+
+        [JsonProperty("postcode")]
+        public string Postcode { get; set; }
+
+        [JsonProperty("longitude")]
+        public double? Longitude { get; set; }
+
+        [JsonProperty("latitude")]
+        public double? Latitude { get; set; }
+
+        [JsonProperty("match")]
+        public double? Match { get; set; }
+    }
+}
