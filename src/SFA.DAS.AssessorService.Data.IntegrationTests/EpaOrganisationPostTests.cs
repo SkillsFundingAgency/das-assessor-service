e283e80d83e71c7f16afa9224268eed76de57f99
Ben Stone <ben1.stone@education.gov.uk>
2021-10-19 13:03:23 +0100

    SV-1277 Revert unnecessary changes

--- src/SFA.DAS.AssessorService.Data.IntegrationTests/EpaOrganisationPostTests.cs
+++ src/SFA.DAS.AssessorService.Data.IntegrationTests/EpaOrganisationPostTests.cs
@@ -30,7 +30,7 @@ namespace SFA.DAS.AssessorService.Data.IntegrationTests
             _repository = new RegisterRepository(_databaseService.WebConfiguration, new Mock<ILogger<RegisterRepository>>().Object);
             _validationRepository = new RegisterValidationRepository(_databaseService.WebConfiguration);
             _queryRepository = new RegisterQueryRepository(_databaseService.WebConfiguration);
-            _organisationIdCreated = "EPA0205";
+            _organisationIdCreated = "EPA0987";
             _ukprnCreated = 123321;
             _organisationTypeId = 5;
             OrganisationTypeHandler.InsertRecord(new OrganisationTypeModel { Id = _organisationTypeId, Status = "new", Type = "organisation type 1" });
