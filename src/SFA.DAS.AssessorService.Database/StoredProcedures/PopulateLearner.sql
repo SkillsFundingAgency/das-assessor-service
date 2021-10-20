dd71ad6b9ee099316ff68fbe31e23ce95de68b3d
Alan Burns <adburns57@gmail.com>
2021-10-20 09:57:47 +0100

    Extend overlap time for both ILR and Approvals updates, to allow for daily failures

--- src/SFA.DAS.AssessorService.Database/StoredProcedures/PopulateLearner.sql
+++ src/SFA.DAS.AssessorService.Database/StoredProcedures/PopulateLearner.sql
@@ -4,8 +4,8 @@ CREATE PROCEDURE [dbo].[PopulateLearner]
 AS
 BEGIN 
    DECLARE 
-		@overlaptimeIlr int = -5,  -- days to allow for an overlap on ILR updates 
-		@overlaptimeApx int = -5,  -- days to allow for an overlap on Approvals changes
+		@overlaptimeIlr int = -15,  -- days to allow for an overlap on ILR updates 
+		@overlaptimeApx int = -15,  -- days to allow for an overlap on Approvals changes
 		@upserted int = 0;
 		
 	BEGIN 
