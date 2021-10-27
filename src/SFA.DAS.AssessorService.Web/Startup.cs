ea2bea9acdb56b4f2a2d4e3a28d00c6ca586e751
Matt Daniels <matt@techtoniq.com>
2021-10-27 16:06:00 +0100

    Remove UseMVC because endpoint routing

--- src/SFA.DAS.AssessorService.Web/Startup.cs
+++ src/SFA.DAS.AssessorService.Web/Startup.cs
@@ -204,6 +204,8 @@ namespace SFA.DAS.AssessorService.Web
                 .UseAuthentication()
                 .UseRequestLocalization()
                 .UseHealthChecks("/health")
+                /*
+                 * // During .Net Core 2.1 => 3.1 migration - warning that UseMVC with routes is not compatible with endpoint routing
                 .UseMvc(routes =>
                 {
                     routes.MapRoute(
@@ -212,7 +214,8 @@ namespace SFA.DAS.AssessorService.Web
                         defaults: new { controller = "Home", action = "Index" }
                         //,constraints: new { controller = new NotEqualRouteContraint("find-an-assessment-opportunity") }
                         );
-                });
+                })
+                */;
         }        
     }
 }
