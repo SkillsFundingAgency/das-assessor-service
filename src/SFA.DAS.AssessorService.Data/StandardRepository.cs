cbb06ac80c3daf3f7e3aa680934a64690fdda67c
Alan Burns <adburns57@gmail.com>
2021-11-01 16:19:48 +0000

    SV-1283_Find_An_EPAO - fix to GetEpaoRegisteredStandardVersionsByIFateReferenceNumber

--- src/SFA.DAS.AssessorService.Data/StandardRepository.cs
+++ src/SFA.DAS.AssessorService.Data/StandardRepository.cs
@@ -626,7 +626,7 @@ FROM [Standards] Where [IFateReferenceNumber] = @iFateReferenceNumber";
         public async Task<IEnumerable<StandardVersion>> GetEpaoRegisteredStandardVersionsByIFateReferenceNumber(string endPointAssessorOrganisationId, string iFateReferenceNumber)
         {
 
-            var sql = @"SELECT osv.StandardUId, os.StandardCode as LarsCode, s.Title, s.Level, s.IFateReferenceNumber, s.Version
+            var sql = @"SELECT osv.StandardUId, os.StandardCode as LarsCode, s.Title, s.Level, s.IFateReferenceNumber, s.Version, osv.EffectiveFrom, osv.EffectiveTo, osv.DateVersionApproved, osv.Status
                         FROM [dbo].[OrganisationStandardVersion] osv
                         INNER JOIN [dbo].[OrganisationStandard] os on osv.OrganisationStandardId = os.Id
                         INNER JOIN [dbo].[Organisations] o on os.EndPointAssessorOrganisationId = o.EndPointAssessorOrganisationId AND o.EndPointAssessorOrganisationId = @endPointAssessorOrganisationId
