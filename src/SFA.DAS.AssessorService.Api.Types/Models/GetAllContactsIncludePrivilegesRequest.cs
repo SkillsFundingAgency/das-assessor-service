483cbd1644c317a5b08755d0dc20621bef6b7251
Matt <matt.daniels@digital.education.gov.uk>
2021-11-05 12:06:20 +0000

    Workaround for deserialization exception

--- src/SFA.DAS.AssessorService.Api.Types/Models/GetAllContactsIncludePrivilegesRequest.cs
+++ src/SFA.DAS.AssessorService.Api.Types/Models/GetAllContactsIncludePrivilegesRequest.cs
@@ -5,13 +5,16 @@ namespace SFA.DAS.AssessorService.Api.Types.Models
 {
     public class GetAllContactsIncludePrivilegesRequest: IRequest<List<ContactIncludePrivilegesResponse>>
     {
+        // @ToDo: .Net Core 3.1 upgrade temporary workaround - adding default constructor to get around deserialization exception
+        public GetAllContactsIncludePrivilegesRequest() { }
+
         public GetAllContactsIncludePrivilegesRequest(string endPointAssessorOrganisationId, bool? withUser = null)
         {
             EndPointAssessorOrganisationId = endPointAssessorOrganisationId;
             WithUser = withUser;
         }
 
-        public string EndPointAssessorOrganisationId { get;}
-        public bool? WithUser { get; }
+        public string EndPointAssessorOrganisationId { get; set; } // @ToDo: .Net Core 3.1 upgrade temporary workaround - adding setter to get around deserialization exception
+        public bool? WithUser { get; set; } // @ToDo: .Net Core 3.1 upgrade temporary workaround - adding setter to get around deserialization exception
     }
 }
