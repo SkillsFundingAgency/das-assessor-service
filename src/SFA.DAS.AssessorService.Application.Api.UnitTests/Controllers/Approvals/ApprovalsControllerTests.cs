a5b2ad250f6187aa5a6f7e40d95876c6c42d480e
codewolf86 <chris@codewolfconsulting.com>
2021-11-16 14:30:56 +0000

    Rework for Setup method

--- src/SFA.DAS.AssessorService.Application.Api.UnitTests/Controllers/Approvals/ApprovalsControllerTests.cs
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/Controllers/Approvals/ApprovalsControllerTests.cs
@@ -18,30 +18,37 @@ namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Controllers.Approval
     [TestFixture]
     public class ApprovalsControllerTests
     {
+        private Mock<IMediator> _mockMediator;
+        private ApprovalsController _sut;
+
+        [SetUp]
+        public void SetupTests()
+        {
+            _mockMediator = new Mock<IMediator>();
+            _sut = new ApprovalsController(Mock.Of<ILogger<ApprovalsController>>(), _mockMediator.Object);
+        }
+
+
         [Test, AutoData]
         public async Task When_PostToImportingApprovals_Then_ImportApprovalsHandlerIsCalled(Mock<IMediator> mockMediator)
         {
             // Arrange.
-            var sut = SetupApprovalsController(mediator: mockMediator);
-
+            
             // Act.
-
-            var controllerResult = await sut.GatherAndStoreApprovals() as ObjectResult;
+            var controllerResult = await _sut.GatherAndStoreApprovals() as ObjectResult;
 
             // Assert.
 
-            mockMediator.Verify(m => m.Send(It.IsAny<ImportApprovalsRequest>(), It.IsAny<CancellationToken>()), Times.Once);
+            _mockMediator.Verify(m => m.Send(It.IsAny<ImportApprovalsRequest>(), It.IsAny<CancellationToken>()), Times.Once);
         }
 
         [Test]
         public async Task When_GatherAndStoreApprovalsHasNoErrors_Then_ReturnOK()
         {
             // Arrange.
-            var sut = SetupApprovalsController();
-
+            
             // Act.
-
-            var controllerResult = await sut.GatherAndStoreApprovals() as ObjectResult;
+            var controllerResult = await _sut.GatherAndStoreApprovals() as ObjectResult;
 
             // Assert.
 
@@ -49,26 +56,25 @@ namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Controllers.Approval
         }
 
         [Test, AutoData]
-        public async Task When_GettingApprovalsLearnerRecord_ThenGetAppovalsLearnerRecordHandlerIsCalled(int stdCode, long uln, Mock<IMediator> mockMediator)
+        public async Task When_GettingApprovalsLearnerRecord_ThenGetAppovalsLearnerRecordHandlerIsCalled(int stdCode, long uln)
         {
             // Arrange.
-            var sut = SetupApprovalsController(mediator: mockMediator);
-            
+                        
             // Act.
-            var controllerResult = await sut.GetLearner(stdCode, uln) as ObjectResult;
+            var controllerResult = await _sut.GetLearner(stdCode, uln) as ObjectResult;
 
             // Assert.
-            mockMediator.Verify(m => m.Send(It.Is<GetApprovalsLearnerRecordRequest>(s => s.StdCode == stdCode && s.Uln == uln), It.IsAny<CancellationToken>()), Times.Once);
+            _mockMediator.Verify(m => m.Send(It.Is<GetApprovalsLearnerRecordRequest>(s => s.StdCode == stdCode && s.Uln == uln), It.IsAny<CancellationToken>()), Times.Once);
         }
 
         [Test, AutoData]
         public async Task When_GettingAprovalsLearnerRecordHasNoErrors_Then_ReturnOK(int stdCode, long uln, ApprovalsLearnerResult result)
         {
             // Arrange.
-            var sut = SetupApprovalsController(result);
+            _mockMediator.Setup(s => s.Send(It.IsAny<GetApprovalsLearnerRecordRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(result);
 
             // Act.
-            var controllerResult = await sut.GetLearner(stdCode, uln) as ObjectResult;
+            var controllerResult = await _sut.GetLearner(stdCode, uln) as ObjectResult;
 
             // Assert.
             controllerResult.StatusCode.Should().Be((int)HttpStatusCode.OK);
@@ -78,22 +84,13 @@ namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Controllers.Approval
         public async Task When_GettingAprovalsLearnerRecordNotFound_Then_ReturnNotFound(int stdCode, long uln)
         {
             // Arrange.
-            var sut = SetupApprovalsController();
+            _mockMediator.Setup(s => s.Send(It.IsAny<GetApprovalsLearnerRecordRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync((ApprovalsLearnerResult)null);
 
             // Act.
-            var controllerResult = await sut.GetLearner(stdCode, uln) as NotFoundResult;
+            var controllerResult = await _sut.GetLearner(stdCode, uln) as NotFoundResult;
 
             // Assert.
             controllerResult.StatusCode.Should().Be((int)HttpStatusCode.NotFound);
-        }
-
-        private ApprovalsController SetupApprovalsController(ApprovalsLearnerResult result = null, Mock<IMediator> mediator = null)
-        {
-            var mockMediator = mediator ?? new Mock<IMediator>();
-            mockMediator.Setup(s => s.Send(It.IsAny<GetApprovalsLearnerRecordRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(result);
-            var sut = new ApprovalsController(Mock.Of<ILogger<ApprovalsController>>(), mockMediator.Object);
-
-            return sut;
         }
     }
 }
