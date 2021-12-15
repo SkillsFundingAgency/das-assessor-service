336f9f71dbfaaf323d3702b1ce5287e5faae8579
techtoniq <matt@techtoniq.com>
2021-12-15 11:20:16 +0000

    Temporarily disable the SV-1305 changes (#1392)

--- src/SFA.DAS.AssessorService.Web/Controllers/OrganisationStandardsController.cs
+++ src/SFA.DAS.AssessorService.Web/Controllers/OrganisationStandardsController.cs
@@ -78,6 +78,11 @@ namespace SFA.DAS.AssessorService.Web.Controllers
                 return RedirectToAction("NotRegistered", "Home");
             }
 
+            // Temporarily disable SV-1305 because testing has uncovered a scenario not considered during development:
+            // if there are one or more applications in progress for an organisation when its financial assessment has expired, the code
+            // resumes the latest application in progress rather than resume at the financial application section as per the A/C
+            model.FinancialInfoStage1Expired = false;
+
             return View("Index", model);
         }
 
