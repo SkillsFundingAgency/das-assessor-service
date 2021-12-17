7054aeeddd142cf32c0ca72db5b73b4011e8bfb2
Chris Woodcock <chris@humesoftware.com>
2021-12-17 10:55:40 +0000

    Update content to match prototype for check; fixed id's and defaulted name

--- src/SFA.DAS.AssessorService.Web/ViewModels/Certificate/CertificateSendToViewModel.cs
+++ src/SFA.DAS.AssessorService.Web/ViewModels/Certificate/CertificateSendToViewModel.cs
@@ -18,9 +18,15 @@ namespace SFA.DAS.AssessorService.Web.ViewModels.Certificate
 
         public override Certificate GetCertificateFromViewModel(Certificate certificate, CertificateData certData)
         {
-            if (certData.SendTo != SendTo)
+            if (SendTo != certData.SendTo)
             {
                 certData = ClearContactInformation(certData);
+            }
+
+            if(SendTo == CertificateSendTo.Apprentice)
+            {
+                // when sending to the apprentice use the apprentice name for the contact
+                certData.ContactName = certData.FullName;
             }
 
             certData.SendTo = SendTo;
