819bfc90effe51a6c0b0a6d0d3f88c650320924d
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2022-01-06 09:09:23 +0000

    CON-4273-Loqate cleanup

--- src/SFA.DAS.AssessorService.Web/Extensions/SecurityHeadersExtensions.cs
+++ src/SFA.DAS.AssessorService.Web/Extensions/SecurityHeadersExtensions.cs
@@ -15,7 +15,7 @@ namespace SFA.DAS.AssessorService.Web.Extensions
                 "default-src 'self' 'unsafe-inline' https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com https://*.rcrsv.io; " +
                 "img-src 'self' *.google-analytics.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com; " +
                 "script-src 'self' 'unsafe-inline' " +
-                "*.googletagmanager.com *.postcodeanywhere.co.uk *.google-analytics.com *.googleapis.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com https://*.rcrsv.io; " +
+                "*.googletagmanager.com *.google-analytics.com *.googleapis.com https://*.zdassets.com https://*.zendesk.com wss://*.zendesk.com wss://*.zopim.com https://*.rcrsv.io; " +
                 "font-src 'self' data: https://*.rcrsv.io;";
                 context.Response.Headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
                 context.Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
