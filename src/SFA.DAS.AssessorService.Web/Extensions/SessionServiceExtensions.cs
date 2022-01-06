9b9f2e63b65a335db526fe0c61951bc2370d337e
Chris Woodcock <chris@humesoftware.com>
2022-01-06 16:03:52 +0000

    Fixed return to check page for options and versions

--- src/SFA.DAS.AssessorService.Web/Extensions/SessionServiceExtensions.cs
+++ src/SFA.DAS.AssessorService.Web/Extensions/SessionServiceExtensions.cs
@@ -5,6 +5,7 @@ namespace SFA.DAS.AssessorService.Web.Extensions
     public static class CertificateSessionExtensions
     {
         private const string RedirectToCheck = nameof(RedirectToCheck);
+        private const string RedirectedFromVersion = nameof(RedirectedFromVersion);
 
         public static bool GetRedirectToCheck(this ISessionService sessionService)
         {
@@ -14,6 +15,21 @@ namespace SFA.DAS.AssessorService.Web.Extensions
         public static void SetRedirectToCheck(this ISessionService sessionService, bool value)
         {
             sessionService.Set(RedirectToCheck, value);
+        }
+
+        public static bool GetRedirectedFromVersion(this ISessionService sessionService)
+        {
+            return sessionService.TryGet<bool>(RedirectedFromVersion, out var redirectedFromVersion) ? redirectedFromVersion : false;
+        }
+
+        public static void SetRedirectedFromVersion(this ISessionService sessionService, bool value)
+        {
+            sessionService.Set(RedirectedFromVersion, value);
+        }
+
+        public static void RemoveRedirectedFromVersion(this ISessionService sessionService)
+        {
+            sessionService.Remove(RedirectedFromVersion);
         }
     }
 }
