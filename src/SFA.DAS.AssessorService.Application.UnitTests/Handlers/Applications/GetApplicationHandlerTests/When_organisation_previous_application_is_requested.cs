f6e7f9fbca657b7dfd6d5b51204fd8610e54f926
anilkdhiman6 <anil.dhiman@digital.education.gov.uk>
2022-01-12 13:32:55 +0000

    Unit test amendments

--- src/SFA.DAS.AssessorService.Application.UnitTests/Handlers/Applications/GetApplicationHandlerTests/When_organisation_previous_application_is_requested.cs
+++ src/SFA.DAS.AssessorService.Application.UnitTests/Handlers/Applications/GetApplicationHandlerTests/When_organisation_previous_application_is_requested.cs
@@ -1,9 +1,12 @@
 using AutoMapper;
+using FluentAssertions;
+using MediatR;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.AssessorService.Api.Types.Models.Apply;
 using SFA.DAS.AssessorService.Application.Handlers.Apply;
 using SFA.DAS.AssessorService.Application.Interfaces;
+using SFA.DAS.AssessorService.Domain.Consts;
 using SFA.DAS.AssessorService.Domain.DTOs;
 using System;
 using System.Collections.Generic;
@@ -12,31 +15,50 @@ using System.Threading.Tasks;
 
 namespace SFA.DAS.AssessorService.Application.UnitTests.Handlers.Applications.GetApplicationHandlerTests
 {
+    [TestFixture]
     public class When_organisation_previous_application_is_requested
     {
 
-        private Mock<IApplyRepository> ApplyRepository;
-        private GetPreviousApplicationsHandler Handler;
+        private Mock<IApplyRepository> _mockApplyRepository;
+        private GetPreviousApplicationsHandler _sut;
 
         [SetUp]
         public void Setup()
         {
+            _mockApplyRepository = new Mock<IApplyRepository>();
+            _mockApplyRepository.Setup(s => s.GetPreviousApplicationsForStandard(It.IsAny<Guid>(), It.IsAny<string>()))
+                .ReturnsAsync(new List<ApplySummary> { new ApplySummary { StandardCode = 59 } });
+
             Mapper.Reset();
             Mapper.Initialize(cfg =>
             {
-                cfg.CreateMap<List<ApplySummary>, List<ApplicationResponse>>();
+                cfg.CreateMap<ApplySummary, ApplicationResponse>();
             });
 
-            ApplyRepository = new Mock<IApplyRepository>();
-            Handler = new GetPreviousApplicationsHandler(ApplyRepository.Object);
+            _sut = new GetPreviousApplicationsHandler(_mockApplyRepository.Object);
         }
 
 
         [Test]
         public async Task Then_organisation_previous_application_is_retrieved()
         {
-            await Handler.Handle(new GetPreviousApplicationsRequest(new Guid(), ""), new CancellationToken());
-            ApplyRepository.Verify(r => r.GetPreviousApplicationsForStandard(It.IsAny<Guid>(),It.IsAny<string>()), Times.Once);
+            await _sut.Handle(new GetPreviousApplicationsRequest(new Guid(), ""), new CancellationToken());
+            _mockApplyRepository.Verify(r => r.GetPreviousApplicationsForStandard(It.IsAny<Guid>(),It.IsAny<string>()), Times.Once);
         }
+
+        [Test]
+        public async Task Then_org_previous_application_is_retrieved_and_mapped_correctly()
+        {
+            //Arrange
+            var request = new GetPreviousApplicationsRequest(new Guid("a623f742-2607-4bc0-85a7-ec5b6dd6a593"), "ST0001");
+
+            //Act
+            var result = await _sut.Handle(request, new CancellationToken());
+
+            //Assert
+            result.Count.Should().Equals(1);
+            result[0].StandardCode.Should().Equals(59);
+        }
+
     }
 }
--- src/SFA.DAS.AssessorService.Web.UnitTests/StandardControllerTests/When_OptIn_Is_Posted_To.cs
+++ src/SFA.DAS.AssessorService.Web.UnitTests/StandardControllerTests/When_OptIn_Is_Posted_To.cs
@@ -34,9 +34,17 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.StandardControllerTests
                });
 
             _mockApiClient
+            .Setup(r => r.GetAllWithdrawnApplicationsForStandard(It.IsAny<Guid>(), It.IsAny<int>()))
+                .ReturnsAsync(new List<ApplicationResponse> {
+                    new ApplicationResponse { StandardReference = "ST0001", ApplyData = new ApplyData{ Apply = new Apply{ Versions = new List<string>{"1.1","1.2"}}},
+                        StandardApplicationType = StandardApplicationTypes.VersionWithdrawal}
+            });
+
+            _mockApiClient
                 .Setup(r => r.GetPreviousApplicationsForStandard(It.IsAny<Guid>(), It.IsAny<string>()))
                 .ReturnsAsync(new List<ApplicationResponse> { 
-                    new ApplicationResponse { StandardReference = "ST0001", StandardApplicationType = StandardApplicationTypes.VersionWithdrawal}
+                    new ApplicationResponse { StandardReference = "ST0001",  ApplyData = new ApplyData{ Apply = new Apply{ Versions = new List<string>{"1.1","1.2"}}}, 
+                        StandardApplicationType = StandardApplicationTypes.VersionWithdrawal}
                      });   
 
             _mockContactsApiClient.Setup(r => r.GetContactBySignInId(It.IsAny<String>()))
