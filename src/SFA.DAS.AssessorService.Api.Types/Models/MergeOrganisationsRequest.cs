8e03dd7ac1f0f5f2ad006204939efddf6ec67d62
techtoniq <matt@techtoniq.com>
2022-02-03 19:11:30 +0000

    SV-1322 Organisation Merge (#1397)
    * Schema changes
    
    * Add missing OrganisationStandardData field to EF model
    
    * Add [NotMapped] to fields in the EF model which are not in the database
    
    * Rename StandardReference to StandardCode
    
    * Add PK to MergeOrganisationStandardVersion
    
    * Fix index on OrganisationStandardVersion
    
    * Fix incorrect key on OrganisationStandardDeliveryArea
    
    * Merge logic working
    
    * Tweak indexes
    
    * Add After snapshot
    
    * Skeleton tests / tdd
    
    * Remove unique constraints from Merge Organisations primary and secondary IDs
    
    * Fix typo
    
    * Add API endpoints to create merge and get merge log
    
    * Add fields to MergeOrganisation
    
    * Add Get endpoint
    
    * Add paging and filtering to the GetMergeOrganisations endpoint
    
    * Add organisation names to the merge log
    
    * tweak column names
    
    * Lengthen status field
    
    * Increase status field length
    
    * API changes to service UI requests per discussion with Ben
    
    * Add sort order and sort direction to the merge log api
    
    * SV-1322 Update orderBy to check "desc" to match existing tag helpers
    
    * Send confirmation email after merge
    
    * Fix Sonar bug
    
    * Fixing code smells
    
    * API and schema changes to suppoer username instead of userid
    
    * Schema changes + snapshot applications, and also delete in-progress applications for secondary organisation on merge.
    
    * Only delete in-progress applications
    
    * Further schema changes and first cut of undo script
    
    * Fix lazy loading issue
    
    * Tweak key on OrganisationStandardVersion
    
    * Tweak logic per feedback
    
    * Fix duplicate code
    
    * Refactor undo script
    
    * Tweak to the undo script
    
    * Tweak error handling in merge undo script and only restore merges at status 'Completed'
    
    * Wrap a transaction around the merge
    
    * Fix transaction issue in merge
    
    * Update to undo script to remove identity inserts
    
    * Bug fix only return completed merges in the merge log
    
    * Tweak to logic that checks if an EPAO has been used in a merge.
    
    * Fix to logic for re-using a reverted secondary organisation in a merge
    
    * Fix integration tests
    
    * Tidy up integration tests
    
    * Added unit tests around the snapshot construction.
    
    * Change comments to match ACs
    
    * Fix email sending condition
    
    * Remove commented-out code
    
    * Remove commented out code
    
    * Remove unnecessary Version in the index
    
    * SV-1322 Update date format
    
    * QF-1322 Add string format to primary email
    
    * SV-1322 Update email tokens
    
    Co-authored-by: Ben Stone <ben1.stone@education.gov.uk>

---
+++ src/SFA.DAS.AssessorService.Api.Types/Models/MergeOrganisationsRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SFA.DAS.AssessorService.Domain.Entities;
+using System;
+
+namespace SFA.DAS.AssessorService.Api.Types.Models
+{
+    public class MergeOrganisationsRequest : IRequest<MergeOrganisation>
+    {
+        public string PrimaryEndPointAssessorOrganisationId { get; set; }
+        public string SecondaryEndPointAssessorOrganisationId { get; set; }
+        public DateTime SecondaryStandardsEffectiveTo { get; set; }
+        public string ActionedByUser { get; set; }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Application/Interfaces/IOrganisationMergingService.cs
@@ -0,0 +1,11 @@
+﻿using SFA.DAS.AssessorService.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.AssessorService.Application.Interfaces
+{
+    public interface IOrganisationMergingService
+    {
+        Task<MergeOrganisation> MergeOrganisations(Organisation primaryOrganisation, Organisation secondaryOrganisation, DateTime secondaryStandardsEffectiveTo, string actionedByUser);
+    }
+}
