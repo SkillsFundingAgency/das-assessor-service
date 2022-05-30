c5c01e3083df5700195c057ac5329460c1a2e5f6
Ian Rose <ian.rose@digital.education.gov.uk>
2022-05-30 16:01:38 +0100

    Altered unit test to reflect new validation

--- src/SFA.DAS.AssessorService.Web.UnitTests/StandardControllerTests/When_OptIn_Is_Posted_To.cs
+++ src/SFA.DAS.AssessorService.Web.UnitTests/StandardControllerTests/When_OptIn_Is_Posted_To.cs
@@ -24,7 +24,7 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.StandardControllerTests
         [Test]
         public async Task Then_Version_Is_Opted_In()
         {
-            // Arrange
+            //Arrange
             _mockStandardVersionApiClient
                .Setup(r => r.GetStandardVersionsByIFateReferenceNumber("ST0001"))
                .ReturnsAsync(new List<StandardVersion> {
@@ -33,10 +33,18 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.StandardControllerTests
                    new StandardVersion { IFateReferenceNumber = "ST0001", Title = "Title 1", Version = "1.2", LarsCode = 1, EPAChanged = false},
                });
 
+            _mockOrgApiClient
+               .Setup(r => r.GetAppliedStandardVersionsForEPAO(It.IsAny<string>(), "ST0001"))
+               .ReturnsAsync(new List<AppliedStandardVersion> {
+                   new AppliedStandardVersion { IFateReferenceNumber = "ST0001", Title = "Title 1", Version = "1.0", LarsCode = 1, EPAChanged = false},
+                   new AppliedStandardVersion { IFateReferenceNumber = "ST0001", Title = "Title 1", Version = "1.1", LarsCode = 1, EPAChanged = false},
+                   new AppliedStandardVersion { IFateReferenceNumber = "ST0001", Title = "Title 1", Version = "1.2", LarsCode = 1, EPAChanged = false}
+               });
+
             _mockApiClient
-            .Setup(r => r.GetAllWithdrawnApplicationsForStandard(It.IsAny<Guid>(), It.IsAny<int>()))
+                .Setup(r => r.GetAllWithdrawnApplicationsForStandard(It.IsAny<Guid>(), It.IsAny<int>()))
                 .ReturnsAsync(new List<ApplicationResponse> {
-                    new ApplicationResponse { StandardReference = "ST0001", ApplyData = new ApplyData{ Apply = new Apply{ Versions = new List<string>{"1.1","1.2"}}},
+                    new ApplicationResponse { StandardReference = "ST0001", ApplyData = new ApplyData{ }, // Apply = new Apply{ Versions = new List<string>{"1.1","1.2"}}},
                         StandardApplicationType = StandardApplicationTypes.VersionWithdrawal}
             });
 
@@ -44,8 +52,8 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.StandardControllerTests
                 .Setup(r => r.GetPreviousApplicationsForStandard(It.IsAny<Guid>(), It.IsAny<string>()))
                 .ReturnsAsync(new List<ApplicationResponse> { 
                     new ApplicationResponse { StandardReference = "ST0001",  ApplyData = new ApplyData{ Apply = new Apply{ Versions = new List<string>{"1.1","1.2"}}}, 
-                        StandardApplicationType = StandardApplicationTypes.VersionWithdrawal}
-                     });   
+                        StandardApplicationType = StandardApplicationTypes.Version}
+            });
 
             _mockContactsApiClient.Setup(r => r.GetContactBySignInId(It.IsAny<String>()))
             .ReturnsAsync(new ContactResponse()
