7fc703b1b44f8dc776f1983db9aa5217e10834ad
PeterDavidWilliams <30286445+PeterDavidWilliams@users.noreply.github.com>
2022-10-06 12:28:44 +0100

    Qf 579 upgrade newtonsoft to 13.0.1 (#1443)
    * Upgraded to 13.0.0.1
    
    * Upgraded missed projects to 13.0.1
    
    * QF-533 accessibility fixes (#1444)
    
    * QF533-accessibility-legend-in-fieldset
    
    * QF533 accessibility fixes
    
    * QF533 non unique ids
    
    * QF533 Fixloop variable init
    
    * QF644 accessibility fixes (#1445)
    
    Co-authored-by: Ian Rose <94618382+ianatesfa@users.noreply.github.com>

--- src/SFA.DAS.AssessorService.ApplyTypes/Application.cs
+++ src/SFA.DAS.AssessorService.ApplyTypes/Application.cs
@@ -1,9 +1,9 @@
-using Newtonsoft.Json;
 using SFA.DAS.AssessorService.ApplyTypes.CharityCommission;
 using SFA.DAS.AssessorService.ApplyTypes.CompaniesHouse;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 
 namespace SFA.DAS.AssessorService.ApplyTypes
 {
