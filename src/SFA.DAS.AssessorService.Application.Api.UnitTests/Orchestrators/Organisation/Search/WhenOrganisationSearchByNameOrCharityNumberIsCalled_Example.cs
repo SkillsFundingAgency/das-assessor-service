7e03d5e1523ecb3249d3dc4bb0946299ce2ed0a0
Saw Naw <saw.naw@digital.education.gov.uk>
2023-03-20 19:03:29 +0000

    Add example tests

---
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/Orchestrators/Organisation/Search/WhenOrganisationSearchByNameOrCharityNumberIsCalled_Example.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.AssessorService.Application.Api.Orchestrators;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Orchestrators.OrganisationSearch
+{
+    public class WhenOrganisationSearchByNameOrCharityNumberOrCompanyNumberIsCalled_Example : OrganisationSearchTestBase
+    {
+        [TestCase("Blue")]
+        [TestCase("Green")]
+        [TestCase("Yellow")]
+        [TestCase("Green Grass Limited")]
+        public async Task ThenOrganisationReturnedIfNameContainsSearchTerm(string searchTerm)
+        {
+            // Act
+            var sut = new OrganisationSearchOrchestrator(_logger.Object, _roatpApiClient.Object, _referenceDataApiClient.Object, _mediator.Object, _regexHelper.Object);
+            var results = await sut.OrganisationSearchByNameOrCharityNumberOrCompanyNumber(searchTerm);
+
+            // Assert
+            Assert.That(results.All(r => r.Name.Contains(searchTerm)));
+        }
+
+        [TestCase("Green Aliens Conglomerate")] // matches "Green" in "Green Grass Limited"
+        [TestCase("Blah Blah Limited")] // matches "Limited" in "Blue Barns Limited" or "Green Grass Limited"
+        public async Task ThenOrganisationReturnedIfAnyWordInSearchTermMatches(string searchTerm)
+        {
+            // Act
+            var sut = new OrganisationSearchOrchestrator(_logger.Object, _roatpApiClient.Object, _referenceDataApiClient.Object, _mediator.Object, _regexHelper.Object);
+            var results = await sut.OrganisationSearchByNameOrCharityNumberOrCompanyNumber(searchTerm);
+
+            // Assert
+            Assert.That(results.All(r => r.Name.Split(' ')
+                                               .Intersect(searchTerm.Split(' '))
+                                               .Any()));
+        }
+
+        [TestCase("Lollipop")]
+        [TestCase("Space Pirates Inc.")]
+        [TestCase("Rawrrr rawrrrr")]
+        [TestCase("98765432")]
+        public async Task ThenEmptyCollectionReturnedIfMatchFails(string searchTerm)
+        {
+            // Act
+            var sut = new OrganisationSearchOrchestrator(_logger.Object, _roatpApiClient.Object, _referenceDataApiClient.Object, _mediator.Object, _regexHelper.Object);
+            var results = await sut.OrganisationSearchByNameOrCharityNumberOrCompanyNumber(searchTerm);
+
+            // Assert
+            Assert.That(results, Is.Empty);
+        }
+
+        [TestCase("00030004", "Green Grass Limited")]
+        [TestCase("00030001", "Blue Barns Limited")]
+        [TestCase("00030002", "Sky Blue Ltd")]
+        public async Task ThenResultHasMatchingCompanyNumberAndName(string number, string expectedName)
+        {
+            // Act
+            var sut = new OrganisationSearchOrchestrator(_logger.Object, _roatpApiClient.Object, _referenceDataApiClient.Object, _mediator.Object, _regexHelper.Object);
+            var results = await sut.OrganisationSearchByNameOrCharityNumberOrCompanyNumber(number);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(results.Single().CompanyNumber, Is.EqualTo(number));
+                Assert.That(results.Single().Name, Is.EqualTo(expectedName));
+            });
+        }
+    }
+}
