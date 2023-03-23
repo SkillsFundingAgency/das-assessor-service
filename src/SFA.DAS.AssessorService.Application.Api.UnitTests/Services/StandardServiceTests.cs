1aa04412dd2d3f31f4498fb2ce1424e8072af236
Peter Williams <peter.david.williams.1991@gmail.com>
2023-03-23 15:16:35 +0000

    Refactored unit tests again - testcase didn't work because of mock setups differing per test case. Added unit tests to cover exception logging

--- src/SFA.DAS.AssessorService.Application.Api.UnitTests/Services/StandardServiceTests.cs
+++ src/SFA.DAS.AssessorService.Application.Api.UnitTests/Services/StandardServiceTests.cs
@@ -280,23 +280,121 @@ namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Services
             Assert.AreEqual(null, result);
         }
 
-        [Test]
-        [InlineAutoData("1", StandardId.StandardIdType.LarsCode)]
-        [InlineAutoData("IFATE1", StandardId.StandardIdType.IFateReferenceNumber)]
-        [InlineAutoData("Test", StandardId.StandardIdType.StandardUId)]
-        [InlineAutoData(null, StandardId.StandardIdType.Unknown)]
-        public async Task When_GettingStandardVersionById_And_StandardIsNotFound_Then_LogInfo_And_ReturnNull(string standardReference, StandardId.StandardIdType idType, string version)
+        [Test, AutoData]
+        public async Task When_GettingStandardVersionsByIFateReferenceNumber_And_ExceptionThrown_Then_LogError_And_ReturnNull(string standardReference)
         {
+            _mockStandardRepository.Setup(repository => repository.GetStandardVersionsByIFateReferenceNumber(It.IsAny<string>()))
+                .Throws(new Exception());
+
+            var result = await _standardService.GetStandardVersionsByIFateReferenceNumber(standardReference);
+
+            VerifyLogger(LogLevel.Error, new EventId(0), $"STANDARD VERSION: Failed to get for iFateReferenceNumber: {standardReference}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test, AutoData]
+        public async Task When_GettingStandardVersionById_And_TheStandardIdTypeIsLarsCode_And_StandardIsNotFound_Then_LogInfo_And_ReturnNull(string version)
+        {
+            string standardReference = "1";
+
             _mockStandardRepository.Setup(repository => repository.GetStandardVersionByLarsCode(It.IsAny<int>(), It.IsAny<string>()))
                 .ReturnsAsync((Standard)null);
 
             var result = await _standardService.GetStandardVersionById(standardReference, version);
 
-            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard for Id: {standardReference}, version: {version} and standard id type: {idType}");
+            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard for Id: {standardReference}, version: {version} and standard id type: {StandardId.StandardIdType.LarsCode}");
 
             Assert.AreEqual(null, result);
         }
-        
+
+        [Test, AutoData]
+        public async Task When_GettingStandardVersionById_And_TheStandardIdTypeIsIFateReferenceNumber_And_StandardIsNotFound_Then_LogInfo_And_ReturnNull(string version)
+        {
+            string standardReference = "IFATE1";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardVersionByIFateReferenceNumber(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync((Standard)null);
+
+            var result = await _standardService.GetStandardVersionById(standardReference, version);
+
+            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard for Id: {standardReference}, version: {version} and standard id type: {StandardId.StandardIdType.IFateReferenceNumber}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test, AutoData]
+        public async Task When_GettingStandardVersionById_And_TheStandardIdTypeIsStandardUId_And_StandardIsNotFound_Then_LogInfo_And_ReturnNull(string version)
+        {
+            string standardReference = "TEST";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardVersionByStandardUId(It.IsAny<string>()))
+                .ReturnsAsync((Standard)null);
+
+            var result = await _standardService.GetStandardVersionById(standardReference, version);
+
+            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard for Id: {standardReference}, version: {version} and standard id type: {StandardId.StandardIdType.StandardUId}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test, AutoData]
+        public async Task When_GettingStandardVersionById_And_TheStandardIdTypeUnknown_And_StandardIsNotFound_Then_LogInfo_And_ReturnNull(string version)
+        {
+            string standardReference = null;
+
+            var result = await _standardService.GetStandardVersionById(standardReference, version);
+
+            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard for Id: {standardReference}, version: {version} and standard id type: {StandardId.StandardIdType.Unknown}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test, AutoData]
+        public async Task When_GettingStandardVersionById_And_StandardIdTypeIsLarsCode_ExceptionThrown_Then_LogError_And_ReturnNull(string version)
+        {
+            string standardReference = "1";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardVersionByLarsCode(It.IsAny<int>(), It.IsAny<string>()))
+                .Throws(new Exception());
+
+            var result = await _standardService.GetStandardVersionById(standardReference, version);
+
+            VerifyLogger(LogLevel.Error, new EventId(0), $"STANDARD VERSION: Failed to get for standard id: {standardReference}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test, AutoData]
+        public async Task When_GettingStandardVersionById_And_StandardIdTypeIsIFateReferenceNumber_ExceptionThrown_Then_LogError_And_ReturnNull(string version)
+        {
+            string standardReference = "IFATE1";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardVersionByIFateReferenceNumber(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws(new Exception());
+
+            var result = await _standardService.GetStandardVersionById(standardReference, version);
+
+            VerifyLogger(LogLevel.Error, new EventId(0), $"STANDARD VERSION: Failed to get for standard id: {standardReference}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test, AutoData]
+        public async Task When_GettingStandardVersionById_And_StandardIdTypeIsStandardUId_ExceptionThrown_Then_LogError_And_ReturnNull(string version)
+        {
+            string standardReference = "TEST";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardVersionByStandardUId(It.IsAny<string>()))
+                .Throws(new Exception());
+
+            var result = await _standardService.GetStandardVersionById(standardReference, version);
+
+            VerifyLogger(LogLevel.Error, new EventId(0), $"STANDARD VERSION: Failed to get for standard id: {standardReference}");
+
+            Assert.AreEqual(null, result);
+        }
+
         [Test, AutoData]
         public async Task When_GettingStandardVersionsByLarsCode_And_StandardIsNotFound_Then_LogInfo_And_ReturnNull(int standardReference)
         {
@@ -311,7 +409,20 @@ namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Services
         }
 
         [Test, AutoData]
-        public async Task When_GettingGetStandardVersionByStandardUId_And_Standard_Then_LogInfo_And_ReturnNull(string standardReference)
+        public async Task When_GettingStandardVersionsByLarsCode_And_ThrowsException_Then_LogError_And_ReturnNull(int standardReference)
+        {
+            _mockStandardRepository.Setup(repository => repository.GetStandardVersionsByLarsCode(It.IsAny<int>()))
+                .Throws(new Exception());
+
+            var result = await _standardService.GetStandardVersionsByLarsCode(standardReference);
+
+            VerifyLogger(LogLevel.Error, new EventId(0), $"STANDARD: Failed to get for LARS code: {standardReference}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test, AutoData]
+        public async Task When_GettingGetStandardVersionByStandardUId_And_StandardIsNotFound_Then_LogInfo_And_ReturnNull(string standardReference)
         {
             _mockStandardRepository.Setup(repository => repository.GetStandardVersionByStandardUId(It.IsAny<string>()))
                 .ReturnsAsync((Standard)null);
@@ -323,32 +434,165 @@ namespace SFA.DAS.AssessorService.Application.Api.UnitTests.Services
             Assert.AreEqual(null, result);
         }
 
-        [TestCase("1", StandardId.StandardIdType.LarsCode)]
-        [TestCase("IFATE1", StandardId.StandardIdType.IFateReferenceNumber)]
-        [TestCase("TEST", StandardId.StandardIdType.StandardUId)]
-        public async Task When_GettingStandardOptionsByStandardId_And_StandardOptionsIsNotFound_Then_LogInfo_And_ReturnNull(string standardReference, StandardId.StandardIdType idType)
+        [Test, AutoData]
+        public async Task When_GettingGetStandardVersionByStandardUId_And_ThrowsException_Then_LogError_And_ReturnNull(string standardReference)
         {
+            _mockStandardRepository.Setup(repository => repository.GetStandardVersionByStandardUId(It.IsAny<string>()))
+                .Throws(new Exception());
+
+            var result = await _standardService.GetStandardVersionByStandardUId(standardReference);
+
+            VerifyLogger(LogLevel.Error, new EventId(0), $"STANDARD: Failed to get for standard id: {standardReference}");
+
+            Assert.AreEqual(null, result);
+        }
+        
+        [Test]
+        public async Task When_GettingStandardOptionsByStandardId_And_StandardIdTypeIsLarsCode_StandardOptionsIsNotFound_Then_LogInfo_And_ReturnNull()
+        {
+            string standardReference = "1";
+
             _mockStandardRepository.Setup(repository => repository.GetStandardOptionsByLarsCode(It.IsAny<int>()))
                 .ReturnsAsync((StandardOptions)null);
 
             var result = await _standardService.GetStandardOptionsByStandardId(standardReference);
 
-            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard options for id: {standardReference} and standard id type: {idType}");
+            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard options for id: {standardReference} and standard id type: {StandardId.StandardIdType.LarsCode}");
 
             Assert.AreEqual(null, result);
-        }        
+        }
 
         [Test]
-        [InlineAutoData("IFATE1", StandardId.StandardIdType.IFateReferenceNumber)]
-        [InlineAutoData("1", StandardId.StandardIdType.LarsCode)]
-        public async Task When_GettingStandardOptionsByStandardReferenceAndVersion_And_StandardWithReferenceAndVersionIsNotFound_Then_LogInfo_And_ReturnNull(string standardReference, StandardId.StandardIdType idType, string version)
+        public async Task When_GettingStandardOptionsByStandardId_And_StandardIdTypeIsIFateReferenceNumber_StandardOptionsIsNotFound_Then_LogInfo_And_ReturnNull()
         {
+            string standardReference = "IFATE1";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardOptionsByIFateReferenceNumber(It.IsAny<string>()))
+                .ReturnsAsync((StandardOptions)null);
+
+            var result = await _standardService.GetStandardOptionsByStandardId(standardReference);
+
+            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard options for id: {standardReference} and standard id type: {StandardId.StandardIdType.IFateReferenceNumber}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public async Task When_GettingStandardOptionsByStandardId_And_StandardIdTypeIsStandardUId_StandardOptionsIsNotFound_Then_LogInfo_And_ReturnNull()
+        {
+            string standardReference = "TEST";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardOptionsByStandardUId(It.IsAny<string>()))
+                .ReturnsAsync((StandardOptions)null);
+
+            var result = await _standardService.GetStandardOptionsByStandardId(standardReference);
+
+            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard options for id: {standardReference} and standard id type: {StandardId.StandardIdType.StandardUId}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public async Task When_GettingStandardOptionsByStandardId_And_StandardIdTypeIsLarsCode_ThrowsException_Then_LogError_And_ReturnNull()
+        {
+            string standardReference = "1";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardOptionsByLarsCode(It.IsAny<int>()))
+                .Throws(new Exception());
+
+            var result = await _standardService.GetStandardOptionsByStandardId(standardReference);
+
+            VerifyLogger(LogLevel.Error, new EventId(0), $"STANDARD OPTIONS: Failed to get standard options for id {standardReference}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public async Task When_GettingStandardOptionsByStandardId_And_StandardIdTypeIsIFateReferenceNumber_ThrowsException_Then_LogError_And_ReturnNull()
+        {
+            string standardReference = "IFATE1";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardOptionsByIFateReferenceNumber(It.IsAny<string>()))
+                .Throws(new Exception());
+
+            var result = await _standardService.GetStandardOptionsByStandardId(standardReference);
+
+            VerifyLogger(LogLevel.Error, new EventId(0), $"STANDARD OPTIONS: Failed to get standard options for id {standardReference}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public async Task When_GettingStandardOptionsByStandardId_And_StandardIdTypeIsStandardUId_ThrowsException_Then_LogError_And_ReturnNull()
+        {
+            string standardReference = "TEST";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardOptionsByStandardUId(It.IsAny<string>()))
+                .Throws(new Exception());
+
+            var result = await _standardService.GetStandardOptionsByStandardId(standardReference);
+
+            VerifyLogger(LogLevel.Error, new EventId(0), $"STANDARD OPTIONS: Failed to get standard options for id {standardReference}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test, AutoData]        
+        public async Task When_GettingStandardOptionsByStandardReferenceAndVersion_And_StandardIdTypeIsIFateReferenceNumber_And_StandardWithReferenceAndVersionIsNotFound_Then_LogInfo_And_ReturnNull(string version)
+        {
+            string standardReference = "IFATE1";
+
             _mockStandardRepository.Setup(repository => repository.GetStandardVersionByIFateReferenceNumber(It.IsAny<string>(), It.IsAny<string>()))
                 .ReturnsAsync((Standard)null);
 
             var result = await _standardService.GetStandardOptionsByStandardIdAndVersion(standardReference, version);
 
-            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard options for id: {standardReference}, version: {version} and standard id type: {idType}");
+            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard options for id: {standardReference}, version: {version} and standard id type: {StandardId.StandardIdType.IFateReferenceNumber}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test, AutoData]
+        public async Task When_GettingStandardOptionsByStandardReferenceAndVersion_And_StandardIdTypeIsLarsCode_And_StandardWithReferenceAndVersionIsNotFound_Then_LogInfo_And_ReturnNull(string version)
+        {
+            string standardReference = "1";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardVersionByLarsCode(It.IsAny<int>(), It.IsAny<string>()))
+                .ReturnsAsync((Standard)null);
+
+            var result = await _standardService.GetStandardOptionsByStandardIdAndVersion(standardReference, version);
+
+            VerifyLogger(LogLevel.Information, new EventId(0), $"Unable to find standard options for id: {standardReference}, version: {version} and standard id type: {StandardId.StandardIdType.LarsCode}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test, AutoData]
+        public async Task When_GettingStandardOptionsByStandardReferenceAndVersion_And_StandardIdTypeIsIFateReferenceNumber_And_ThrowsException_Then_LogError_And_ReturnNull(string version)
+        {
+            string standardReference = "IFATE1";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardVersionByIFateReferenceNumber(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws(new Exception());
+
+            var result = await _standardService.GetStandardOptionsByStandardIdAndVersion(standardReference, version);
+
+            VerifyLogger(LogLevel.Error, new EventId(0), $"Could not find standard with id: {standardReference} and Version: {version}");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test, AutoData]
+        public async Task When_GettingStandardOptionsByStandardReferenceAndVersion_And_StandardIdTypeIsLarsCode_And_ThrowsException_Then_LogError_And_ReturnNull(string version)
+        {
+            string standardReference = "1";
+
+            _mockStandardRepository.Setup(repository => repository.GetStandardVersionByLarsCode(It.IsAny<int>(), It.IsAny<string>()))
+                .Throws(new Exception());
+
+            var result = await _standardService.GetStandardOptionsByStandardIdAndVersion(standardReference, version);
+
+            VerifyLogger(LogLevel.Error, new EventId(0), $"Could not find standard with id: {standardReference} and Version: {version}");
 
             Assert.AreEqual(null, result);
         }
