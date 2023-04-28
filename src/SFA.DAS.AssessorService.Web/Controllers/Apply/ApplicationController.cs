d7c0cd9e7e4dee58c3cb98d78e6b490bb440aec2
Sarah Duncan <sarah.coetzee@education.gov.uk>
2023-04-28 14:59:49 +0100

    QF-1329 add null check for 'updatePageResult'

--- src/SFA.DAS.AssessorService.Web/Controllers/Apply/ApplicationController.cs
+++ src/SFA.DAS.AssessorService.Web/Controllers/Apply/ApplicationController.cs
@@ -662,7 +662,7 @@ namespace SFA.DAS.AssessorService.Web.Controllers.Apply
                         return RedirectToAction("Feedback", new { Id });
                     }
 
-                    if (!string.IsNullOrEmpty(updatePageResult.NextAction))
+                    if (updatePageResult != null && !string.IsNullOrEmpty(updatePageResult.NextAction))
                         return RedirectToNextAction(Id, sequenceNo, sectionNo, updatePageResult.NextAction, updatePageResult.NextActionId, __redirectAction);
                 }
 
