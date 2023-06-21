a043901145327fb38449ba44f6a0a748fda67b88
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-06-21 16:35:33 +0100

    QF-602 add given names and family name controllers

---
+++ src/SFA.DAS.AssessorService.Web/Controllers/CertificateFamilyNameController.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AssessorService.Application.Api.Client.Clients;
+using SFA.DAS.AssessorService.Domain.Consts;
+using SFA.DAS.AssessorService.Web.Infrastructure;
+using SFA.DAS.AssessorService.Web.ViewModels.Certificate;
+using System.Threading.Tasks;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace SFA.DAS.AssessorService.Web.Controllers
+{
+    [Route("certificate/familyname")]
+    public class CertificateFamilyNameController : CertificateBaseController
+    {
+        private readonly IValidator<CertificateNamesViewModel> _certificateNameChangeValidator;
+        public CertificateFamilyNameController(ILogger<CertificateController> logger, IHttpContextAccessor contextAccessor, ICertificateApiClient certificateApiClient, ISessionService sessionService, IValidator<CertificateNamesViewModel> certificateNameChangeValidator) : base(logger, contextAccessor, certificateApiClient, sessionService)
+        {
+            _certificateNameChangeValidator = certificateNameChangeValidator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FamilyName(bool? redirectToCheck = true)
+        {
+            return await LoadViewModel<CertificateNamesViewModel>("~/Views/Certificate/FamilyName.cshtml");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FamilyName(CertificateNamesViewModel viewModel)
+        {
+            var validationResult = _certificateNameChangeValidator.Validate(viewModel);
+
+            if (!validationResult.IsValid)
+            {
+                foreach (var error in validationResult.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+                return View("/Views/Certificate/FamilyName.cshtml", viewModel);
+            }
+
+            return await SaveViewModel(viewModel,
+                returnToIfModelNotValid: "~/Views/Certificate/FamilyName.cshtml",
+                nextAction: RedirectToAction("Check", "CertificateCheck"), action: CertificateActions.FirstName);
+        }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Web/Controllers/CertificateGivenNamesController.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AssessorService.Application.Api.Client.Clients;
+using SFA.DAS.AssessorService.Domain.Consts;
+using SFA.DAS.AssessorService.Web.Infrastructure;
+using SFA.DAS.AssessorService.Web.ViewModels.Certificate;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.AssessorService.Web.Controllers
+{
+    [Route("certificate/givennames")]
+    public class CertificateGivenNamesController : CertificateBaseController
+    {
+        private readonly IValidator<CertificateNamesViewModel> _certificateNameChangeValidator;
+
+        public CertificateGivenNamesController(ILogger<CertificateController> logger, IHttpContextAccessor contextAccessor, ICertificateApiClient certificateApiClient, ISessionService sessionService, IValidator<CertificateNamesViewModel> certificateNameChangeValidator) : base(logger, contextAccessor, certificateApiClient, sessionService)
+        {
+            _certificateNameChangeValidator = certificateNameChangeValidator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GivenNames(bool? redirectToCheck = true)
+        {
+            return await LoadViewModel<CertificateNamesViewModel>("~/Views/Certificate/GivenNames.cshtml");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GivenNames(CertificateNamesViewModel viewModel)
+        {
+            var validationResult = _certificateNameChangeValidator.Validate(viewModel);
+
+            if (!validationResult.IsValid)
+            {
+                foreach (var error in validationResult.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+                return View("/Views/Certificate/GivenNames.cshtml", viewModel);
+            }
+
+            return await SaveViewModel(viewModel,
+                returnToIfModelNotValid: "~/Views/Certificate/GivenNames.cshtml",
+                nextAction: RedirectToAction("Check", "CertificateCheck"), action: CertificateActions.FirstName);
+        }
+    }
+}
