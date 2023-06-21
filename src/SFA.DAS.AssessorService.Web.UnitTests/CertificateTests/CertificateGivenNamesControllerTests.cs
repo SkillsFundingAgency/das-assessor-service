3e69aa3ff8c950ae9e3d2778b859b6a4fb3f75a0
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-06-21 16:36:48 +0100

    QF-602 update the unit tests for the given names controller and family name controller

--- src/SFA.DAS.AssessorService.Web.UnitTests/CertificateTests/CertificateGivenNamesControllerTests.cs
+++ src/SFA.DAS.AssessorService.Web.UnitTests/CertificateTests/CertificateGivenNamesControllerTests.cs
@@ -8,7 +8,6 @@ using NUnit.Framework;
 using SFA.DAS.AssessorService.Application.Api.Client.Clients;
 using SFA.DAS.AssessorService.Web.Controllers;
 using SFA.DAS.AssessorService.Web.Infrastructure;
-using SFA.DAS.AssessorService.Web.Validators;
 using SFA.DAS.AssessorService.Web.ViewModels.Certificate;
 using SFA.DAS.Testing.AutoFixture;
 using System.Security.Claims;
@@ -16,6 +15,9 @@ using System;
 using System.Threading.Tasks;
 using SFA.DAS.AssessorService.Domain.Entities;
 using SFA.DAS.AssessorService.Domain.JsonData;
+using FluentValidation;
+using FluentValidation.Results;
+using System.Collections.Generic;
 
 namespace SFA.DAS.AssessorService.Web.UnitTests.CertificateTests
 {
@@ -26,7 +28,7 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.CertificateTests
         private Mock<IHttpContextAccessor> _mockContextAccessor;
         private Mock<ISessionService> _mockSessionService;
         private Mock<ICertificateApiClient> _mockCertificateApiClient;
-        private Mock<CertificateNameViewModelValidator> _validator;
+        private Mock<IValidator<CertificateNamesViewModel>> _validator;
 
         private const int Ukprn = 123456;
         private const string Username = "TestProviderUsername";
@@ -40,7 +42,7 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.CertificateTests
             _mockContextAccessor = new Mock<IHttpContextAccessor>();
             _mockSessionService = new Mock<ISessionService>();
             _mockCertificateApiClient = new Mock<ICertificateApiClient>();
-            _validator = new Mock<CertificateNameViewModelValidator>();
+            _validator = new Mock<IValidator<CertificateNamesViewModel>>();
 
             _mockContextAccessor.Setup(s => s.HttpContext.User.FindFirst("http://schemas.portal.com/ukprn")).Returns(new Claim("", Ukprn.ToString()));
             _mockContextAccessor.Setup(s => s.HttpContext.User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn")).Returns(new Claim("", Username));
@@ -82,6 +84,8 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.CertificateTests
         [Test, MoqAutoData]
         public async Task WhenPostingGivenNamesView_AndInputIsValid_ThenRedirectsToCheckPage(CertificateNamesViewModel mockViewModel)
         {
+            _validator.Setup(s => s.Validate(It.IsAny<CertificateNamesViewModel>())).Returns(new ValidationResult());
+
             var result = await _controller.GivenNames(mockViewModel) as RedirectToActionResult;
 
             result.ControllerName.Should().Be("CertificateCheck");
@@ -91,9 +95,13 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.CertificateTests
         [Test, MoqAutoData]
         public async Task WhenPostingGivenNamesView_AndInputIsNotValid_ThenRefreshesWithErrors(CertificateNamesViewModel mockViewModel)
         {
+            _validator.Setup(s => s.Validate(It.IsAny<CertificateNamesViewModel>())).Returns(new ValidationResult(new List<ValidationFailure> {
+                new ValidationFailure("Error", "Error message")
+            }));
+
             var result = await _controller.GivenNames(mockViewModel) as ViewResult;
 
-            result.ViewName.Should().Be("~/Views/Certificate/GivenNames.cshtml");
+            result.ViewName.Should().Be("/Views/Certificate/GivenNames.cshtml");
             result.ViewData.ModelState.ErrorCount.Should().BeGreaterThanOrEqualTo(1);
         }
     }
