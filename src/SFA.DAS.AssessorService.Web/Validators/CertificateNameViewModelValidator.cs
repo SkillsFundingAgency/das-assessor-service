ae9bd90d21bbbe8f0a31f2c6e433a542b8c14d56
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-06-21 16:35:02 +0100

    QF-602 Add viewmodel for name controllers and add validator

---
+++ src/SFA.DAS.AssessorService.Web/Validators/CertificateNameViewModelValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using SFA.DAS.AssessorService.Web.ViewModels.Certificate;
+
+namespace SFA.DAS.AssessorService.Web.Validators
+{
+    public class CertificateNameViewModelValidator : AbstractValidator<CertificateNamesViewModel>
+    {
+        public CertificateNameViewModelValidator()
+        {
+            RuleFor(vm => vm.GivenNames).NotEmpty().WithMessage("First names must not be empty").WithName("GivenNames").DependentRules(() =>
+            {
+                RuleFor(vm => vm.GivenNames.ToLower()).NotEmpty().Equal(vm => vm.PreviousGivenNames.ToLower()).WithMessage("You can only change the casing of the first names").WithName("GivenNames");
+            });
+
+            RuleFor(vm => vm.FamilyName).NotEmpty().WithMessage("Last name must not be empty").WithName("FamilyName").DependentRules(() =>
+            {
+                RuleFor(vm => vm.FamilyName.ToLower()).NotEmpty().Equal(vm => vm.PreviousFamilyName.ToLower()).WithMessage("You can only change the casing of the last name").WithName("FamilyName");
+            });
+        }
+    }
+}
