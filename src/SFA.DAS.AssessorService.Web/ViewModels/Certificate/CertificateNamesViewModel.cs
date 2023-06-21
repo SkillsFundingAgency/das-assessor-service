f4ac8b7946a75d8ea898ee620ebef96b14eb0fda
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-06-21 16:35:02 +0100

    QF-602 Add viewmodel for name controllers and add validator

---
+++ src/SFA.DAS.AssessorService.Web/ViewModels/Certificate/CertificateNamesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using SFA.DAS.AssessorService.Domain.JsonData;
+
+namespace SFA.DAS.AssessorService.Web.ViewModels.Certificate
+{
+    public class CertificateNamesViewModel : CertificateBaseViewModel
+    {
+        public string PreviousGivenNames { get; set; }
+        public string PreviousFamilyName { get; set; }
+
+        public override Domain.Entities.Certificate GetCertificateFromViewModel(Domain.Entities.Certificate certificate, CertificateData certData)
+        {
+            certData.LearnerGivenNames = GivenNames;
+            certData.LearnerFamilyName = FamilyName;
+            certData.FullName = GivenNames + FamilyName;
+
+            certificate.CertificateData = JsonConvert.SerializeObject(certData);
+
+            return certificate;
+        }
+    }
+}
