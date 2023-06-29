0e29380e2928b7edd8407767f5f50ec97997e34a
Saw Naw <waterchan@gmail.com>
2023-06-29 17:40:23 +0100

    QF-1519 Disable withdrawal and re-enable new standard application

---
+++ src/SFA.DAS.AssessorService.Web.UnitTests/ViewModelTests/ApplicationResponseViewModelTests/WhenCallingApplicationPostAction.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SFA.DAS.AssessorService.Web.Controllers.Apply;
+using SFA.DAS.AssessorService.Web.ViewModels.Apply;
+
+namespace SFA.DAS.AssessorService.Web.UnitTests.ViewModelTests.ApplicationResponseViewModelTests
+{
+    public class WhenCallingApplicationPostAction
+    {
+        [TestCase(true, nameof(ApplicationController.FinancialExpiredRedir))]
+        [TestCase(false, nameof(ApplicationController.StartApplication))]
+        public void ActionNameIsReturned_BasedOn_FinancialInfoStage1Expired(bool isExpired, string actionMethodName)
+        {
+            var sut = new ApplicationResponseViewModel() { FinancialInfoStage1Expired = isExpired };
+            Assert.That(sut.ApplicationPostAction(), Is.EqualTo(actionMethodName));
+        }
+    }
+}
--- src/SFA.DAS.AssessorService.Web/ViewModels/Apply/ApplicationResponseViewModel.cs
+++ src/SFA.DAS.AssessorService.Web/ViewModels/Apply/ApplicationResponseViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using SFA.DAS.AssessorService.Api.Types.Models.Apply;
+using SFA.DAS.AssessorService.Web.Controllers.Apply;
 using System.Collections.Generic;
 
 namespace SFA.DAS.AssessorService.Web.ViewModels.Apply
@@ -8,5 +9,12 @@ namespace SFA.DAS.AssessorService.Web.ViewModels.Apply
         public List<ApplicationResponse> ApplicationResponse { get; set; }
         public bool FinancialInfoStage1Expired { get; set; }
         public string FinancialAssessmentUrl { get; set; }
+
+        public string ApplicationPostAction()
+        {
+            return FinancialInfoStage1Expired
+                ? nameof(ApplicationController.FinancialExpiredRedir)
+                : nameof(ApplicationController.StartApplication);
+        }
     }
 }
