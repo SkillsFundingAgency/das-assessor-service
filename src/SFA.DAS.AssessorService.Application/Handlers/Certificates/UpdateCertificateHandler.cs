5e939d976f2e254250fe528d595777105b425bf6
SawNawDfE <saw.naw@education.gov.uk>
2023-08-24 16:01:50 +0100

    QF-1610 Add tests for HandleSendToUpdate

--- src/SFA.DAS.AssessorService.Application/Handlers/Certificates/UpdateCertificateHandler.cs
+++ src/SFA.DAS.AssessorService.Application/Handlers/Certificates/UpdateCertificateHandler.cs
@@ -54,7 +54,7 @@ namespace SFA.DAS.AssessorService.Application.Handlers.Certificates
                 _logger.LogInformation($"Certificate with ID: {request.Certificate.Id} Updated with reference of {request.Certificate.CertificateReference}");
             }
 
-            updatedData = HandleSendToUpdate(currentCertificate, currentData, updatedData);
+            updatedData = CertificateDataCleanser.HandleSendToUpdate(currentCertificate, currentData, updatedData);
 
             request.Certificate.CertificateData = JsonConvert.SerializeObject(updatedData);
 
@@ -106,56 +106,10 @@ namespace SFA.DAS.AssessorService.Application.Handlers.Certificates
 
             if (latestLogEntry != null && latestLogEntry.Action == request.Action && latestLogEntry.CertificateData == request.Certificate.CertificateData && string.IsNullOrWhiteSpace(request.ReasonForChange))
             {
-                return await _certificateRepository.Update(request.Certificate, request.Username, request.Action, updateLog:false);
+                return await _certificateRepository.Update(request.Certificate, request.Username, request.Action, updateLog: false);
             }
 
             return await _certificateRepository.Update(request.Certificate, request.Username, request.Action, updateLog: true, reasonForChange: request.ReasonForChange);
-        }
-
-        private CertificateData HandleSendToUpdate(Certificate currentCertificate, CertificateData currentData, CertificateData updatedData)
-        {
-            if (ShouldClearAddressFields(currentCertificate, currentData, updatedData))
-            {
-                updatedData = ClearAddressFields(updatedData);
-            }
-
-            if (updatedData.SendTo == CertificateSendTo.Apprentice)
-            {
-                // when sending to the apprentice use the apprentice name for the contact
-                updatedData.ContactName = updatedData.FullName;
-            }
-
-            if (updatedData.ContactName != null) { updatedData.ContactName = updatedData.ContactName.ToUpper(); }
-
-            return updatedData;
-        }
-
-        private bool ShouldClearAddressFields(Certificate currentCertificate, CertificateData currentCertificateData, CertificateData updatedCertificateData)
-        {
-            var shouldClearAddressFields = (updatedCertificateData.SendTo != currentCertificateData.SendTo);
-
-            // a certificate which has been created by the API will not have a SendTo but does have valid employer details
-            // so retain these employer details when the new SendTo is for an employer
-            if (currentCertificate.CreatedBy == "API" && currentCertificateData.SendTo == CertificateSendTo.None && updatedCertificateData.SendTo == CertificateSendTo.Employer)
-            {
-                shouldClearAddressFields = false;
-            }
-
-            return shouldClearAddressFields;
-        }
-
-        private CertificateData ClearAddressFields(CertificateData certificateData)
-        {
-            certificateData.ContactName = null;
-            certificateData.Department = null;
-            certificateData.ContactOrganisation = null;
-            certificateData.ContactAddLine1 = null;
-            certificateData.ContactAddLine2 = null;
-            certificateData.ContactAddLine3 = null;
-            certificateData.ContactAddLine4 = null;
-            certificateData.ContactPostCode = null;
-
-            return certificateData;
         }
     }
 }
