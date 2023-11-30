c4cf1d72248fbe8f04423e3537a2b17162ffa4e5
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2023-11-30 11:09:41 +0000

    FAI-1012 - Apar banner deleted

--- src/SFA.DAS.AssessorService.Web/Constants/Banners.cs
+++ src/SFA.DAS.AssessorService.Web/Constants/Banners.cs
@@ -2,6 +2,5 @@
 
 public static class Banners
 {
-    public const string APARNotifyBannerViewPath = "~/Views/Banners/_APARNotifyBannerPartial.cshtml";
     public const string GovNotifyBannerViewPath = "~/Views/Banners/_GovNotifyBannerPartial.cshtml";
 }
