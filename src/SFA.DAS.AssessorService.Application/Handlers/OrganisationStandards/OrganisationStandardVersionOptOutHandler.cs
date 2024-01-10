adf928860c693d609620e7d68f5c4a29225cf0fa
Chris Woodcock <chris@humesoftware.com>
2024-01-10 13:51:59 +0000

    Store a value for standard version date version approved but retain existing value if any exists

--- src/SFA.DAS.AssessorService.Application/Handlers/OrganisationStandards/OrganisationStandardVersionOptOutHandler.cs
+++ src/SFA.DAS.AssessorService.Application/Handlers/OrganisationStandards/OrganisationStandardVersionOptOutHandler.cs
@@ -59,7 +59,7 @@ namespace SFA.DAS.AssessorService.Application.Handlers.Apply
                     OrganisationStandardId = organisationStandard.Id,
                     EffectiveFrom = request.EffectiveFrom,
                     EffectiveTo = request.EffectiveTo,
-                    DateVersionApproved = null,
+                    DateVersionApproved = DateTime.UtcNow,
                     Comments = string.IsNullOrEmpty(existingVersion?.Comments)
                         ? newComment
                         : existingVersion.Comments + ";" + newComment,
