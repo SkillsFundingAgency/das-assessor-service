bdfd6d5faf0787356fc439e1c8bce6d94e9ddab0
Chris Woodcock <chris@humesoftware.com>
2024-01-12 14:39:28 +0000

    Fixed merge issue

--- src/SFA.DAS.AssessorService.Web.UnitTests/Helpers/ApplicationFinderTests.cs
+++ src/SFA.DAS.AssessorService.Web.UnitTests/Helpers/ApplicationFinderTests.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Moq;
+﻿using Moq;
 using NUnit.Framework;
 using SFA.DAS.AssessorService.Api.Types.Models.Apply;
 using SFA.DAS.AssessorService.Application.Api.Client.Clients;
 using SFA.DAS.AssessorService.ApplyTypes;
 using SFA.DAS.AssessorService.Domain.Consts;
+using SFA.DAS.AssessorService.Domain.Entities;
 using SFA.DAS.AssessorService.Web.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.AssessorService.Web.UnitTests.Helpers
 {
@@ -51,21 +51,18 @@ namespace SFA.DAS.AssessorService.Web.UnitTests.Helpers
                 new ApplicationResponse() { ApplyData = GetApplyDataWithNullVersion(), StandardReference = "ST9999", ApplicationStatus = applicationStatus },
                 new ApplicationResponse() { ApplyData = GetApplyDataWithNullVersion(), StandardReference = "ST0003", ApplicationStatus = ApplicationStatus.Deleted },
                 new ApplicationResponse() { ApplyData = GetApplyDataWithNullVersion(), StandardReference = "ST0004", ApplicationStatus = ApplicationStatus.New }
-            };                             
+            };
             result.ForEach(r => r.ApplyData = GetApplyDataWithNullVersion());
             result.ForEach(r => r.ApplicationType = ApplicationTypes.StandardWithdrawal);
 
             return result;
-        }                                   
-        
+        }
+
         private ApplyData GetApplyDataWithNullVersion()
         {
             var data = new ApplyData();
-            data.Apply = new Apply() { Versions = null };
+            data.Apply = new ApplyInfo() { Versions = null };
             return data;
         }
-    }                                                          
-}                                                              
-                                                               
-                                                               
-                                                               
+    }
+}
