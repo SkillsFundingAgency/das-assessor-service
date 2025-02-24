55829617e9a59cb3da5b7d234320ff7f822805bc
Chris Woodcock <chris@humesoftware.com>
2025-02-24 10:54:28 +0000

    Create frameworks maig certificates table

---
+++ src/SFA.DAS.AssessorService.Database/Tables/frameworks/maig_certificates.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [frameworks].[maig_certificates]
+(
+  [cert_id] bigint NOT NULL,
+  [cert_description] nvarchar(400),
+  [cert_details] nvarchar(400),
+  [cert_file] nvarchar(200),
+  [cert_code] nvarchar(max),
+  PRIMARY KEY ([cert_id])
+)
+GO
