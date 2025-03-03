2613c7b74293bfa0397fcb197099b3052f3004ee
Karen Hanna <karen@laveryhanna.com>
2025-03-03 12:49:11 +0000

    api changes to get framework certificate by id

--- src/SFA.DAS.AssessorService.Application.Api.Client/Clients/FrameworkSearchApiClient.cs
+++ src/SFA.DAS.AssessorService.Application.Api.Client/Clients/FrameworkSearchApiClient.cs
@@ -2,6 +2,8 @@
 using SFA.DAS.AssessorService.Api.Common;
 using SFA.DAS.AssessorService.Api.Types.Models;
 using SFA.DAS.AssessorService.Api.Types.Models.FrameworkSearch;
+using SFA.DAS.AssessorService.Api.Types.Models.Staff;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -20,6 +22,15 @@ namespace SFA.DAS.AssessorService.Application.Api.Client.Clients
             using (var request = new HttpRequestMessage(HttpMethod.Post, "/api/v1/search/frameworks"))
             {
                 return await PostPutRequestWithResponseAsync<FrameworkSearchQuery, List<FrameworkSearchResult>>(request, frameworkSearchQuery);
+            }
+        }
+
+        public async Task<GetFrameworkCertificateResult> GetFrameworkCertificate(Guid id)
+        { 
+            using (var request = new HttpRequestMessage(HttpMethod.Get, $"/api/v1/search/framework-certificate/{id}"))
+            {
+                return await RequestAndDeserialiseAsync<GetFrameworkCertificateResult>(request,
+                    $"Could not find the framework certificate");
             }
         }
     }
---
+++ src/SFA.DAS.AssessorService.Application/Mapping/AutoMapperProfiles/FrameworkLearnerProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SFA.DAS.AssessorService.Api.Types.Models.FrameworkSearch;
+using SFA.DAS.AssessorService.Application.Mapping.CustomResolvers;
+using SFA.DAS.AssessorService.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SFA.DAS.AssessorService.Application.Mapping.AutoMapperProfiles
+{
+    public class FrameworkLearnerProfile : Profile
+    {
+        public FrameworkLearnerProfile()
+        {
+            CreateMap<FrameworkLearner, FrameworkSearchResult>();
+
+            CreateMap<FrameworkLearner, GetFrameworkCertificateResult>()
+                .ForMember(dest => dest.CertificateNumber, opt => opt.MapFrom(src => src.ApprenticeReference))
+                .ForMember(dest => dest.QualificationsAndAwardingBodies, opts => opts.MapFrom<QualificationDetailsResolver>());
+        }
+    }
+}
