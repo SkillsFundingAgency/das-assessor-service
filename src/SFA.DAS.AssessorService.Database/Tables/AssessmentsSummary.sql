78d592ba6833ea4e3221ebb9160de6dbc30f0b8c
Alan Burns <adburns57@gmail.com>
2025-05-01 09:47:49 +0100

    Table and SP for AssessmentsSummary

---
+++ src/SFA.DAS.AssessorService.Database/Tables/AssessmentsSummary.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[AssessmentsSummary]
+([Ukprn] bigint NOT NULL,
+ [IfateReferenceNumber] nvarchar(10) NOT NULL,
+ [EarliestAssessment] datetime2  NOT NULL, 
+ [EndpointAssessmentCount] int NOT NULL, 
+ [UpdatedAt] datetime2);
+ GO
+ 
+ CREATE UNIQUE INDEX IXU_AssessmentsSummary ON [dbo].[AssessmentsSummary]
+ ([Ukprn],[IfateReferenceNumber])
+ INCLUDE ([EarliestAssessment], [EndpointAssessmentCount]);
+ GO
+ 
+ CREATE INDEX IX_AssessmentsSummary_Ukprn ON [dbo].[AssessmentsSummary]
+ ([Ukprn])
+ INCLUDE ([EarliestAssessment], [EndpointAssessmentCount]);
+ GO
+ 
