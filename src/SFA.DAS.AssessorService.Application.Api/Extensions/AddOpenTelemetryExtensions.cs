7c6d9e0ebee59ff3bbc2f76e7cf3fc2a92aa5f70
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-05-19 11:32:51 +0100

    P2-1952-Kibana decommission

---
+++ src/SFA.DAS.AssessorService.Application.Api/Extensions/AddOpenTelemetryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Azure.Monitor.OpenTelemetry.AspNetCore;
+
+namespace SFA.DAS.AssessorService.Application.Api.Extensions
+{
+    public static class AddOpenTelemetryExtensions
+    {
+        /// <summary>
+        /// Add the OpenTelemetry telemetry service to the application.
+        /// </summary>
+        /// <param name="services">Service Collection</param>
+        /// <param name="appInsightsConnectionString">Azure app insights connection string.</param>
+        public static void AddOpenTelemetryRegistration(this IServiceCollection services, string appInsightsConnectionString)
+        {
+            if (!string.IsNullOrEmpty(appInsightsConnectionString))
+            {
+                // This service will collect and send telemetry data to Azure Monitor.
+                services.AddOpenTelemetry().UseAzureMonitor(options =>
+                {
+                    options.ConnectionString = appInsightsConnectionString;
+                });
+            }
+        }
+    }
+}
