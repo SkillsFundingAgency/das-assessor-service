433e25248f02d2e53392685c17907a70df6f4332
Chris Woodcock <chris@humesoftware.com>
2026-06-03 17:59:17 +0100

    Code tidy

--- src/SFA.DAS.AssessorService.Application/Handlers/Staff/StartCertificateHandler.cs
+++ src/SFA.DAS.AssessorService.Application/Handlers/Staff/StartCertificateHandler.cs
@@ -6,14 +6,12 @@ using System.Threading.Tasks;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.AssessorService.Api.Types.Models.Certificates;
-using SFA.DAS.AssessorService.Api.Types.Models.Standards;
 using SFA.DAS.AssessorService.Application.Interfaces;
 using SFA.DAS.AssessorService.Data.Interfaces;
 using SFA.DAS.AssessorService.Domain.Consts;
 using SFA.DAS.AssessorService.Domain.Entities;
 using SFA.DAS.AssessorService.Domain.JsonData;
 using CertificateStatus = SFA.DAS.AssessorService.Domain.Consts.CertificateStatus;
-using Learner = SFA.DAS.AssessorService.Domain.Entities.Learner;
 
 namespace SFA.DAS.AssessorService.Application.Handlers.Staff
 {
