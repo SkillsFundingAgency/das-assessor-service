ba72316a6b72d333c72cb5ceccb5b331cc8b2617
Chris Woodcock <chris@humesoftware.com>
2026-08-27 12:46:03 +0100

    Updating DateOfBirth in repository and testing new and updated repository methods

---
+++ src/SFA.DAS.AssessorService.Data.UnitTests/Certificates/WhenSystemCreatesStandardCertificate.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Moq;
+using Moq.EntityFrameworkCore;
+using NUnit.Framework;
+using SFA.DAS.AssessorService.Data.Interfaces;
+using SFA.DAS.AssessorService.Domain.Entities;
+using SFA.DAS.AssessorService.Domain.JsonData;
+
+namespace SFA.DAS.AssessorService.Data.UnitTests.Certificates;
+
+public class WhenSystemCreatesStandardCertificate
+{
+    private Mock<IAssessorDbContext> _mockDbContext;
+    private Mock<IAssessorUnitOfWork> _mockUnitOfWork;
+    private Mock<DbSet<CertificateBase>> _mockCertificateSet;
+    private CertificateRepository _sut;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockDbContext = new Mock<IAssessorDbContext>();
+        _mockUnitOfWork = new Mock<IAssessorUnitOfWork>();
+        _mockCertificateSet = new Mock<DbSet<CertificateBase>>();
+
+        _mockDbContext
+            .Setup(x => x.StandardCertificates)
+            .ReturnsDbSet(new List<Certificate>());
+
+        _mockDbContext
+            .Setup(x => x.Set<CertificateBase>())
+            .Returns(_mockCertificateSet.Object);
+
+        _mockDbContext
+            .Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(1);
+
+        var mockCertificateLogs = new Mock<DbSet<CertificateLog>>();
+
+        mockCertificateLogs
+            .Setup(x => x.Add(It.IsAny<CertificateLog>()))
+            .Returns((EntityEntry<CertificateLog>)null);
+
+        _mockDbContext
+            .Setup(x => x.CertificateLogs)
+            .Returns(mockCertificateLogs.Object);
+
+        _mockCertificateSet
+            .Setup(x => x.AddAsync(
+                It.IsAny<CertificateBase>(),
+                It.IsAny<CancellationToken>()))
+            .Returns(new ValueTask<EntityEntry<CertificateBase>>(
+                (EntityEntry<CertificateBase>)null));
+
+        _mockUnitOfWork
+            .SetupGet(x => x.AssessorDbContext)
+            .Returns(_mockDbContext.Object);
+
+        _mockUnitOfWork
+            .Setup(x => x.ExecuteInTransactionAsync(
+                It.IsAny<Func<Task>>(),
+                It.IsAny<CancellationToken>()))
+            .Returns(
+                (Func<Task> action, CancellationToken cancellationToken) =>
+                    action());
+
+        _sut = new CertificateRepository(_mockUnitOfWork.Object);
+    }
+
+    [Test]
+    public async Task ThenNullDateOfBirthIsPreserved()
+    {
+        // Arrange
+        var certificate = new Certificate
+        {
+            Uln = 1234567890,
+            StandardCode = 123,
+            DateOfBirth = null,
+            CertificateData = new CertificateData
+            {
+                EpaDetails = new EpaDetails()
+            }
+        };
+
+        CertificateBase addedCertificate = null;
+
+        _mockCertificateSet
+            .Setup(x => x.AddAsync(
+                It.IsAny<CertificateBase>(),
+                It.IsAny<CancellationToken>()))
+            .Callback<CertificateBase, CancellationToken>(
+                (entity, cancellationToken) => addedCertificate = entity)
+            .Returns(new ValueTask<EntityEntry<CertificateBase>>(
+                (EntityEntry<CertificateBase>)null));
+
+        // Act
+        await _sut.NewStandardCertificate(certificate);
+
+        // Assert
+        addedCertificate
+            .Should()
+            .BeOfType<Certificate>()
+            .Which.DateOfBirth
+            .Should()
+            .BeNull();
+
+        _mockDbContext.Verify(
+            x => x.SaveChangesAsync(It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    [Test]
+    public async Task ThenPopulatedDateOfBirthIsPreserved()
+    {
+        // Arrange
+        var dateOfBirth = new DateTime(2000, 2, 3, 0, 0, 0, DateTimeKind.Utc);
+
+        var certificate = new Certificate
+        {
+            Uln = 1234567890,
+            StandardCode = 123,
+            DateOfBirth = dateOfBirth,
+            CertificateData = new CertificateData
+            {
+                EpaDetails = new EpaDetails()
+            }
+        };
+
+        CertificateBase addedCertificate = null;
+
+        _mockCertificateSet
+            .Setup(x => x.AddAsync(
+                It.IsAny<CertificateBase>(),
+                It.IsAny<CancellationToken>()))
+            .Callback<CertificateBase, CancellationToken>(
+                (entity, cancellationToken) => addedCertificate = entity)
+            .Returns(new ValueTask<EntityEntry<CertificateBase>>(
+                (EntityEntry<CertificateBase>)null));
+
+        // Act
+        await _sut.NewStandardCertificate(certificate);
+
+        // Assert
+        addedCertificate
+            .Should()
+            .BeOfType<Certificate>()
+            .Which.DateOfBirth
+            .Should()
+            .Be(dateOfBirth);
+    }
+}
---
+++ src/SFA.DAS.AssessorService.Data.UnitTests/Certificates/WhenSystemUpdatesStandardCertificate.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Moq.EntityFrameworkCore;
+using NUnit.Framework;
+using SFA.DAS.AssessorService.Data.Interfaces;
+using SFA.DAS.AssessorService.Domain.Entities;
+using SFA.DAS.AssessorService.Domain.JsonData;
+
+namespace SFA.DAS.AssessorService.Data.UnitTests.Certificates;
+
+public class WhenSystemUpdatesStandardCertificate
+{
+    private Mock<IAssessorDbContext> _mockDbContext;
+    private CertificateRepository _sut;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockDbContext = new Mock<IAssessorDbContext>();
+
+        _mockDbContext
+            .Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(1);
+
+        var mockUnitOfWork = new Mock<IAssessorUnitOfWork>();
+
+        mockUnitOfWork
+            .SetupGet(x => x.AssessorDbContext)
+            .Returns(_mockDbContext.Object);
+
+        _sut = new CertificateRepository(mockUnitOfWork.Object);
+    }
+
+    [Test]
+    public async Task ThenDateOfBirthIsUpdated()
+    {
+        // Arrange
+        var certificateId = Guid.NewGuid();
+        var updatedDateOfBirth = new DateTime(2000, 2, 3, 0, 0, 0, DateTimeKind.Utc);
+
+        var storedCertificate = new Certificate
+        {
+            Id = certificateId,
+            DateOfBirth = new DateTime(1, 1, 1, 0, 0, 0, DateTimeKind.Utc),
+            CertificateData = new CertificateData()
+        };
+
+        var updatedCertificate = new Certificate
+        {
+            Id = certificateId,
+            DateOfBirth = updatedDateOfBirth,
+            CertificateData = new CertificateData()
+        };
+
+        _mockDbContext
+            .Setup(x => x.Set<Certificate>())
+            .ReturnsDbSet(new List<Certificate>
+            {
+                storedCertificate
+            });
+
+        // Act
+        await _sut.UpdateStandardCertificate(
+            updatedCertificate,
+            "test-user",
+            action: null,
+            updateLog: false);
+
+        // Assert
+        storedCertificate.DateOfBirth.Should().Be(updatedDateOfBirth);
+
+        _mockDbContext.Verify(
+            x => x.SaveChangesAsync(It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    [Test]
+    public async Task ThenDateOfBirthCanBeUpdatedToNull()
+    {
+        // Arrange
+        var certificateId = Guid.NewGuid();
+
+        var storedCertificate = new Certificate
+        {
+            Id = certificateId,
+            DateOfBirth = new DateTime(2000, 2, 3, 0, 0, 0, DateTimeKind.Utc),
+            CertificateData = new CertificateData()
+        };
+
+        var updatedCertificate = new Certificate
+        {
+            Id = certificateId,
+            DateOfBirth = null,
+            CertificateData = new CertificateData()
+        };
+
+        _mockDbContext
+            .Setup(x => x.Set<Certificate>())
+            .ReturnsDbSet(new List<Certificate>
+            {
+                storedCertificate
+            });
+
+        // Act
+        await _sut.UpdateStandardCertificate(
+            updatedCertificate,
+            "test-user",
+            action: null,
+            updateLog: false);
+
+        // Assert
+        storedCertificate.DateOfBirth.Should().BeNull();
+    }
+}
